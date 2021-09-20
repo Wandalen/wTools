6039df6d93080c1e2faf6a9473987fa08f37eeaa
dmvict <dm.vict.kr@gmail.com>
2021-09-20 10:58:34 +0300

    Implement trivial routine `left_index`

--- rust/wtools/vector.rs
+++ rust/wtools/vector.rs
@@ -3,7 +3,7 @@ use std::cmp::PartialEq;
 
 //
 
-pub fn append_vectors_once<'a, T: PartialEq + Copy, F: FnMut(T)>( dst : &'a mut Vec<T>, src : &'a Vec<Vec<T>>, on_evaluate : Option<F> ) -> &'a mut Vec<T>
+pub fn append_vectors_once<'a, T : PartialEq + Copy, F : FnMut(T)>( dst : &'a mut Vec<T>, src : &'a Vec<Vec<T>>, on_evaluate : Option<F> ) -> &'a mut Vec<T>
 {
   if on_evaluate.is_none()
   {
@@ -24,5 +24,19 @@ pub fn append_vectors_once<'a, T: PartialEq + Copy, F: FnMut(T)>( dst : &'a mut 
   }
 
   dst
+}
+
+//
+
+pub fn left_index<T : PartialEq + Copy, F : FnMut(T)>( src : &Vec<T>, el : T, on_evaluate1 : Option<F> ) -> Option<usize>
+{
+  if on_evaluate1.is_none()
+  {
+    return src.iter().position( | &x | x == el );
+  }
+  else
+  {
+    unimplemented!( "not implemented for callbacks" );
+  }
 }
 
