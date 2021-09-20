8917d82886e9cff8feb12f404f795e94ce927312
dmvict <dm.vict.kr@gmail.com>
2021-09-20 10:58:12 +0300

    Write test routine `left_index_trivial`

--- rust/test/vector_test.rs
+++ rust/test/vector_test.rs
@@ -6,7 +6,7 @@ use wtools::vector;
 #[test]
 fn append_vectors_once_trivial()
 {
-  println!( "empty vector::" );
+  println!( "empty vector" );
   let mut dst: Vec<u8> = vec![];
   let src: Vec<Vec<u8>> = vec![];
   let got = vector::append_vectors_once( &mut dst, &src, None::<fn(_)> );
@@ -44,6 +44,38 @@ fn append_vectors_once_trivial()
   assert_eq!( *got, vec![ 1, 2 ] );
 }
 
+//
+
+#[test]
+fn left_index_trivial()
+{
+  println!( "empty vector" );
+  let src: Vec<u8> = vec![];
+  let got = vector::left_index( &src, 1, None::<fn(_)> );
+  assert_eq!( got, None );
+
+  println!( "filled vector, not matches" );
+  let src: Vec<u8> = vec![ 1, 2, 3 ];
+  let got = vector::left_index( &src, 4, None::<fn(_)> );
+  assert_eq!( got, None );
+
+  println!( "filled vector, matches one" );
+  let src: Vec<u8> = vec![ 1, 2, 3 ];
+  let got = vector::left_index( &src, 2, None::<fn(_)> );
+  assert_eq!( got, Some( 1 ) );
+
+  println!( "filled vector, matches several" );
+  let src: Vec<u8> = vec![ 1, 2, 2 ];
+  let got = vector::left_index( &src, 2, None::<fn(_)> );
+  assert_eq!( got, Some( 1 ) );
+
+  println!( "filled vector, matches all" );
+  let src: Vec<u8> = vec![ 2, 2, 2 ];
+  let got = vector::left_index( &src, 2, None::<fn(_)> );
+  assert_eq!( got, Some( 0 ) );
+}
+
 /*
   append_vectors_once_trivial
+  left_index_trivial
 */
