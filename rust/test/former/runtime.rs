c51be743b680dd7e805647c7bc7d6ce1cec09be1
wandalen <wandalen.me@gmail.com>
2021-11-29 18:15:56 +0200

    publishing module::former_runtime

--- rust/test/former/runtime.rs
+++ rust/test/former/runtime.rs
@@ -1,13 +1,13 @@
 
 use std::env;
 
-#[test]
-fn trybuild_tests()
-{
-  println!( "current_dir : {:?}", env::current_dir().unwrap() );
-  // let t = trybuild::TestCases::new();
-  // t.pass( "rust/test/former/test/basic_manual.rs" );
-}
+// #[test]
+// fn trybuild_tests()
+// {
+//   println!( "current_dir : {:?}", env::current_dir().unwrap() );
+//   // let t = trybuild::TestCases::new();
+//   // t.pass( "rust/test/former/test/basic_manual.rs" );
+// }
 
 include!( "./test/basic_manual.rs" );
 
