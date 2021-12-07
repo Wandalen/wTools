217d1075e1fb098e286c0c0517f8d701635d771a
wandalen <wandalen.me@gmail.com>
2021-12-07 13:31:14 +0200

    publish rust module::wtools

--- rust/impl/mod.rs
+++ rust/impl/mod.rs
@@ -5,11 +5,26 @@
 //! wTools - development tools.
 //!
 
-pub mod former;
-pub mod meta;
-pub mod str;
-pub mod time;
-pub mod vector;
+// pub mod former;
+// pub mod meta;
+// pub mod str;
+// pub mod time;
+// pub mod vector;
 
-pub use werror;
+// pub use werror;
+
+pub use inspect_type::*;
+pub use is_slice::*;
+pub use implements::*;
+
+pub use former as former;
+pub use werror as error;
 pub use wtest_basic as test;
+
+// former = { path = "../former", version = "~0" }
+// inspect_type = { path = "../inspect_type", version = "~0" }
+// is_slice = { path = "../is_slice", version = "~0" }
+// implements = { path = "../implements", version = "~0" }
+// wtest_basic = { path = "../wtest_basic", version = "~0" }
+// werror = { path = "../werror", version = "~0" }
+// wproc_macro = { path = "../wproc_macro", version = "~0", optional = true }
