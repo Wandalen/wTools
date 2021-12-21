55916541b8badc8fde632c7e527ace1ceb4008ab
dmvict <dm.vict.kr@gmail.com>
2021-12-21 17:39:29 +0200

    Extend test, use example as real utility

--- rust/test/commandsaggregator/commandsaggregator.rs
+++ rust/test/commandsaggregator/commandsaggregator.rs
@@ -20,31 +20,47 @@ use std::collections::HashMap;
 fn full_interface()
 {
   let mut track: Vec<ArgParsed> = vec![];
-  let mut command_with = | e : &ArgParsed |
+  let mut command_run = | e : &ArgParsed |
   {
     track.push( e.clone() );
-  }
+  };
   let commands = Commands::default();
-  commands.insert( ".with", CommandDescriptor { ro : Box::new( command_with ), h : "with", lh : "WITH" } );
+  let command_run_descriptor = CommandDescriptor
+  {
+    ro : Box::new( command_run ),
+    h : "Run test",
+    lh : "Run test",
+
+    /* can construct type RoutineDescriptor */
+    subject_hint : "Path to tests",
+    properties : HashMap
+    {
+      "v" : "verbosity of output",
+      "rapidity" : "rapidity of testing",
+      "routine" : "routine to run",
+    },
+    properties_aliases : HashMap { "verbosity" : [ "v" ] },
+  }
+  commands.insert( ".with", command_run_descriptor );
 
   let mut aggregator = CommandsAggregator::new()
   .commands().replace( commands ).end()
   .with_help( true )
   .form();
 
-  aggregator.instructionPerform( ".with subject num:1 str:abc array:[ a b c ]" )
+  aggregator.instructionPerform( ".run proto/ v:7 rapidity:-1 routine:[ test1, test2 ]" );
   let properties_map = HashMap
   {
-    "num" : OpType::Int( 1 ),
-    "str" : OpType::Str( "str" ),
-    "array" : OpType::Vector( vec![ "a", "b", "c" ] )
+    "verbosity" : OpType::Int( 7 ),
+    "rapidity" : OpType::Int( -7 ),
+    "routine" : OpType::Vector( vec![ "test1", "test2" ] ),
   };
   let exp = ArgParsed
   {
-    command : ".with subject num:1 str:abc array:[ a b c ]",
-    command_name : ".with",
-    subject : "subject",
-    instruction_argument : "subject num:1 str:abc array:[ a b c ]",
+    command : ".run proto/ v:7 rapidity:-1 routine:[ test1, test2 ]",
+    command_name : ".run",
+    subject : "proto/",
+    instruction_argument : "proto/ v:7 rapidity:-1 routine:[ test1, test2 ]",
     properties_map,
 
     /* additional fields */
