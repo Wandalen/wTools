a66ad0624b2ce6ea0e6ff2ee3d511b47803fd804
dmvict <dm.vict.kr@gmail.com>
2022-04-11 12:00:10 +0300

    Write test routines  `split_with_option_preserving_empty`, `split_with_option_preserving_delimeters`, `split_with_option_stripping`

--- rust/test/string/split_test.rs
+++ rust/test/string/split_test.rs
@@ -16,6 +16,7 @@ fn _basic()
   let iter = TheModule::string::split()
   .src( src )
   .delimeter( "bc" )
+  .stripping( false )
   .form();
   // assert_eq!( iter.collect::< Vec< _ > >(), vec![ "a", "bc", " def" ] );
   assert_eq!( iter.map( | e | String::from( e ) ).collect::< Vec< _ > >(), vec![ "a", "bc", " def" ] );
@@ -24,7 +25,97 @@ fn _basic()
 
 //
 
+fn _split_with_option_preserving_empty()
+{
+  let src = "a b c";
+  let iter = TheModule::string::split()
+  .src( src )
+  .delimeter( " " )
+  .preserving_empty( true )
+  .stripping( false )
+  .form();
+  assert_eq!( iter.map( | e | String::from( e ) ).collect::< Vec< _ > >(), vec![ "a", " ", "b", " ", "c" ] );
+
+  let src = "a b c";
+  let iter = TheModule::string::split()
+  .src( src )
+  .delimeter( " " )
+  .preserving_empty( false )
+  .stripping( false )
+  .form();
+  assert_eq!( iter.map( | e | String::from( e ) ).collect::< Vec< _ > >(), vec![ "a", " ", "b", " ", "c" ] );
+
+  /* */
+
+  let src = "a b c";
+  let iter = TheModule::string::split()
+  .src( src )
+  .delimeter( " " )
+  .preserving_empty( true )
+  .stripping( true )
+  .form();
+  assert_eq!( iter.map( | e | String::from( e ) ).collect::< Vec< _ > >(), vec![ "a", "", "b", "", "c" ] );
+
+  let src = "a b c";
+  let iter = TheModule::string::split()
+  .src( src )
+  .delimeter( " " )
+  .preserving_empty( false )
+  .stripping( true )
+  .form();
+  assert_eq!( iter.map( | e | String::from( e ) ).collect::< Vec< _ > >(), vec![ "a", "b", "c" ] );
+}
+
+//
+
+fn _split_with_option_preserving_delimeters()
+{
+  let src = "a b c";
+  let iter = TheModule::string::split()
+  .src( src )
+  .delimeter( " " )
+  .preserving_delimeters( true )
+  .stripping( false )
+  .form();
+  assert_eq!( iter.map( | e | String::from( e ) ).collect::< Vec< _ > >(), vec![ "a", " ", "b", " ", "c" ] );
+
+  let src = "a b c";
+  let iter = TheModule::string::split()
+  .src( src )
+  .delimeter( " " )
+  .preserving_delimeters( false )
+  .stripping( false )
+  .form();
+  assert_eq!( iter.map( | e | String::from( e ) ).collect::< Vec< _ > >(), vec![ "a", "b", "c" ] );
+}
+
+//
+
+fn _split_with_option_stripping()
+{
+  let src = "a b c";
+  let iter = TheModule::string::split()
+  .src( src )
+  .delimeter( " " )
+  .stripping( true )
+  .form();
+  assert_eq!( iter.map( | e | String::from( e ) ).collect::< Vec< _ > >(), vec![ "a", "", "b", "", "c" ] );
+
+  let src = "a b c";
+  let iter = TheModule::string::split()
+  .src( src )
+  .delimeter( " " )
+  .stripping( false )
+  .form();
+  assert_eq!( iter.map( | e | String::from( e ) ).collect::< Vec< _ > >(), vec![ "a", " ", "b", " ", "c" ] );
+}
+
+//
+
 test_suite!
 {
   basic,
+  split_with_option_preserving_empty,
+  split_with_option_preserving_delimeters,
+  split_with_option_stripping,
 }
