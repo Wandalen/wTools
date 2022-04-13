ede7df222064afb291186e2fb6e20f377cb6f6f4
dmvict <dm.vict.kr@gmail.com>
2022-04-13 11:11:36 +0300

    Extend test routine `split_with_option_stripping`, add test cases for stripped parts

--- rust/test/string/split_test.rs
+++ rust/test/string/split_test.rs
@@ -104,6 +104,25 @@ fn _split_with_option_stripping()
   .stripping( false )
   .perform();
   assert_eq!( iter.map( | e | String::from( e ) ).collect::< Vec< _ > >(), vec![ "a", " ", "b", " ", "c" ] );
+
+  /* */
+
+  let src = "a b c";
+  let iter = TheModule::string::split()
+  .src( src )
+  .delimeter( "b" )
+  .stripping( true )
+  .perform();
+  assert_eq!( iter.map( | e | String::from( e ) ).collect::< Vec< _ > >(), vec![ "a", "b", "c" ] );
+
+  let src = "a b c";
+  let iter = TheModule::string::split()
+  .src( src )
+  .delimeter( "b" )
+  .preserving_delimeters( false )
+  .stripping( true )
+  .perform();
+  assert_eq!( iter.map( | e | String::from( e ) ).collect::< Vec< _ > >(), vec![ "a", "c" ] );
 }
 
 //
