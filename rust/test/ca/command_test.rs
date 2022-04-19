c8531c52044d8819bf031fcb4c55335baa9e2aac
dmvict <dm.vict.kr@gmail.com>
2022-04-19 08:41:06 +0300

    Write test routine `callback_trivial`

--- rust/test/ca/command_test.rs
+++ rust/test/ca/command_test.rs
@@ -1,13 +1,12 @@
 
 use wca::*;
 use maplit::hashmap;
+use wtest_basic::*;
 
 //
 
-#[test]
-fn basic()
+fn _basic()
 {
-
   let command = CommandOptions::default()
   .hint( "hint" )
   .long_hint( "long_hint" )
@@ -17,9 +16,8 @@ fn basic()
   .property_hint( "prop2", "hint of prop2" )
   .property_alias( "property_alias", "a1" )
   .property_alias( "property_alias", "a2" )
-  .routine( &|| { println!( "hello" ) } )
-  .form()
-  ;
+  .routine( &| _i : &instruction::Instruction | { println!( "hello" ); Ok( () ) } )
+  .form();
 
   dbg!( &command );
 
@@ -40,21 +38,17 @@ fn basic()
     "property_alias".to_string() => vec![ "a1".to_string(), "a2".to_string() ],
   };
   assert_eq!( command.properties_aliases, properties_aliases );
-
 }
 
 //
 
-#[test]
-fn shortcut()
+fn _shortcut()
 {
-
   let command = CommandOptions::default()
   .h( "hint2" )
   .lh( "long_hint2" )
-  .ro( &|| { println!( "hello" ) } )
-  .form()
-  ;
+  .ro( &| _i : &instruction::Instruction | { println!( "hello" ); Ok( () ) } )
+  .form();
 
   dbg!( &command );
 
@@ -62,5 +56,34 @@ fn shortcut()
   assert_eq!( command.long_hint, "long_hint2".to_string() );
   assert_eq!( command.phrase, "".to_string() );
   assert_eq!( command.subject_hint, "".to_string() );
-
 }
+
+//
+
+fn _callback_trivial()
+{
+  let command = CommandOptions::default()
+  .hint( "hint" )
+  .long_hint( "long_hint" )
+  .phrase( "phrase" )
+  .subject_hint( "subject_hint" )
+  .property_hint( "prop1", "hint of prop1" )
+  .property_hint( "prop2", "hint of prop2" )
+  .property_alias( "property_alias", "a1" )
+  .property_alias( "property_alias", "a2" )
+  .routine( &| _i : &instruction::Instruction | { println!( "hello" ); Ok( () ) } )
+  .form();
+
+  let perform = command.perform( &instruction::Instruction::default() );
+  assert!( perform.is_ok() );
+}
+
+//
+
+test_suite!
+{
+  basic,
+  shortcut,
+  callback_trivial,
+}
+
