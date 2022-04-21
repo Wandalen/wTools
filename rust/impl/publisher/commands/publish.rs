8a402356179533fda3969d88220b287546f10134
dmvict <dm.vict.kr@gmail.com>
2022-04-21 09:15:24 +0300

    Improve routine `publish`, publish multiple packages using globs, skip local packages, handle situation with no remote version

--- rust/impl/publisher/commands/publish.rs
+++ rust/impl/publisher/commands/publish.rs
@@ -5,6 +5,7 @@ use wca::*;
 use std::env;
 use std::fs;
 use std::fmt::Write;
+use std::path::PathBuf;
 use toml_edit::value;
 use cargo_metadata::MetadataCommand;
 use wtools::error::Error;
@@ -17,76 +18,90 @@ pub fn publish( instruction : &instruction::Instruction ) -> Result<(), Error>
 {
   let current_path = env::current_dir().unwrap();
 
-  let mut manifest = manifest_get( instruction.subject.as_str() ).unwrap();
-  let data = manifest.manifest_data.as_deref_mut().unwrap();
-
-  let mut package_dir = manifest.manifest_path.clone();
-  package_dir.pop();
-
-  let output = process::start_sync( "cargo package", &package_dir ).unwrap();
-  process::log_output( &output );
-
-  let name = &data[ "package" ][ "name" ].clone();
-  let name = name.as_str().unwrap();
-  let version = &data[ "package" ][ "version" ].clone();
-  let version = version.as_str().unwrap();
-  let local_package_path = local_package_path_get( name, version, &manifest.manifest_path );
-
-  let local_package = fs::read( &local_package_path ).unwrap();
-  let remote_package = http::retrieve_bytes( name, version ).unwrap();
-
-  let digest_of_local = digest::hash( &local_package );
-  let digest_of_remote = digest::hash( &remote_package );
-
-  if digest_of_local != digest_of_remote
+  let paths = files::find( &current_path, instruction.subject.split( " " ).collect::<Vec<&str>>().as_slice() );
+  let mut paths = paths.iter().filter_map( | s | if s.ends_with( "Cargo.toml" ) { Some( s.into() ) } else { None } ).collect::<Vec<PathBuf>>();
+  if paths.is_empty() /* && !path.glob_is( &instruction.subject ) qqq : implement `glob_is` */
   {
-    data[ "package" ][ "version" ] = bump( version ).unwrap();
+    paths.push( PathBuf::from( &instruction.subject ) );
+  }
+
+  for path in paths
+  {
+    let mut manifest = manifest_get( path ).unwrap();
+    if !manifest.package_is() || manifest.local_is()
+    {
+      continue;
+    }
+    let data = manifest.manifest_data.as_deref_mut().unwrap();
+
+    let mut package_dir = manifest.manifest_path.clone();
+    package_dir.pop();
+
+    let output = process::start_sync( "cargo package", &package_dir ).unwrap();
+    process::log_output( &output );
+
+    let name = &data[ "package" ][ "name" ].clone();
+    let name = name.as_str().unwrap();
     let version = &data[ "package" ][ "version" ].clone();
     let version = version.as_str().unwrap();
-    manifest.store().unwrap();
+    let local_package_path = local_package_path_get( name, version, &manifest.manifest_path );
 
-    let dry = match instruction.properties_map.get( "dry" )
+    let local_package = fs::read( &local_package_path ).unwrap();
+    let remote_package = http::retrieve_bytes( name, version ).unwrap_or_default();
+
+    let digest_of_local = digest::hash( &local_package );
+    let digest_of_remote = digest::hash( &remote_package );
+
+    if digest_of_local != digest_of_remote
     {
-      Some( x ) => x.to_bool_like(),
-      None => BoolLike::False,
-    };
+      data[ "package" ][ "version" ] = bump( version ).unwrap();
+      let version = &data[ "package" ][ "version" ].clone();
+      let version = version.as_str().unwrap();
+      manifest.store().unwrap();
 
-    if dry == BoolLike::True
-    {
-      let mut buf = String::new();
-      write!( &mut buf, "git commit --dry-run -am \"{} v{}\"", name, version ).unwrap();
-      let output = process::start_sync( &buf, &current_path ).unwrap();
-      process::log_output( &output );
+      let dry = match instruction.properties_map.get( "dry" )
+      {
+        Some( x ) => x.to_bool_like(),
+        None => BoolLike::False,
+      };
 
-      let output = process::start_sync( "git push --dry-run", &current_path ).unwrap();
-      process::log_output( &output );
+      if dry == BoolLike::True
+      {
+        let mut buf = String::new();
+        write!( &mut buf, "git commit --dry-run -am \"{} v{}\"", name, version ).unwrap();
+        let output = process::start_sync( &buf, &current_path ).unwrap();
+        process::log_output( &output );
 
-      let output = process::start_sync( "cargo publish --dry-run --allow-dirty", &package_dir ).unwrap();
-      process::log_output( &output );
+        let output = process::start_sync( "git push --dry-run", &current_path ).unwrap();
+        process::log_output( &output );
+
+        let output = process::start_sync( "cargo publish --dry-run --allow-dirty", &package_dir ).unwrap();
+        process::log_output( &output );
+      }
+      else
+      {
+        let mut buf = String::new();
+        write!( &mut buf, "git commit -am \"{} v{}\"", name, version ).unwrap();
+        let output = process::start_sync( &buf, &current_path ).unwrap();
+        process::log_output( &output );
+
+        let output = process::start_sync( "git push", &current_path ).unwrap();
+        process::log_output( &output );
+
+        let output = process::start_sync( "cargo publish", &package_dir ).unwrap();
+        process::log_output( &output );
+      }
     }
     else
     {
-      let mut buf = String::new();
-      write!( &mut buf, "git commit -am \"{} v{}\"", name, version ).unwrap();
-      let output = process::start_sync( &buf, &current_path ).unwrap();
-      process::log_output( &output );
-
-      let output = process::start_sync( "git push", &current_path ).unwrap();
-      process::log_output( &output );
-
-      let output = process::start_sync( "cargo publish", &package_dir ).unwrap();
-      process::log_output( &output );
+      println!( "Package {} is up to date", name );
     }
-  }
-  else
-  {
-    println!( "Package {} is up to date", name );
   }
 
   Ok( () )
 }
 
-fn manifest_get( path : &str ) -> anyhow::Result<manifest::Manifest>
+fn manifest_get( path : impl Into<PathBuf> ) -> anyhow::Result<manifest::Manifest>
 {
   let mut manifest = manifest::Manifest::new();
   manifest.manifest_path_from_str( path )?;
