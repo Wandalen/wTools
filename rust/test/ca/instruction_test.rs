954095f449fe1f425cdb57f762c6e22374d9f44b
dmvict <dm.vict.kr@gmail.com>
2022-04-26 10:16:23 +0300

    Write test routine `with_several_values`

--- rust/test/ca/instruction_test.rs
+++ rust/test/ca/instruction_test.rs
@@ -1,6 +1,6 @@
 
 use wca::*;
-use wstring_tools::string::parse::OpType::{ Primitive/* , Vector */ };
+use wstring_tools::string::parse::OpType::{ Primitive, Vector };
 use wtest_basic::*;
 use std::collections::HashMap;
 
@@ -128,9 +128,95 @@ fn _basic()
   assert_eq!( instruction, exp );
 }
 
+fn _with_several_values()
+{
+  let instruction = instruction::instruction_parse()
+  .instruction( ".get some v:1 v:2" )
+  .several_values( false )
+  .perform();
+  let exp = instruction::Instruction
+  {
+    err : None,
+    command_name : ".get".to_string(),
+    subject : "some".to_string(),
+    properties_map : HashMap::from([ ( "v".to_string(), Primitive( "2".to_string() ) ) ]),
+  };
+  assert_eq!( instruction, exp );
+
+  let instruction = instruction::instruction_parse()
+  .instruction( ".get some v:[1,2]" )
+  .several_values( false )
+  .perform();
+  let exp = instruction::Instruction
+  {
+    err : None,
+    command_name : ".get".to_string(),
+    subject : "some".to_string(),
+    properties_map : HashMap::from([ ( "v".to_string(), Vector( vec![ "1".to_string(), "2".to_string() ] ) ) ]),
+  };
+  assert_eq!( instruction, exp );
+
+  /* */
+
+  let instruction = instruction::instruction_parse()
+  .instruction( ".get some v:1 v:2" )
+  .several_values( true )
+  .perform();
+  let exp = instruction::Instruction
+  {
+    err : None,
+    command_name : ".get".to_string(),
+    subject : "some".to_string(),
+    properties_map : HashMap::from([ ( "v".to_string(), Vector( vec![ "1".to_string(), "2".to_string() ] ) ) ]),
+  };
+  assert_eq!( instruction, exp );
+
+  let instruction = instruction::instruction_parse()
+  .instruction( ".get some v:[1,2]" )
+  .several_values( true )
+  .perform();
+  let exp = instruction::Instruction
+  {
+    err : None,
+    command_name : ".get".to_string(),
+    subject : "some".to_string(),
+    properties_map : HashMap::from([ ( "v".to_string(), Vector( vec![ "1".to_string(), "2".to_string() ] ) ) ]),
+  };
+  assert_eq!( instruction, exp );
+
+  /* */
+
+  let instruction = instruction::instruction_parse()
+  .instruction( ".get some v:[1,2] v:3" )
+  .several_values( true )
+  .perform();
+  let exp = instruction::Instruction
+  {
+    err : None,
+    command_name : ".get".to_string(),
+    subject : "some".to_string(),
+    properties_map : HashMap::from([ ( "v".to_string(), Vector( vec![ "1".to_string(), "2".to_string(), "3".to_string() ] ) ) ]),
+  };
+  assert_eq!( instruction, exp );
+
+  let instruction = instruction::instruction_parse()
+  .instruction( ".get some v:3 v:[1,2]" )
+  .several_values( true )
+  .perform();
+  let exp = instruction::Instruction
+  {
+    err : None,
+    command_name : ".get".to_string(),
+    subject : "some".to_string(),
+    properties_map : HashMap::from([ ( "v".to_string(), Vector( vec![ "3".to_string(), "1".to_string(), "2".to_string() ] ) ) ]),
+  };
+  assert_eq!( instruction, exp );
+}
+
 //
 
 test_suite!
 {
   basic,
+  with_several_values,
 }
