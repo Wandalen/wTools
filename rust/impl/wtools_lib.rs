11c96053b635fabee1952d82d03f369f576d5419
wandalen <wandalen.me@gmail.com>
2022-05-16 16:22:39 +0300

    .

--- rust/impl/wtools_lib.rs
+++ rust/impl/wtools_lib.rs
@@ -4,10 +4,6 @@
 
 /* qqq : for Dima : wrong header */
 
-// #![ warn( rust_2018_idioms ) ]
-// #![ warn( missing_debug_implementations ) ]
-// #![ warn( missing_docs ) ]
-
 //!
 //! wTools - Collection of general purpose tools for solving problems. Fundamentally extend the language without spoiling, so may be used solely or in conjunction with another module of such kind.
 //!
