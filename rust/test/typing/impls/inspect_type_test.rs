be88b179e1f5c61b47d229039c0199dc79d4b587
dmvict <dm.vict.kr@gmail.com>
2022-05-25 09:38:02 +0300

    Update tests of module `typing_tools`, use new test macroses

--- rust/test/typing/impls/inspect_type_test.rs
+++ rust/test/typing/impls/inspect_type_test.rs
@@ -6,39 +6,43 @@ use super::TheModule;
 //
 
 #[ cfg( feature = "nightly" ) ]
-fn inspect_to_str_type_of_test()
+tests_impls!
 {
+  #[ test ]
+  fn inspect_to_str_type_of()
+  {
 
-  let exp = "sizeof( &[1, 2, 3][..] : &[i32] ) = 16".to_string();
-  let got = TheModule::inspect_to_str_type_of!( &[ 1, 2, 3 ][ .. ] );
-  assert_eq!( got, exp );
+    let exp = "sizeof( &[1, 2, 3][..] : &[i32] ) = 16".to_string();
+    let got = TheModule::inspect_to_str_type_of!( &[ 1, 2, 3 ][ .. ] );
+    assert_eq!( got, exp );
 
-  let exp = "sizeof( &[1, 2, 3] : &[i32; 3] ) = 8".to_string();
-  let got = TheModule::inspect_to_str_type_of!( &[ 1, 2, 3 ] );
-  assert_eq!( got, exp );
+    let exp = "sizeof( &[1, 2, 3] : &[i32; 3] ) = 8".to_string();
+    let got = TheModule::inspect_to_str_type_of!( &[ 1, 2, 3 ] );
+    assert_eq!( got, exp );
 
+  }
+
+  //
+
+  #[ test ]
+  fn inspect_type_of()
+  {
+
+    let exp = "sizeof( &[1, 2, 3][..] : &[i32] ) = 16".to_string();
+    let got = TheModule::inspect_type_of!( &[ 1, 2, 3 ][ .. ] );
+    assert_eq!( got, exp );
+
+    let exp = "sizeof( &[1, 2, 3] : &[i32; 3] ) = 8".to_string();
+    let got = TheModule::inspect_type_of!( &[ 1, 2, 3 ] );
+    assert_eq!( got, exp );
+
+  }
 }
 
 //
 
 #[ cfg( feature = "nightly" ) ]
-fn inspect_type_of_test()
-{
-
-  let exp = "sizeof( &[1, 2, 3][..] : &[i32] ) = 16".to_string();
-  let got = TheModule::inspect_type_of!( &[ 1, 2, 3 ][ .. ] );
-  assert_eq!( got, exp );
-
-  let exp = "sizeof( &[1, 2, 3] : &[i32; 3] ) = 8".to_string();
-  let got = TheModule::inspect_type_of!( &[ 1, 2, 3 ] );
-  assert_eq!( got, exp );
-
-}
-
-//
-
-#[ cfg( feature = "nightly" ) ]
-test_suite!
+tests_impls!
 {
   inspect_to_str_type_of,
   inspect_type_of,
