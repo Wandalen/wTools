8ae7a2d98f1f85ceef2920917c1bb0c8e9c7dc12
wandalen <wandalen.me@gmail.com>
2022-05-26 17:56:11 +0300

    docs experiment

--- rust/test/dt/type_constructor/type_constructor_lib_test.rs
+++ rust/test/dt/type_constructor/type_constructor_lib_test.rs
@@ -1,6 +1,3 @@
-
-#![doc(html_logo_url="https://raw.githubusercontent.com/Wandalen/wTools/master/asset/img/logo_v2_trans_rect_small.png")]
-
 #![ warn( rust_2018_idioms ) ]
 #![ warn( missing_debug_implementations ) ]
 #![ warn( missing_docs ) ]
