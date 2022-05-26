3d9bafe7d178761a9471bbf4eb6213489e4dba5d
dmvict <dm.vict.kr@gmail.com>
2022-05-26 13:44:13 +0300

    Update file `integration_test.rs`, write code to perform smoke testing using environments

--- rust/test/integration_test.rs
+++ rust/test/integration_test.rs
@@ -1,40 +1,48 @@
-#[ allow( unused_imports ) ]
-use test_tools::test_suite;
+// #[ allow( unused_imports ) ]
+// use test_tools::test_suite;
 
 #[ derive( Debug ) ]
-struct IntegrationModuleTest< 'a >
+struct SmokeModuleTest< 'a >
 {
   dependency_name : &'a str,
   version : &'a str,
+  local_path : &'a str,
   code : &'a str,
   test_path : std::path::PathBuf,
+  test_postfix : &'a str,
 }
 
-impl< 'a > IntegrationModuleTest< 'a >
+impl< 'a > SmokeModuleTest< 'a >
 {
-  fn new( dependency_name : &'a str ) -> IntegrationModuleTest< 'a >
+  fn new( dependency_name : &'a str ) -> SmokeModuleTest< 'a >
   {
     let mut test_path = std::env::temp_dir();
     test_path.push( dependency_name );
 
-    IntegrationModuleTest
+    SmokeModuleTest
     {
       dependency_name,
       version : "*",
+      local_path : "",
       code : "",
       test_path,
+      test_postfix : "_smoke_test",
     }
   }
 
-  #[ allow( dead_code ) ]
-  fn version( &mut self, version : &'a str ) -> &mut IntegrationModuleTest< 'a >
+  fn version( &mut self, version : &'a str ) -> &mut SmokeModuleTest< 'a >
   {
     self.version = version;
     self
   }
 
-  #[ allow( dead_code ) ]
-  fn code( &mut self, code : &'a str ) -> &mut IntegrationModuleTest< 'a >
+  fn local_path( &mut self, local_path : &'a str ) -> &mut SmokeModuleTest< 'a >
+  {
+    self.local_path = local_path;
+    self
+  }
+
+  fn code( &mut self, code : &'a str ) -> &mut SmokeModuleTest< 'a >
   {
     self.code = code;
     self
@@ -47,7 +55,7 @@ impl< 'a > IntegrationModuleTest< 'a >
     let mut test_path = self.test_path.clone();
 
     /* create binary test module */
-    let test_name = format!( "{}_test", self.dependency_name );
+    let test_name = format!( "{}{}", self.dependency_name, self.test_postfix );
     let output = std::process::Command::new( "cargo" )
     .current_dir( &test_path )
     .args([ "new", "--bin", &test_name ])
@@ -58,69 +66,56 @@ impl< 'a > IntegrationModuleTest< 'a >
     test_path.push( test_name );
 
     /* setup config */
-    let dependency_name = format!( "dependencies.{}", self.dependency_name );
-    let selector = self.selector_global_or_temporary();
-    let output = std::process::Command::new( &selector )
-    .current_dir( &test_path )
-    .args([ "set", "./Cargo.toml", &dependency_name, "*" ])
-    .output()
-    .expect( "Failed to execute command" );
-    let output = std::str::from_utf8( &output.stdout ).expect( "Found invalid UTF-8" );
+    let local_path = if self.local_path == "" { "".to_string() } else { format!( ", path = \"{}\"", self.local_path ) };
+    let dependencies_section = format!( "{} = {{ version = \"{}\"{} }}", self.dependency_name, self.version, &local_path );
+    let config_data = format!
+    (
+      "[package]\n\
+      edition = \"2018\"\n\
+      name = \"{}_smoke_test\"\n\
+      version = \"0.0.1\"\n\
+      \n\
+      [dependencies]\n\
+      {}",
+      &self.dependency_name,
+      &dependencies_section
+    );
     let mut config_path = test_path.clone();
     config_path.push( "Cargo.toml" );
-    std::fs::write( config_path, output ).unwrap();
+    std::fs::write( config_path, config_data ).unwrap();
 
     /* write code */
     test_path.push( "src" );
     test_path.push( "main.rs" );
-    let code = format!( "#[ allow( unused_imports ) ]\nfn main()\n{{\n  use {}::*;\n  {}\n}}", self.dependency_name, self.code );
+    let code = format!
+    (
+      "#[ allow( unused_imports ) ]\n\
+      fn main()\n\
+      {{\n  \
+        use {}::*;\n  \
+        {}\n\
+      }}",
+      self.dependency_name,
+      self.code
+    );
     std::fs::write( &test_path, code ).unwrap();
 
     Ok( () )
   }
 
-  fn selector_global_or_temporary( &self ) -> String
-  {
-    let output = std::process::Command::new( "selector" )
-    .current_dir( &self.test_path )
-    .arg( "--help" )
-    .output();
-
-    if output.is_ok()
-    {
-      "selector".to_owned()
-    }
-    else
-    {
-      let mut tmp_path = self.test_path.clone();
-      tmp_path.push( ".cargo" );
-
-      std::fs::create_dir( &tmp_path ).unwrap();
-
-      let _output = std::process::Command::new( "cargo" )
-      .current_dir( &tmp_path )
-      .args([ "install", "wselector", "--root", tmp_path.to_str().unwrap() ])
-      .output()
-      .expect( "Failed to install local version of selector" );
-
-      tmp_path.push( "bin" );
-      tmp_path.push( "selector" );
-      let selector_str = tmp_path.to_str().unwrap();
-      selector_str.to_owned()
-    }
-  }
-
   fn perform( &self ) -> Result<(), &'static str>
   {
     let mut test_path = self.test_path.clone();
-    let test_name = format!( "{}_test", self.dependency_name );
+    let test_name = format!( "{}{}", self.dependency_name, self.test_postfix );
     test_path.push( test_name );
 
     let output = std::process::Command::new( "cargo" )
     .current_dir( test_path )
-    .args([ "test", "--release" ])
+    .args([ "run", "--release" ])
     .output()
     .unwrap();
+    assert!( output.status.success(), "Smoke test failed" );
+    println!( "{}", std::str::from_utf8( &output.stdout ).expect( "Found invalid UTF-8" ) );
     println!( "{}", std::str::from_utf8( &output.stderr ).expect( "Found invalid UTF-8" ) );
 
     Ok( () )
@@ -144,28 +139,54 @@ impl< 'a > IntegrationModuleTest< 'a >
 
 //
 
-macro_rules! module_integration_test
+fn smoke_test( local : bool )
 {
-  ( $( $name : ident , )* ) =>
+  let module_name = std::env::var( "CARGO_PKG_NAME" ).unwrap();
+  let module_path = std::env::var( "CARGO_MANIFEST_DIR" ).unwrap();
+  let mut code_path = std::path::PathBuf::from( module_path.clone() );
+  code_path.push( "rust" );
+  code_path.push( "test" );
+  code_path.push( if module_name.starts_with( "w" ) { &module_name[ 1.. ] } else { module_name.as_str() } );
+  code_path.push( "_asset" );
+  code_path.push( "smoke.rs" );
+
+  let mut t = SmokeModuleTest::new( module_name.as_str() );
+  t.clean( true ).unwrap();
+
+  let data;
+  if code_path.exists()
   {
-    $(
-      #[ test ]
-      fn $name()
-      {
-        let t = IntegrationModuleTest::new( stringify!( $name ) );
-        t.clean( true ).unwrap();
-        t.form().unwrap();
-        t.perform().unwrap();
-        t.clean( false ).unwrap();
-      }
-    )*
+    data = std::fs::read_to_string( code_path ).unwrap();
+    t.code( &data );
   }
+  t.version( "*" );
+  if local
+  {
+    t.local_path( module_path.as_str() );
+  }
+  t.form().unwrap();
+  t.perform().unwrap();
+  t.clean( false ).unwrap();
 }
 
 //
 
-module_integration_test!
+#[ test ]
+fn integration()
 {
-  wtools,
-  wtest_basic,
+  if let Ok( value ) = std::env::var( "WITH_SMOKE" )
+  {
+    match value.as_str()
+    {
+      "false" => {},
+      "local" => smoke_test( true ),
+      "published" => smoke_test( false ),
+      _ =>
+      {
+        smoke_test( true );
+        smoke_test( false );
+      },
+    }
+  }
 }
+
