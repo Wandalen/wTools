350c60695c499c1fcce94353b1918002b4aeb0f4
wandalen <wandalen.me@gmail.com>
2022-05-26 22:45:57 +0300

    publishing

--- rust/impl/dt/type_constructor/type_constructor_lib.rs
+++ rust/impl/dt/type_constructor/type_constructor_lib.rs
@@ -1,4 +1,4 @@
-#![doc(html_logo_url="https://raw.githubusercontent.com/Wandalen/wTools/master/asset/img/logo_v3_trans_square.png")]
+#![ doc( html_logo_url = "https://raw.githubusercontent.com/Wandalen/wTools/master/asset/img/logo_v3_trans_square.png" ) ]
 #![ warn( rust_2018_idioms ) ]
 #![ warn( missing_debug_implementations ) ]
 #![ warn( missing_docs ) ]
