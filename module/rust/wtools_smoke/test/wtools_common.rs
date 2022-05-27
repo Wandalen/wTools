11eeb89ef03eb01d71f55cc1a7987b7a1e2dbb0c
dmvict <dm.vict.kr@gmail.com>
2022-05-27 09:46:14 +0300

    Simplify module for smoke testing, remove extra tests

--- module/rust/wtools_smoke/test/wtools_common.rs
+++
@@ -1,9 +0,0 @@
-
-#[ allow( unused_imports ) ]
-use super::wtools;
-
-pub fn wtools_time()
-{
-  let now = wtools::time::now();
-  assert!( now < 0 );
-}
---
+++ module/rust/wtools_smoke/test/wtools_smoke_test.rs
@@ -0,0 +1,5 @@
+#![ allow( unused_imports ) ]
+
+use local;
+use published;
+
