bad86531673da8ffe4c0bf69115cb6e056e28d04
dmvict <dm.vict.kr@gmail.com>
2022-05-27 11:36:03 +0300

    Fix module `time_tools`, update tests and samples to run under each feature

--- rust/test/time/lib_test.rs
+++ rust/test/time/lib_test.rs
@@ -1,3 +1,5 @@
+#[ cfg( feature = "chrono" ) ]
 use time_tools as TheModule;
 
+#[ cfg( feature = "chrono" ) ]
 mod now_test;
--- rust/test/time/mod.rs
+++ rust/test/time/mod.rs
@@ -1,3 +1,5 @@
+#[ cfg( feature = "chrono" ) ]
 use wtools::time as TheModule;
 
+#[ cfg( feature = "chrono" ) ]
 mod now_test;
