e4b42d0732acb513bac42d5d4dc808bdae628d8c
wandalen <wandalen.me@gmail.com>
2022-05-31 22:38:59 +0300

    docs experiment

--- rust/impl/dt/type_constructor/type_constructor_lib.rs
+++ rust/impl/dt/type_constructor/type_constructor_lib.rs
@@ -1,4 +1,7 @@
 #![ doc( html_logo_url = "https://raw.githubusercontent.com/Wandalen/wTools/master/asset/img/logo_v3_trans_square.png" ) ]
+#![ doc( html_favicon_url = "https://raw.githubusercontent.com/Wandalen/wTools/master/asset/img/logo_v3_trans_square_icon_small.ico")]
+#![ doc( html_root_url = "https://docs.rs/type_constructor/latest/type_constructor/")]
+
 #![ warn( rust_2018_idioms ) ]
 #![ warn( missing_debug_implementations ) ]
 #![ warn( missing_docs ) ]
@@ -8,7 +11,13 @@
 //!
 
 #![ doc = include_str!( concat!( env!( "CARGO_MANIFEST_DIR" ), "/Readme.md" ) ) ]
+
 // #![ no_std ]
+
+#[ cfg( not( feature = "use_std" ) ) ]
+extern crate core as std;
+#[ cfg( all( not( feature = "use_std" ), feature = "use_alloc" ) ) ]
+extern crate alloc;
 
 #[ path = "./inc.rs" ]
 mod inc;
