125aaef72e132721d9791b7e8aaad3a88c54f62d
dmvict <dm.vict.kr@gmail.com>
2022-05-31 08:12:33 +0300

    Fix module `type_constructor`, run test under feature `make`

--- rust/test/dt/type_constructor/inc.rs
+++ rust/test/dt/type_constructor/inc.rs
@@ -5,6 +5,7 @@ use super::TheModule;
 mod single_test;
 mod pair_test;
 mod homo_pair_test;
+#[ cfg( any( feature = "make", feature = "dt_make" ) ) ]
 mod many_test;
 
 #[ cfg( any( feature = "make", feature = "dt_make" ) ) ]
