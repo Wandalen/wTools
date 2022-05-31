48b628eedb9c74ac2f1b2b9854d11c6103e0a663
dmvict <dm.vict.kr@gmail.com>
2022-05-31 08:53:29 +0300

    Fix module `type_constructor`, remove directive from root of module tests, fix test routine `parametrized_multiple` use directive for separate cases

--- rust/test/dt/type_constructor/inc.rs
+++ rust/test/dt/type_constructor/inc.rs
@@ -5,7 +5,6 @@ use super::TheModule;
 mod single_test;
 mod pair_test;
 mod homo_pair_test;
-#[ cfg( any( feature = "make", feature = "dt_make" ) ) ]
 mod many_test;
 
 #[ cfg( any( feature = "make", feature = "dt_make" ) ) ]
