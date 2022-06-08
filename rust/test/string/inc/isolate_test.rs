4cfa42e37e69f308a5abc646e1f8fc68b419d893
dmvict <dm.vict.kr@gmail.com>
2022-06-08 09:12:05 +0300

    Write test routine `isolate_left_or_none`

--- rust/test/string/inc/isolate_test.rs
+++ rust/test/string/inc/isolate_test.rs
@@ -14,6 +14,84 @@ tests_impls!
     let mut exp = ( "", None, "" );
     assert_eq!( req, exp );
   }
+
+  fn isolate_left_or_none()
+  {
+    /* no entry */
+    let src = "abaca";
+    let req = TheModule::string::isolate_left()
+    .src( src )
+    .delimeter( "f" )
+    .none( true )
+    .perform();
+    let mut exp = ( "", None, "abaca" );
+    assert_eq!( req, exp );
+
+    /* default */
+    let src = "abaca";
+    let req = TheModule::string::isolate_left()
+    .src( src )
+    .delimeter( "a" )
+    .none( true )
+    .perform();
+    let mut exp = ( "", Some( "a" ), "baca" );
+    assert_eq!( req, exp );
+
+    /* times - 0 */
+    let src = "abaca";
+    let req = TheModule::string::isolate_left()
+    .src( src )
+    .delimeter( "a" )
+    .times( 0 )
+    .none( true )
+    .perform();
+    let mut exp = ( "", None, "abaca" );
+    assert_eq!( req, exp );
+
+    /* times - 1 */
+    let src = "abaca";
+    let req = TheModule::string::isolate_left()
+    .src( src )
+    .delimeter( "a" )
+    .times( 1 )
+    .none( true )
+    .perform();
+    let mut exp = ( "", Some( "a" ), "baca" );
+    assert_eq!( req, exp );
+
+    /* times - 2 */
+    let src = "abaca";
+    let req = TheModule::string::isolate_left()
+    .src( src )
+    .delimeter( "a" )
+    .times( 2 )
+    .none( true )
+    .perform();
+    let mut exp = ( "ab", Some( "a" ), "ca" );
+    assert_eq!( req, exp );
+
+    /* times - 3 */
+    let src = "abaca";
+    let req = TheModule::string::isolate_left()
+    .src( src )
+    .delimeter( "a" )
+    .times( 3 )
+    .none( true )
+    .perform();
+    let mut exp = ( "abac", Some( "a" ), "" );
+    assert_eq!( req, exp );
+
+    /* times - 4 */
+    let src = "abaca";
+    let req = TheModule::string::isolate_left()
+    .src( src )
+    .delimeter( "a" )
+    .times( 4 )
+    .none( true )
+    .perform();
+    let mut exp = ( "", None, "abaca" );
+    assert_eq!( req, exp );
+  }
 }
 
 //
@@ -21,4 +99,5 @@ tests_impls!
 tests_index!
 {
   basic,
+  isolate_left_or_none,
 }
