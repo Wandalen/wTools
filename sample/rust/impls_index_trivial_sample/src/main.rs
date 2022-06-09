717cb648d8c1a917340ce9234a3498f0ab1d0fb8
dmvict <dm.vict.kr@gmail.com>
2022-06-09 10:57:17 +0300

    Add sample `impls_index_trivial_sample`

---
+++ sample/rust/impls_index_trivial_sample/src/main.rs
@@ -0,0 +1,22 @@
+
+fn main()
+{
+  use ::impls_index::*;
+
+  impls1!
+  {
+    fn f1() -> i32
+    {
+      println!( "f1() : 13" );
+      13
+    }
+  }
+
+  index!
+  {
+    f1,
+  }
+  assert_eq!( f1(), 13 );
+  /* print : f1() : 13 */
+}
+
