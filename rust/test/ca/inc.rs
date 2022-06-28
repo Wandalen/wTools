2404e43266ae1019e2ed74161330940bd15c6227
dmvict <dm.vict.kr@gmail.com>
2022-06-28 08:05:38 +0300

    Fix sample `wca_trivial_sample`, compile sample under feature `use_std`

--- rust/test/ca/inc.rs
+++ rust/test/ca/inc.rs
@@ -1,5 +1,8 @@
+#[ allow( unused_imports ) ]
 use test_tools::*;
+#[ allow( unused_imports ) ]
 use wtools::meta::prelude::*;
+#[ allow( unused_imports ) ]
 use std::collections::HashMap;
 
 #[ cfg( feature = "use_std" ) ]
