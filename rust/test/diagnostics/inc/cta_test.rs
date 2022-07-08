9104e5c8000f627460ecd0be71f70c8eb8285070
dmvict <dm.vict.kr@gmail.com>
2022-07-08 13:19:40 +0300

    Extend coverage of module `diagnostics_tools`, add negative tests

--- rust/test/diagnostics/inc/cta_test.rs
+++ rust/test/diagnostics/inc/cta_test.rs
@@ -4,24 +4,32 @@ use super::*;
 #[ allow( unused_imports ) ]
 use TheModule::prelude::*;
 
-// qqq : do negative testing
+// qqq : do negative testing /* aaa : Dmytro : done */
 
 tests_impls!
 {
-
   #[ cfg( any( feature = "compiletime_assertions", feature = "diagnostics_compiletime_assertions" ) ) ]
-  #[ test ]
-  fn cta_true_test()
+  fn cta_true_pass()
   {
+    // test.case( "check feature, true" );
     cta_true!( any( feature = "compiletime_assertions", feature = "diagnostics_compiletime_assertions" ) );
     // xxx : try ( 1 + 2 == 3 )
   }
 
+  #[ cfg( any( feature = "compiletime_assertions", feature = "diagnostics_compiletime_assertions" ) ) ]
+  #[ test_tools::rustversion::nightly ]
+  fn cta_trybuild_tests()
+  {
+    use test_tools::dependencies::trybuild;
+    let t = trybuild::TestCases::new();
+    t.compile_fail( "../../../rust/test/diagnostics/inc/cta_true_fail.rs" );
+  }
 }
 
 //
 
 tests_index!
 {
-  cta_true_test,
+  cta_true_pass,
+  cta_trybuild_tests,
 }
--- rust/test/diagnostics/inc/layout_test.rs
+++ rust/test/diagnostics/inc/layout_test.rs
@@ -1,32 +1,27 @@
 #[ allow( unused_imports ) ]
 use super::*;
-// use test_tools::*;
 #[ allow( unused_imports ) ]
 use TheModule::prelude::*;
 
-// qqq : do negative testing
+// qqq : do negative testing /* aaa : Dmytro : done */
 // xxx : continue here
 
 tests_impls!
 {
-
   #[ cfg( any( feature = "compiletime_assertions", feature = "diagnostics_compiletime_assertions" ) ) ]
-  #[ test ]
-  fn cta_type_same_size_test()
+  fn cta_type_same_size_pass()
   {
-
     struct Int( i16 );
     let got = cta_type_same_size!( Int, i16 );
     assert!( got );
     // cta_type_same_size!( Int, i32 );
-
   }
 
-  #[ cfg( any( feature = "compiletime_assertions", feature = "diagnostics_compiletime_assertions" ) ) ]
-  #[ test ]
-  fn cta_type_same_align_test()
-  {
+  //
 
+  #[ cfg( any( feature = "compiletime_assertions", feature = "diagnostics_compiletime_assertions" ) ) ]
+  fn cta_type_same_align_pass()
+  {
     struct Int1( i16 );
     #[ repr( align( 128 ) )]
     struct Int2( i16 );
@@ -34,14 +29,11 @@ tests_impls!
     assert!( got );
     // cta_type_same_align!( Int1, Int2 );
     // cta_type_same_align!( Int1, i32 );
-
   }
 
   #[ cfg( any( feature = "compiletime_assertions", feature = "diagnostics_compiletime_assertions" ) ) ]
-  #[ test ]
-  fn cta_ptr_same_size_test()
+  fn cta_ptr_same_size_pass()
   {
-
     struct Int( i16 );
     let ins1 = Int( 31 );
     let ins2 = 13_i16;
@@ -52,14 +44,11 @@ tests_impls!
     let got = cta_ptr_same_size!( &ins1, &31_i16 );
     assert!( got );
     // cta_ptr_same_size!( &ins1, &13_i32 );
-
   }
 
   #[ cfg( any( feature = "compiletime_assertions", feature = "diagnostics_compiletime_assertions" ) ) ]
-  #[ test ]
-  fn cta_mem_same_size_test()
+  fn cta_mem_same_size_pass()
   {
-
     struct Int( i16 );
     let ins1 = Int( 31 );
     let ins2 = 13_i16;
@@ -70,17 +59,28 @@ tests_impls!
     let got = cta_mem_same_size!( ins1, 31_i16 );
     assert!( got );
     // cta_mem_same_size!( ins1, 13_i32 );
-
   }
 
+  #[ cfg( any( feature = "compiletime_assertions", feature = "diagnostics_compiletime_assertions" ) ) ]
+  #[ test_tools::rustversion::nightly ]
+  fn cta_trybuild_tests()
+  {
+    use test_tools::dependencies::trybuild;
+    let t = trybuild::TestCases::new();
+    t.compile_fail( "../../../rust/test/diagnostics/inc/cta_type_same_size_fail.rs" );
+    t.compile_fail( "../../../rust/test/diagnostics/inc/cta_type_same_align_fail.rs" );
+    t.compile_fail( "../../../rust/test/diagnostics/inc/cta_ptr_same_size_fail.rs" );
+    t.compile_fail( "../../../rust/test/diagnostics/inc/cta_mem_same_size_fail.rs" );
+  }
 }
 
 //
 
 tests_index!
 {
-  cta_type_same_size_test,
-  cta_type_same_align_test,
-  cta_ptr_same_size_test,
-  cta_mem_same_size_test,
+  cta_type_same_size_pass,
+  cta_type_same_align_pass,
+  cta_ptr_same_size_pass,
+  cta_mem_same_size_pass,
+  cta_trybuild_tests,
 }
