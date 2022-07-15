13effbdad6f5e4cdc328bbeeaa8dcca8dfad4500
dmvict <dm.vict.kr@gmail.com>
2022-07-15 09:59:15 +0300

    Implement routine `workspace_publish` and add command `.workspace.publish`

--- rust/impl/publisher/commands/init.rs
+++ rust/impl/publisher/commands/init.rs
@@ -17,6 +17,17 @@ pub fn commands_form() -> std::collections::HashMap< String, wca::command::Comma
   .routine( &super::publish::publish )
   .form();
 
+  let workspace_publish_command = wca::CommandOptions::default()
+  .hint( "Publish packages from workspace on `crates.io`." )
+  .long_hint( "Publish packages from workspace on `crates.io`." )
+  .phrase( "workspace.publish" )
+  .subject_hint( "A path to manifest path with workspace. Should be a directory with file `Cargo.toml`." )
+  .property_hint( "dry", "Run command dry. Default is false." )
+  .property_hint( "verbosity", "Setup level of verbosity." )
+  .property_alias( "verbosity", "v" )
+  .routine( &super::publish::workspace_publish )
+  .form();
+
   let list_command = wca::CommandOptions::default()
   .hint( "List packages." )
   .long_hint( "List packages" )
@@ -28,6 +39,7 @@ pub fn commands_form() -> std::collections::HashMap< String, wca::command::Comma
   let ca_map = std::collections::HashMap::from
   ([
     ( ".publish".to_string(), publish_command ),
+    ( ".workspace.publish".to_string(), workspace_publish_command ),
     ( ".list".to_string(), list_command ),
   ]);
 
--- rust/impl/publisher/commands/publish.rs
+++ rust/impl/publisher/commands/publish.rs
@@ -7,8 +7,17 @@ use std::fs;
 use core::fmt::Write;
 use std::path::PathBuf;
 use toml_edit::value;
-use cargo_metadata::MetadataCommand;
 use wtools::error::BasicError;
+use cargo_metadata::
+{
+  DependencyKind,
+  Metadata,
+  MetadataCommand,
+  Package,
+};
+use petgraph::graph::Graph;
+use petgraph::algo::toposort;
+use std::collections::HashMap;
 
 ///
 /// Publish package.
@@ -25,77 +34,15 @@ pub fn publish( instruction : &instruction::Instruction ) -> Result< (), BasicEr
     paths.push( PathBuf::from( &instruction.subject ) );
   }
 
+  let dry = match instruction.properties_map.get( "dry" )
+  {
+    Some( x ) => x.clone().primitive().unwrap().to_bool_like(),
+    None => BoolLike::False,
+  };
+
   for path in paths
   {
-    let mut manifest = manifest_get( path ).unwrap();
-    if !manifest.package_is() || manifest.local_is()
-    {
-      continue;
-    }
-    let data = manifest.manifest_data.as_deref_mut().unwrap();
-
-    let mut package_dir = manifest.manifest_path.clone();
-    package_dir.pop();
-
-    let output = process::start_sync( "cargo package", &package_dir ).unwrap();
-    process::log_output( &output );
-
-    let name = &data[ "package" ][ "name" ].clone();
-    let name = name.as_str().unwrap();
-    let version = &data[ "package" ][ "version" ].clone();
-    let version = version.as_str().unwrap();
-    let local_package_path = local_package_path_get( name, version, &manifest.manifest_path );
-
-    let local_package = fs::read( &local_package_path ).unwrap();
-    let remote_package = http::retrieve_bytes( name, version ).unwrap_or_default();
-
-    let digest_of_local = digest::hash( &local_package );
-    let digest_of_remote = digest::hash( &remote_package );
-
-    if digest_of_local != digest_of_remote
-    {
-      data[ "package" ][ "version" ] = bump( version ).unwrap();
-      let version = &data[ "package" ][ "version" ].clone();
-      let version = version.as_str().unwrap();
-      manifest.store().unwrap();
-
-      let dry = match instruction.properties_map.get( "dry" )
-      {
-        Some( x ) => x.clone().primitive().unwrap().to_bool_like(),
-        None => BoolLike::False,
-      };
-
-      if dry == BoolLike::True
-      {
-        let mut buf = String::new();
-        write!( &mut buf, "git commit --dry-run -am \"{} v{}\"", name, version ).unwrap();
-        let output = process::start_sync( &buf, &current_path ).unwrap();
-        process::log_output( &output );
-
-        let output = process::start_sync( "git push --dry-run", &current_path ).unwrap();
-        process::log_output( &output );
-
-        let output = process::start_sync( "cargo publish --dry-run --allow-dirty", &package_dir ).unwrap();
-        process::log_output( &output );
-      }
-      else
-      {
-        let mut buf = String::new();
-        write!( &mut buf, "git commit -am \"{} v{}\"", name, version ).unwrap();
-        let output = process::start_sync( &buf, &current_path ).unwrap();
-        process::log_output( &output );
-
-        let output = process::start_sync( "git push", &current_path ).unwrap();
-        process::log_output( &output );
-
-        let output = process::start_sync( "cargo publish", &package_dir ).unwrap();
-        process::log_output( &output );
-      }
-    }
-    else
-    {
-      println!( "Package {} is up to date", name );
-    }
+    package_publish( &current_path, &path, &dry )?;
   }
 
   Ok( () )
@@ -109,7 +56,7 @@ fn manifest_get( path : impl Into<PathBuf> ) -> anyhow::Result<manifest::Manifes
   Ok( manifest )
 }
 
-fn local_package_path_get<'a>( name : &'a str, version : &'a str, manifest_path : &'a std::path::PathBuf ) -> std::path::PathBuf
+fn local_package_path_get<'a>( name : &'a str, version : &'a str, manifest_path : &'a PathBuf ) -> PathBuf
 {
   let mut buf = String::new();
   write!( &mut buf, "package/{0}-{1}.crate", name, version ).unwrap();
@@ -117,7 +64,7 @@ fn local_package_path_get<'a>( name : &'a str, version : &'a str, manifest_path 
   .manifest_path( manifest_path )
   .exec()
   .unwrap();
-  let mut local_package_path = std::path::PathBuf::new();
+  let mut local_package_path = PathBuf::new();
   local_package_path.push( package_metadata.target_directory );
   local_package_path.push( buf );
   local_package_path
@@ -130,4 +77,157 @@ fn bump( version : &str ) -> anyhow::Result<toml_edit::Item>
   let v = &patch_version.to_string();
   splits[ 2 ] = v;
   Ok( value( splits.join( "." ) ) )
+}
+
+fn package_publish( current_path : &PathBuf, path : &PathBuf, dry : &BoolLike ) -> Result< (), BasicError >
+{
+  let mut manifest = manifest_get( path ).unwrap();
+  if !manifest.package_is() || manifest.local_is()
+  {
+    return Ok( () );
+  }
+  let data = manifest.manifest_data.as_deref_mut().unwrap();
+
+  let mut package_dir = manifest.manifest_path.clone();
+  package_dir.pop();
+
+  let output = process::start_sync( "cargo package", &package_dir ).unwrap();
+  process::log_output( &output );
+
+  let name = &data[ "package" ][ "name" ].clone();
+  let name = name.as_str().unwrap();
+  let version = &data[ "package" ][ "version" ].clone();
+  let version = version.as_str().unwrap();
+  let local_package_path = local_package_path_get( name, version, &manifest.manifest_path );
+
+  let local_package = fs::read( &local_package_path ).unwrap();
+  let remote_package = http::retrieve_bytes( name, version ).unwrap_or_default();
+
+  let digest_of_local = digest::hash( &local_package );
+  let digest_of_remote = digest::hash( &remote_package );
+
+  if digest_of_local != digest_of_remote
+  {
+    data[ "package" ][ "version" ] = bump( version ).unwrap();
+    let version = &data[ "package" ][ "version" ].clone();
+    let version = version.as_str().unwrap();
+    manifest.store().unwrap();
+
+    if dry == &BoolLike::True
+    {
+      let mut buf = String::new();
+      write!( &mut buf, "git commit --dry-run -am \"{} v{}\"", name, version ).unwrap();
+      let output = process::start_sync( &buf, &current_path ).unwrap();
+      process::log_output( &output );
+
+      let output = process::start_sync( "git push --dry-run", &current_path ).unwrap();
+      process::log_output( &output );
+
+      let output = process::start_sync( "cargo publish --dry-run --allow-dirty", &package_dir ).unwrap();
+      process::log_output( &output );
+    }
+    else
+    {
+      let mut buf = String::new();
+      write!( &mut buf, "git commit -am \"{} v{}\"", name, version ).unwrap();
+      let output = process::start_sync( &buf, &current_path ).unwrap();
+      process::log_output( &output );
+
+      let output = process::start_sync( "git push", &current_path ).unwrap();
+      process::log_output( &output );
+
+      let output = process::start_sync( "cargo publish", &package_dir ).unwrap();
+      process::log_output( &output );
+    }
+  }
+  else
+  {
+    println!( "Package {} is up to date", name );
+  }
+
+  Ok( () )
+}
+
+///
+/// Publish packages from workspace.
+///
+
+pub fn workspace_publish( instruction : &instruction::Instruction ) -> Result< (), BasicError >
+{
+  let current_path = env::current_dir().unwrap();
+
+  let mut manifest = manifest::Manifest::new();
+  let manifest_path = manifest.manifest_path_from_str( &instruction.subject ).unwrap();
+  let package_metadata = MetadataCommand::new()
+  .manifest_path( &manifest_path )
+  .no_deps()
+  .exec()
+  .unwrap();
+
+  let packages_map = packages_filter( &package_metadata );
+  let sorted = toposort_local_packages( &packages_map );
+
+  let dry = match instruction.properties_map.get( "dry" )
+  {
+    Some( x ) => x.clone().primitive().unwrap().to_bool_like(),
+    None => BoolLike::False,
+  };
+
+  for name in sorted.iter()
+  {
+    package_publish( &current_path, &packages_map[ name ].manifest_path.clone().into(), &dry )?;
+  }
+
+  Ok( () )
+}
+
+fn packages_filter( metadata : &Metadata ) -> HashMap< String, &Package >
+{
+  let mut packages_map = HashMap::new();
+  let _packages = metadata.packages.iter().filter( | package |
+  {
+    if package.publish.is_none()
+    {
+      packages_map.insert( package.name.clone(), *package );
+      return true;
+    }
+    false
+  }).collect::< Vec< _ > >();
+  packages_map
+}
+
+fn toposort_local_packages( packages : &HashMap< String, &Package > ) -> Vec< String >
+{
+  let mut deps = Graph::< &str, &str >::new();
+  let _update_graph = packages.iter().map( | ( _name, package ) |
+  {
+    let root_node = if let Some( node ) = deps.node_indices().find( | i | deps[ *i ] == &package.name )
+    {
+      node
+    }
+    else
+    {
+      deps.add_node( &package.name )
+    };
+    for dep in &package.dependencies
+    {
+      if dep.path.is_some() && dep.kind != DependencyKind::Development
+      {
+        let dep_node = if let Some( node ) = deps.node_indices().find( | i | deps[ *i ] == &dep.name )
+        {
+          node
+        }
+        else
+        {
+          deps.add_node( &dep.name )
+        };
+
+        deps.add_edge( root_node, dep_node, &package.name );
+      }
+    }
+  }).collect::< Vec< _ > >();
+
+  let sorted = toposort( &deps, None ).unwrap();
+  let names = sorted.iter().rev().map( | dep_idx | deps.node_weight( *dep_idx ).unwrap().to_string() ).collect::< Vec< String > >();
+  names
 }
