d213a086d16ee6c97eee5e5d2307db353771b80b
wandalen <wandalen.me@gmail.com>
2022-07-17 16:51:50 +0300

    publish meta_tools

--- rust/impl/meta/meta.rs
+++ rust/impl/meta/meta.rs
@@ -11,23 +11,22 @@ mod_interface::mod_interface!
 
   #[ cfg( feature = "impls_index" ) ]
   use ::impls_index;
-
+  #[ cfg( feature = "for_each" ) ]
+  use ::for_each;
   #[ cfg( feature = "mod_interface" ) ]
   use ::mod_interface;
   #[ cfg( feature = "mod_interface" ) ]
   prelude use ::mod_interface::mod_interface;
 
-  #[ cfg( feature = "for_each" ) ]
-  use ::for_each;
   #[ cfg( any( feature = "options", feature = "meta_options" ) ) ]
   use ::woptions;
-  #[ cfg( any( feature = "options", feature = "meta_options" ) ) ]
-  prelude use ::woptions as options;
+  // #[ cfg( any( feature = "options", feature = "meta_options" ) ) ]
+  // prelude use ::woptions as options;
 
   #[ cfg( any( feature = "former", feature = "meta_former" ) ) ]
   use ::former;
-  #[ cfg( any( feature = "former", feature = "meta_former" ) ) ]
-  prelude use ::former as former;
+  // #[ cfg( any( feature = "former", feature = "meta_former" ) ) ]
+  // prelude use ::former as former;
 
   #[ cfg( feature = "collection_make" ) ]
   prelude use ::literally::*;
