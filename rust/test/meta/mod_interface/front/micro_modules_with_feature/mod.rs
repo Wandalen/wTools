a2a256593f12e58cddf68408cb6312902960d682
dmvict <dm.vict.kr@gmail.com>
2022-07-18 11:56:18 +0300

    Update test for `mod_interface`, add debug output and comment test suite

--- rust/test/meta/mod_interface/front/micro_modules_with_feature/mod.rs
+++ rust/test/meta/mod_interface/front/micro_modules_with_feature/mod.rs
@@ -8,10 +8,33 @@ mod private
 
 TheModule::mod_interface!
 {
+  #![ debug ]
   /// mod_protected
   #[ cfg( feature = "use_std" ) ]
   protected mod mod_protected;
 }
+
+// #[doc = " mod_protected"]
+// #[cfg(feature = "use_std")]
+// pub mod mod_protected ;
+//
+// #[doc(inline)]
+// pub use protected :: * ;
+//
+// #[doc = r" Protected namespace of the module."]
+// pub mod protected
+// {
+//     #[doc(inline)]
+//     pub use super :: orphan :: * ;
+//     #[doc(inline)]
+//     pub use super::mod_protected ;
+// }
+// #[doc = r" Orphan namespace of the module."] pub mod orphan
+// { #[doc(inline)] pub use super :: exposed :: * ; }
+// #[doc = r" Exposed namespace of the module."] pub mod exposed
+// { #[doc(inline)] pub use super :: prelude :: * ; }
+// #[doc = r" Prelude to use essentials: `use my_module::prelude::*`."] pub mod
+// prelude {}
 
 //
 
--- rust/test/meta/mod_interface/only_test/micro_modules_with_feature_only_test.rs
+++ rust/test/meta/mod_interface/only_test/micro_modules_with_feature_only_test.rs
@@ -1,24 +1,24 @@
-#[ cfg( feature = "use_std" ) ]
-tests_impls!
-{
-  fn basic()
-  {
-    {
-      /* test module */
-      a_id!( mod_protected::has_protected(), true );
-    }
-
-    {
-      /* test namespace */
-      a_id!( protected::mod_protected::has_protected(), true );
-    }
-  }
-}
-
+// #[ cfg( feature = "use_std" ) ]
+// tests_impls!
+// {
+//   fn basic()
+//   {
+//     {
+//       /* test module */
+//       a_id!( mod_protected::has_protected(), true );
+//     }
 //
-
-#[ cfg( feature = "use_std" ) ]
-tests_index!
-{
-  basic,
-}
+//     {
+//       /* test namespace */
+//       a_id!( protected::mod_protected::has_protected(), true );
+//     }
+//   }
+// }
+//
+// //
+//
+// #[ cfg( feature = "use_std" ) ]
+// tests_index!
+// {
+//   basic,
+// }
