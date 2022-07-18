6199fa0c8335b7c2760c24acb3d1d054617bfcab
dmvict <dm.vict.kr@gmail.com>
2022-07-18 09:46:06 +0300

    Update module `meta_tools_min`, inline doc of reexports

--- rust/impl/meta/meta_min.rs
+++ rust/impl/meta/meta_min.rs
@@ -9,33 +9,43 @@ pub( crate ) mod private
 /// Protected namespace of the module.
 pub mod protected
 {
+  #[ doc( inline ) ]
   pub use super::orphan::*;
   #[ cfg( feature = "impls_index" ) ]
+  #[ doc( inline ) ]
   pub use ::impls_index::orphan::*;
   #[ cfg( feature = "mod_interface" ) ]
+  #[ doc( inline ) ]
   pub use ::mod_interface::orphan::*;
   #[ cfg( feature = "for_each" ) ]
+  #[ doc( inline ) ]
   pub use ::for_each::orphan::*;
 }
 
+#[ doc( inline ) ]
 pub use protected::*;
 
 /// Shared with parent namespace of the module
 pub mod orphan
 {
+  #[ doc( inline ) ]
   pub use super::exposed::*;
 }
 
 /// Exposed namespace of the module.
 pub mod exposed
 {
+  #[ doc( inline ) ]
   pub use super::prelude::*;
 
   #[ cfg( feature = "impls_index" ) ]
+  #[ doc( inline ) ]
   pub use ::impls_index::exposed::*;
   #[ cfg( feature = "mod_interface" ) ]
+  #[ doc( inline ) ]
   pub use ::mod_interface::exposed::*;
   #[ cfg( feature = "for_each" ) ]
+  #[ doc( inline ) ]
   pub use ::for_each::exposed::*;
 }
 
@@ -43,11 +53,15 @@ pub mod exposed
 pub mod prelude
 {
   #[ cfg( feature = "impls_index" ) ]
+  #[ doc( inline ) ]
   pub use ::impls_index::prelude::*;
   #[ cfg( feature = "mod_interface" ) ]
+  #[ doc( inline ) ]
   pub use ::mod_interface::prelude::*;
   #[ cfg( feature = "for_each" ) ]
+  #[ doc( inline ) ]
   pub use ::for_each::prelude::*;
   #[ cfg( feature = "collection_make" ) ]
+  #[ doc( inline ) ]
   pub use ::literally::*;
 }
