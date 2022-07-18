4c46b04e17716aa9adaeedcc819ecc6e2bbd9fe4
dmvict <dm.vict.kr@gmail.com>
2022-07-18 09:52:41 +0300

    Update module `time_tools`, inline doc of reexports

--- rust/impl/time/time.rs
+++ rust/impl/time/time.rs
@@ -12,22 +12,27 @@ pub mod dependency
 /// Protected namespace of the module.
 pub mod protected
 {
+  #[ doc( inline ) ]
   pub use super::orphan::*;
 }
 
+#[ doc( inline ) ]
 pub use protected::*;
 
 /// Shared with parent namespace of the module
 pub mod orphan
 {
+  #[ doc( inline ) ]
   pub use super::exposed::*;
 }
 
 /// Exposed namespace of the module.
 pub mod exposed
 {
+  #[ doc( inline ) ]
   pub use super::prelude::*;
   #[ cfg( feature = "now" ) ]
+  #[ doc( inline ) ]
   pub use super::now::*;
 }
 
