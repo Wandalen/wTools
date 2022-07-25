4991707c87bde23557f1c34d8d9f28826da8c26f
dmvict <dm.vict.kr@gmail.com>
2022-07-25 13:45:02 +0300

    Write test routine `basic_with_images_rgb`

--- rust/test/video/inc/encoder_strategy_test.rs
+++ rust/test/video/inc/encoder_strategy_test.rs
@@ -2,7 +2,6 @@ use super::*;
 
 //
 
-
 fn animation_write( encoder : &mut super::encoder_strategy::Encoder ) -> Result< (), Box< dyn std::error::Error > >
 {
   let mut buf = [ 255u8; 30_000 ];
@@ -25,8 +24,23 @@ fn animation_write( encoder : &mut super::encoder_strategy::Encoder ) -> Result<
     encoder.encode( &buf )?;
   }
 
-  encoder.flush()?;
-  Ok( () )
+  encoder.flush()
+}
+
+//
+
+fn animation_write_from_img_rgb( encoder : &mut super::encoder_strategy::Encoder ) -> Result< (), Box< dyn std::error::Error > >
+{
+
+  for i in 1..4
+  {
+    let path = std::path::PathBuf::from( format!( "./rust/test/video/_asset/img/rust_logo{}.png", i ) );
+    let rgb_image = image::open( path )?.into_rgb8();
+    let bytes = rgb_image.as_raw();
+    encoder.encode( &bytes )?;
+  }
+
+  encoder.flush()
 }
 
 //
@@ -68,6 +82,27 @@ tests_impls!
 
     Ok( () )
   }
+
+  //
+
+  fn basic_with_images_rgb() -> Result< (), Box< dyn std::error::Error > >
+  {
+    let mut encoder_gif = super::encoder_strategy::Encoder::new( EncoderType::Gif, 512, 512, 1, None, ColorType::Rgb, "../../../target/image.gif" )?;
+    let mut encoder_png = super::encoder_strategy::Encoder::new( EncoderType::Png, 512, 512, 1, None, ColorType::Rgb, "../../../target/image.png" )?;
+    let mut encoder_mp4 = super::encoder_strategy::Encoder::new( EncoderType::Mp4, 512, 512, 1, None, ColorType::Rgb, "../../../target/image.mp4" )?;
+    animation_write_from_img_rgb( &mut encoder_gif )?;
+    animation_write_from_img_rgb( &mut encoder_png )?;
+    animation_write_from_img_rgb( &mut encoder_mp4 )?;
+
+    let mut path = std::path::PathBuf::from( "../../../target/image.gif" );
+    a_id!( path.exists(), true );
+    path.set_extension( "png" );
+    a_id!( path.exists(), true );
+    path.set_extension( "mp4" );
+    a_id!( path.exists(), true );
+
+    Ok( () )
+  }
 }
 
 //
@@ -76,4 +111,5 @@ tests_index!
 {
   basic,
   basic_with_change,
+  basic_with_images_rgb,
 }
