05b8913b2ce68cbcd75b71a5b8d33d4fd6f21c20
f01dab1e <f01dab1e@protonmail.com>
2022-08-03 12:57:47 +0300

    docs and fmt

--- rust/impl/mem/mem.rs
+++ rust/impl/mem/mem.rs
@@ -3,19 +3,25 @@ pub( crate ) mod private
 {
   // use crate::protected::*;
 
-  /// TODO
+  ///
+  /// Are two pointers points on the same data.
+  ///
+  /// Does not require arguments to have the same type.
+  ///
+
   pub fn same_data< T1 : ?Sized, T2 : ?Sized >( src1 : &T1, src2 : &T2 ) -> bool
   {
-    extern "C" { fn memcmp(s1: *const u8, s2: *const u8, n: usize) -> i32; }
+    extern "C" { fn memcmp( s1 : *const u8, s2 : *const u8, n : usize ) -> i32; }
 
     let mem1 = src1 as *const _ as *const u8;
     let mem2 = src2 as *const _ as *const u8;
 
-    if same_size( src1, src2 ) {
-      unsafe { memcmp( mem1, mem2, core::mem::size_of_val( src1 ) ) == 0 }
-    } else {
-        false
+    if !same_size( src1, src2 )
+    {
+      return false;
     }
+
+    unsafe { memcmp( mem1, mem2, core::mem::size_of_val( src1 ) ) == 0 }
   }
 
   /* zzz : qqq : implement mem::same_data, comparing data. discuss */
