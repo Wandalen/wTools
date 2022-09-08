cc7ebed7121bc1d0540d22f49f5fc4431dd98bcd
Serhii Chushyn <chushin.sergey@gmail.com>
2022-09-08 13:11:31 +0300

    Fix formatting

--- rust/test/publisher/_asset/package/src/lib.rs
+++ rust/test/publisher/_asset/package/src/lib.rs
@@ -1,8 +1,10 @@
-#[cfg(test)]
-mod tests {
-  #[test]
-  fn it_works() {
+# [ cfg ( test ) ]
+mod tests
+{
+  #[ test ]
+  fn it_works()
+  {
     let result = 2 + 2;
-    assert_eq!(result, 4);
+    assert_eq!( result, 4 );
   }
 }
