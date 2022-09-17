b815e7e654a9ee332ca4ba87958424e9c6626482
Barsik <bogdan.balushkin5@gmail.com>
2022-09-17 14:25:50 +0300

    Added RGBA to RGB convertion for gif::basic_rgb test

--- rust/test/video/inc/gif_test.rs
+++ rust/test/video/inc/gif_test.rs
@@ -1,5 +1,17 @@
 use super::*;
 
+fn rgba_to_rgb( rgba_buf : Vec< u8 > ) -> Vec< u8 >
+{
+  let mut result = vec![ 0; rgba_buf.len() * 3 / 4];
+  let mut i = 0;
+  for chunk in rgba_buf.chunks(4)
+  {
+      result[ i..i+3 ].copy_from_slice( &chunk[ 0..3 ] );
+      i += 3;
+  }
+  result
+}
+
 tests_impls!
 {
   fn basic_rgb() -> Result< (), Box< dyn std::error::Error > >
@@ -35,13 +47,14 @@ tests_impls!
     reader.next_frame_info()?;
     let mut bytes = vec![ 0; reader.buffer_size() ];
     reader.read_into_buffer( &mut bytes )?;
+    bytes = rgba_to_rgb( bytes );
 
     a_id!( reader.width(), 100 );
     a_id!( reader.height(), 100 );
 
     // first frame
     a_id!( [ 0, 0, 0 ], bytes[ ..3 ] );
-    // assert_eq!( [ 255u8; 30_000 - 3 ], bytes[ 3.. ] );
+    assert_eq!( [ 255u8; 30_000 - 3 ], bytes[ 3.. ] );
 
     // all frames valid
     for _ in 1..100
@@ -49,9 +62,11 @@ tests_impls!
       assert!( reader.next_frame_info().is_ok() );
     }
 
-    // // last frame
+    // last frame
+    let mut bytes = vec![ 0; reader.buffer_size() ];
     reader.read_into_buffer( &mut bytes )?;
-    // assert_eq!( buf, bytes.as_slice() );
+    bytes = rgba_to_rgb( bytes );
+    assert_eq!( buf, bytes.as_slice() );
     Ok( () )
   }
 
