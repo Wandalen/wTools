633fe080d479c3da17cb7bc5dd5a117f78aba6df
Barsik <bogdan.balushkin5@gmail.com>
2022-10-03 14:59:19 +0000

    Refactoring, impls clone_as_tuple, clone_as_array

--- rust/impl/dt/type_constructor/derive_pair/src/lib.rs
+++ rust/impl/dt/type_constructor/derive_pair/src/lib.rs
@@ -1,11 +1,11 @@
 use proc_macro2::Ident;
-use quote::quote;
+use quote::{ quote, ToTokens };
 use syn::{ parse_macro_input, Fields, ItemStruct, Field, TypeParam };
 
 struct DerivePair
 {
   ident : Ident,
-  generics : ( Option< TypeParam >, Option< TypeParam > ),
+  generics : Vec< TypeParam >,
   fields : ( Field, Field ),
 }
 
@@ -14,7 +14,7 @@ impl DerivePair
   fn parse( input: ItemStruct ) -> Self
   {
     let ident = &input.ident;
-    let generics = &mut input.generics.type_params().cloned();
+    let generics =  input.generics.type_params().cloned().collect::< Vec< _ > >() ;
     let mut fields  = match &input.fields
     {
       Fields::Unnamed( fields ) =>
@@ -26,34 +26,31 @@ impl DerivePair
     Self
     {
       ident : ident.to_owned(),
-      generics : ( generics.next(), generics.next() ),
+      generics,
       fields : ( fields.next().unwrap(), fields.next().unwrap() ),
     }
   }
 
-  fn impl_from_slice( &self ) -> proc_macro2::TokenStream
+  fn impl_from_tuple( &self ) -> proc_macro2::TokenStream
   {
     let struct_name = &self.ident;
-    let generic1 = &self.generics.0.as_ref().map( | g | quote!( #g, ) );
-    let generic2 = &self.generics.1.as_ref().map( | g | quote!( #g, ) );
-    let gtype1 = &self.generics.0.as_ref().map( | g |
-    { let gi = g.ident.to_owned(); quote!( #gi, ) } );
-    let gtype2 = &self.generics.1.as_ref().map( | g |
-    { let gi = g.ident.to_owned(); quote!( #gi, ) } );
+    let generics = &self.generics;
+    let gtypes = &self.generics.iter()
+    .map( | g | g.ident.to_owned() ).collect::< Vec< _ > >();
     let param1 = &self.fields.0.ty;
     let param2 = &self.fields.1.ty;
     quote!
     (
       impl
       <
-        #generic1 #generic2
+        #( #generics, )*
         Into1 : Into< #param1 >, Into2 : Into< #param2 >
       >
       From
       <(
        Into1, Into2
       )>
-      for #struct_name< #gtype1 #gtype2 >
+      for #struct_name< #( #gtypes ),* >
       {
         #[ inline ]
         fn from( src : ( Into1, Into2 ) ) -> Self
@@ -64,27 +61,109 @@ impl DerivePair
     )
   }
 
-  fn impl_to_slice( &self ) -> proc_macro2::TokenStream
+  fn impl_to_tuple( &self ) -> proc_macro2::TokenStream
   {
     let struct_name = &self.ident;
-    let generic1 = &self.generics.0.as_ref().map( | g | quote!( #g, ) );
-    let generic2 = &self.generics.1.as_ref().map( | g | quote!( #g, ) );
-    let gtype1 = &self.generics.0.as_ref().map( | g |
-    { let gi = g.ident.to_owned(); quote!( #gi, ) } );
-    let gtype2 = &self.generics.1.as_ref().map( | g |
-    { let gi = g.ident.to_owned(); quote!( #gi, ) } );
+    let generics = &self.generics;
+    let gtypes = &self.generics.iter()
+    .map( | g | g.ident.to_owned() ).collect::< Vec< _ > >();
     let param1 = &self.fields.0.ty;
     let param2 = &self.fields.1.ty;
     quote!
     (
-      impl< #generic1 #generic2 >
-      From  < #struct_name< #gtype1 #gtype2 > >
+      impl< #( #generics ),* >
+      From  < #struct_name< #( #gtypes ),* > >
       for ( #param1, #param2 )
       {
         #[ inline ]
-        fn from( src : #struct_name< #gtype1 #gtype2 > ) -> Self
+        fn from( src : #struct_name< #( #gtypes ),* > ) -> Self
         {
           ( src.0, src.1 )
+        }
+      }
+    )
+  }
+
+  fn impl_from_slice( &self ) -> proc_macro2::TokenStream
+  {
+    quote!()
+  }
+
+  fn impl_to_slice( &self ) -> proc_macro2::TokenStream
+  {
+    quote!()
+  }
+
+  fn impl_as_tuple( &self ) -> proc_macro2::TokenStream
+  {
+    quote!()
+  }
+
+  fn impl_as_array( &self ) -> proc_macro2::TokenStream
+  {
+    quote!()
+  }
+
+  fn impl_as_slice( &self ) -> proc_macro2::TokenStream
+  {
+    quote!()
+  }
+
+  fn impl_clone_as_tuple( &self ) -> proc_macro2::TokenStream
+  {
+    let struct_name = &self.ident;
+    let generics = &self.generics;
+    let gtypes = &self.generics.iter()
+    .map( | g | g.ident.to_owned() ).collect::< Vec< _ > >();
+    let param1 = &self.fields.0.ty;
+    let param2 = &self.fields.1.ty;
+
+    let where_clauses = &gtypes.iter().map( | gt |
+    {
+      quote!( #gt : Clone )
+    })
+    .collect::< Vec< _ > >();
+    quote!
+    (
+      impl< #( #generics ),* >
+      CloneAsTuple<( #param1, #param2 )>
+      for #struct_name< #( #gtypes ),* >
+      where
+        #( #where_clauses ),*
+      {
+        #[ inline ]
+        fn clone_as_tuple( &self ) -> ( #param1, #param2 )
+        {
+          ( self.0.clone(), self.1.clone() )
+        }
+      }
+    )
+  }
+
+  fn impl_clone_as_array( &self ) -> proc_macro2::TokenStream
+  {
+    let struct_name = &self.ident;
+    let generics = &self.generics;
+    let gtypes = &self.generics.iter()
+    .map( | g | g.ident.to_owned() ).collect::< Vec< _ > >();
+    let param1 = &self.fields.0.ty;
+    let where_clauses = &gtypes.iter().map( | gt |
+    {
+      quote!( #gt : Clone )
+    })
+    .collect::< Vec< _ > >();
+    quote!
+    (
+      impl< #( #generics ),* >
+      CloneAsArray< #param1, 2 >
+      for #struct_name< #( #gtypes ),* >
+      where
+        #( #where_clauses ),*
+      {
+        #[ inline ]
+        fn clone_as_array( &self ) -> [ #param1; 2 ]
+        {
+          [ self.0.clone(), self.1.clone() ]
         }
       }
     )
@@ -97,11 +176,23 @@ pub fn derive_make( input: proc_macro::TokenStream ) -> proc_macro::TokenStream
   let input = parse_macro_input!( input as syn::ItemStruct );
   let dp = DerivePair::parse( input );
 
-  let impls =
+  let mut impls =
+  vec!
   [
-    dp.impl_from_slice(),
-    dp.impl_to_slice(),
+    dp.impl_from_tuple(),
+    dp.impl_to_tuple(),
+    dp.impl_clone_as_tuple(),
   ];
+
+  // if two fields has the same types => it can be stored into array/slice/...
+  let type1_as_string = &dp.fields.0.ty.clone().into_token_stream().to_string();
+  let type2_as_string = &dp.fields.1.ty.clone().into_token_stream().to_string();
+  if type1_as_string == type2_as_string
+  {
+    impls.push( dp.impl_from_slice() );
+    impls.push( dp.impl_to_slice() );
+    impls.push( dp.impl_clone_as_array() );
+  }
   let result = impls.iter().fold( quote!(), | mut result, i |
   {
     result = quote!( #result #i );
