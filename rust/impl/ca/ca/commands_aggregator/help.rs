652b4781171188cd6ed7aa6bbd8951fd1ce3ad98
SupperZum <levchenko.d25@gmail.com>
2023-04-14 20:03:48 +0300

    ads md generator

--- rust/impl/ca/ca/commands_aggregator/help.rs
+++ rust/impl/ca/ca/commands_aggregator/help.rs
@@ -12,6 +12,73 @@ pub( crate ) mod private
 
   use std::rc::Rc;
 
+
+  use std::fs::File;
+
+  use std::io::prelude::*;
+
+
+  fn md_file_generator( grammar : &GrammarConverter )
+  {
+    let text = grammar.commands
+    .iter()
+    .sorted_by_key( |( name, _ )| *name )
+    .map( |( name, cmd )|
+    {
+      cmd.iter().fold( String::new(), | _, cmd |
+      {
+        let subjects = cmd.subjects.iter().fold( String::new(), | _, _ | format!( " `[Subject]`" ) );
+        let properties = if cmd.properties.is_empty() { " " } else { " `[properties]` " };
+
+
+        format!( "[.{name}{subjects}{properties}](#{}{}{})", name.replace( '.', "" ), if cmd.subjects.is_empty() { "" } else { "-subject" }, if cmd.properties.is_empty() { "" } else { "-properties" } )
+      })
+    })
+    .fold( String::new(), | acc, cmd |
+    {
+      format!( "{acc}\n- {cmd}" )
+    });
+
+    let list_of_commands = format!( "# Commands\n\n{}", text );
+
+    let about_each_command = grammar.commands
+    .iter()
+    .sorted_by_key( |( name, _ )| *name )
+    .map( |( name, cmd )|
+    {
+      cmd.iter().fold( String::new(), | _, cmd |
+      {
+        let subjects = cmd.subjects.iter().fold( String::new(), | _, _ | format!( " `[Subject]`" ) );
+        let properties = if cmd.properties.is_empty() { " " } else { " `[properties]` " };
+        let hint = if cmd.hint.is_empty() { &cmd.long_hint } else { &cmd.hint };
+
+        let heading = format!( "# .{name}{subjects}{properties}\n__{}__\n", hint );
+
+        let hint = if cmd.long_hint.is_empty() { &cmd.hint } else { &cmd.long_hint };
+        let full_subjects = cmd.subjects.iter().enumerate().map( |( number, subj )| format!( "\n- subject_{number} - {} `[{:?}]`", subj.hint, subj.kind ) ).join( "\n" );
+        let full_properties = cmd.properties.iter().sorted_by_key( |( name, _ )| *name ).map( |( name, value )| format!( "\n- {name} - {} `[{:?}]`", value.hint, value.kind ) ).join( "\n" );
+
+        format!
+        ( 
+          "{heading}\n{hint}{}{}", 
+          if cmd.subjects.is_empty() { "".to_string() } else { format!( "\n\nSubjects:{}", &full_subjects ) }, 
+          if cmd.properties.is_empty() { "".to_string() } else { format!( "\n\nProperties:{}",&full_properties ) } 
+        )
+
+      })
+    })
+    .fold( String::new(), | acc, cmd |
+    {
+      format!( "{acc}\n\n{cmd}" )
+    });
+
+    let mut file = File::create( "./Readme.md" ).unwrap();
+    file.write_all( format!( "{list_of_commands}\n{about_each_command}" ).as_bytes() ).unwrap();
+
+
+  }
+
+
   fn generate_help_content( grammar : &GrammarConverter, command : Option< &Command > ) -> String
   {
     if let Some( command ) = command
@@ -89,6 +156,7 @@ pub( crate ) mod private
       let help = Command::former()
       .hint( "prints information about existing commands" )
       .phrase( &phrase )
+      .property( "format", "readme file generate format", Type::String )
       .form();
 
       let command_variants = grammar.commands.entry( phrase.to_owned() ).or_insert_with( Vec::new );
@@ -98,6 +166,7 @@ pub( crate ) mod private
       // replace old help command with new one
       let subject_help = executor.routines.remove( &phrase );
       let text = helper.exec( grammar, None );
+      let grammar_for_md = grammar.clone();
       let routine = Routine::new
       (
         move |( args, props )|
@@ -105,7 +174,15 @@ pub( crate ) mod private
           match &subject_help
           {
             Some( Routine::WithoutContext( help ) ) if !args.is_empty() => help(( args, props ))?,
-            _ => println!( "Help command\n{text}" ),
+            _ => 
+            {
+              let format : String = props.get_owned( "format" ).unwrap_or_default();
+              if format == "md"
+              {
+                md_file_generator( &grammar_for_md );
+              }
+              println!( "Help command\n{text}" );
+            }
           }
 
           Ok( () )
