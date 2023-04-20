5593e3fa3a1d1f08902af56229a499ffffd34bb2
Barsik-sus <bogdan.balushkin5@gmail.com>
2023-04-20 17:27:51 +0300

    mod.rs comments

--- rust/impl/ca/ca/commands_aggregator/mod.rs
+++ rust/impl/ca/ca/commands_aggregator/mod.rs
@@ -1,7 +1,7 @@
 crate::mod_interface!
 {
-  /// -
+  /// Responsible for aggregating all commands that the user defines, and for parsing and executing them
   layer aggregator;
-  /// -
+  /// Helper commands
   layer help;
 }
--- rust/impl/ca/ca/executor/mod.rs
+++ rust/impl/ca/ca/executor/mod.rs
@@ -1,11 +1,11 @@
 crate::mod_interface!
 {
-  /// -
+  /// Executor that is responsible for executing the program’s commands
   layer executor;
-  /// -
+  /// All needed for `ExecutableCommand`
   layer execute;
-  /// -
+  /// Represents the state of the program's runtime
   layer runtime;
-  /// -
+  /// Converts from `GrammarCommand` to `ExecutableCommand`
   layer converter;
 }
