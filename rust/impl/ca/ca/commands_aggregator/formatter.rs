74658fd84e711a1070a6242f1f2ad4d98fb3ba14
SupperZum <levchenko.d25@gmail.com>
2023-04-27 11:32:53 +0300

    update md, and update .help

--- rust/impl/ca/ca/commands_aggregator/formatter.rs
+++ rust/impl/ca/ca/commands_aggregator/formatter.rs
@@ -34,7 +34,7 @@ pub( crate ) mod private
       format!( "{acc}\n- {cmd}" )
     });
 
-    let list_of_commands = format!( "# Commands\n\n{}", text );
+    let list_of_commands = format!( "## Commands\n\n{}", text );
 
     let about_each_command = grammar.commands
     .iter()
@@ -47,7 +47,7 @@ pub( crate ) mod private
         let properties = if cmd.properties.is_empty() { " " } else { " `[properties]` " };
         let hint = if cmd.hint.is_empty() { &cmd.long_hint } else { &cmd.hint };
 
-        let heading = format!( "# .{name}{subjects}{properties}\n__{}__\n", hint );
+        let heading = format!( "## .{name}{subjects}{properties}\n__{}__\n", hint );
 
         let hint = if cmd.long_hint.is_empty() { &cmd.hint } else { &cmd.long_hint };
         let full_subjects = cmd.subjects.iter().enumerate().map( |( number, subj )| format!( "\n- {}subject_{number} - {} `[{:?}]`", if subj.optional { "`<optional>` " } else { "" }, subj.hint, subj.kind ) ).join( "\n" );
@@ -55,7 +55,7 @@ pub( crate ) mod private
 
         format!
         ( 
-          "{heading}\n{}{}{hint}\n", 
+          "{heading}\n{}{}\n\n{hint}\n", 
           if cmd.subjects.is_empty() { "".to_string() } else { format!( "\n\nSubjects:{}", &full_subjects ) }, 
           if cmd.properties.is_empty() { "".to_string() } else { format!( "\n\nProperties:{}",&full_properties ) }
         )
