4538fcb03f88e4481ee641b609e442c7371cc2fb
f01dab1e <andriy.n@obox.systems>
2023-07-04 11:15:02 +0000

    add code

--- rust/impl/ca/ca/stdx.rs
+++ rust/impl/ca/ca/stdx.rs
@@ -1,6 +1,16 @@
 pub( crate ) mod private
 {
+  /// A type alias for `miette::Result<T, E>`.
+  pub type Result< T = (), E = miette::Report > = miette::Result< T, E >;
 
+  /// Creates a command-line interface (CLI) builder with the given initial state.
+  ///
+  /// This function initializes a `CommandBuilder` with the provided `state` and
+  /// returns it for further configuration of the CLI.
+  pub fn cli< T >( state: T ) -> CommandBuilder< T, 0 > 
+  {
+    CommandBuilder::with_state( state )
+  }
 }
 
 crate::mod_interface!
