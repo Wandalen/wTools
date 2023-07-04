c53f11d1d15e50006fba01856acd21444afa9340
f01dab1e <andriy.n@obox.systems>
2023-07-04 10:32:33 +0000

    asd

---
+++ rust/impl/ca/ca/stdx.rs
@@ -0,0 +1,14 @@
+pub( crate ) mod private
+{
+
+}
+
+crate::mod_interface!
+{
+  /// Macro for parsing WCA arguments.
+  layer macros;
+  /// This is a module for defining traits.
+  layer traits;
+  /// The `ir` module provides functionality for intermediate representation.
+  layer ir;
+}
