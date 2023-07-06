3c13daf2576af8550bc2daf50b6a2a16b00361d0
f01dab1e <andriy.n@obox.systems>
2023-07-06 06:11:45 -0400

    update sample

--- sample/rust/wca_stdx_trivial_sample/src/main.rs
+++ sample/rust/wca_stdx_trivial_sample/src/main.rs
@@ -1,6 +1,19 @@
-fn main() {
+use wca::prelude::*;
+
+fn echo( () : (), args : Args, _ : Props ) -> Result< (), () > 
+{
+  let mut args = args.0.into_iter();
+  wca::parse_args!( args, value: String );
+
+  println!( "{value}" );
+
+  Ok( () )
+}
+
+fn main() 
+{
   let args = std::env::args().skip( 1 ).collect::< Vec< _ > >().join( " " );
 
-  let aggregator = wca::stdx::cli( (  ) ).build();
+  let aggregator = wca::stdx::cli( () ).command( echo.arg( "string", Type::String) ).build();
   aggregator.perform( args ).unwrap();
 }
