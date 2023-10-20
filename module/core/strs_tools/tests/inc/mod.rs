fc75dea3813972837ece01949f9160d82666d3be
Sakapoi <yuhnon@mail.ru>
2023-10-20 09:06:13 +0300

    changed mod files

--- module/core/strs_tools/tests/inc/mod.rs
+++ module/core/strs_tools/tests/inc/mod.rs
@@ -1,6 +1,16 @@
-#[ cfg( feature = "string" ) ]
-use super::*;
-use crate::TheModule::string as TheModule;
+// #[ cfg( feature = "string" ) ]
+// use super::*;
+// use crate::TheModule::string as TheModule;
 
-#[ cfg( feature = "string" ) ]
-mod inc;
+// #[ cfg( feature = "string" ) ]
+// mod inc;
+
+use super::*;
+
+pub mod indentation_test;
+pub mod isolate_test;
+pub mod number_test;
+pub mod parse_test;
+pub mod split_test;
+pub mod wstring_tools_tests;
+pub mod inc;
--- module/core/time_tools/src/lib.rs
+++ module/core/time_tools/src/lib.rs
@@ -46,7 +46,7 @@
 // pub use time::*;
 
 /// Collection of time tools.
-pub mod time;
+// pub mod time;
 
 /// Dependencies.
 pub mod dependency
@@ -75,13 +75,80 @@ pub mod exposed
 {
   #[ doc( inline ) ]
   pub use super::prelude::*;
-  #[ doc( inline ) ]
-  pub use super::time::time::exposed::*;
+  // #[ doc( inline ) ]
+  // pub use super::time::time::exposed::*;
 }
 
 /// Prelude to use essentials: `use my_module::prelude::*`.
 pub mod prelude
 {
-  #[ doc( inline ) ]
-  pub use super::time::time::prelude::*;
+  // #[ doc( inline ) ]
+  // pub use super::time::time::prelude::*;
+}
+
+use std::time;
+
+///
+/// Get current time. Units are milliseconds.
+///
+
+pub fn now() -> i64
+{
+  time::SystemTime::now()
+  .duration_since( time::UNIX_EPOCH ).unwrap()
+  .as_millis() as i64
+}
+
+///
+/// Default units are seconds.
+///
+
+pub mod s
+{
+  use super::*;
+
+  /// Get current time. Units are seconds.
+  pub fn now() -> i64
+  {
+    time::SystemTime::now()
+    .duration_since( time::UNIX_EPOCH ).unwrap()
+    .as_secs() as i64
+  }
+}
+
+///
+/// Default units are milliseconds.
+///
+
+pub mod ms
+{
+  use super::*;
+
+  /// Get current time. Units are milliseconds.
+  pub fn now() -> i64
+  {
+    time::SystemTime::now()
+    .duration_since( time::UNIX_EPOCH ).unwrap()
+    .as_millis() as i64
+  }
+}
+
+// xxx : qqq for Dima : problem. ms should not be part of `wtools::ms`, something is wrong. fix it, please
+/* aaa : Dmytro : all routines and modules is inside wtools and wtools::time, added test suite to test it */
+
+///
+/// Default units are nanoseconds.
+///
+
+pub mod ns
+{
+  use super::*;
+
+  /// Get current time. Units are nanoseconds.
+  pub fn now() -> i64
+  {
+    time::SystemTime::now()
+    .duration_since( time::UNIX_EPOCH ).unwrap()
+    .as_nanos() as i64
+  }
 }
