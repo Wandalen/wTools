608cf083cc354fe1288eac59285c1a838b7f3ae3
SRetip <shvayko.p.y@gmail.com>
2023-10-27 13:57:25 +0300

    feat: refactor InnerFrom

--- module/core/derive_tools_meta/src/implementation/inner_from.rs
+++ module/core/derive_tools_meta/src/implementation/inner_from.rs
@@ -12,77 +12,69 @@ pub fn inner_from( input : proc_macro::TokenStream ) -> Result< proc_macro2::Tok
   let field_types = parsed.field_types()?;
   let field_names = parsed.field_names()?;
   let item_name = parsed.item_name;
-  let result;
+  let result = 
   match (field_types.len(), field_names) {
-      (1, Some(field_names)) => {
-        let field_name = field_names.get(0).unwrap();
-        let field_type = field_types.get(0).unwrap();
-        result = qt!
-        {
-          #[ automatically_derived ]
-          impl From< #item_name > for #field_type
-          {
-            #[ inline( always ) ]
-            fn from( src : #item_name ) -> Self
-            {
-              src.#field_name
-            }
-          }
-        };
-      }
-      (1, None) => {
-        let field_type = field_types.get(0).unwrap();
-        result = qt!
-        {
-          #[ automatically_derived ]
-          impl From< #item_name > for #field_type
-          {
-            #[ inline( always ) ]
-            fn from( src : #item_name ) -> Self
-            {
-              src.0
-            }
-          }
-        };
-      }
-      (_, Some(field_names)) => {
-        let mut params: Vec<TokenStream> = vec![];
-        for field_name in field_names.iter(){
-          params.push(qt!{ src.#field_name });
-        }
-        result = qt!
-        {
-          #[ automatically_derived ]
-          impl From< #item_name > for (# (#field_types), *)
-          {
-            #[ inline( always ) ]
-            fn from( src : #item_name ) -> Self
-            {
-              (# (#params), *)
-            }
-          }
-        };
-      }
-      (n, None) => {
-        let mut params: Vec<TokenStream> = vec![];
-        for index in 0..n{
-          let index: TokenStream = index.to_string().parse()?;
-          params.push(qt!{ src.#index });
-        }
-        result = qt!
-        {
-          #[ automatically_derived ]
-          impl From< #item_name > for (# (#field_types), *)
-          {
-            #[ inline( always ) ]
-            fn from( src : #item_name ) -> Self
-            {
-              (# (#params), *)
-            }
-          }
-        };
-      }
+    (1, Some(field_names)) => {
+      let field_name = field_names.get(0).unwrap();
+      let field_type = field_types.get(0).unwrap();
+      generate_from_impl(item_name, field_type, field_name)
+  }
+  (1, None) => {
+      let field_type = field_types.get(0).unwrap();
+      generate_from_impl_no_field_names(item_name, field_type)
+  }
+  (_, Some(field_names)) => {
+      let params: Vec<TokenStream> = field_names.iter()
+          .map(|field_name| qt! { src.#field_name })
+          .collect();
+      generate_from_impl_multiple_fields(item_name, &field_types, &params)
+  }
+  (n, None) => {
+      let params: Vec<TokenStream> = (0..n)
+          .map(|index| {
+              let index: TokenStream = index.to_string().parse().unwrap();
+              qt! { src.#index }
+          })
+          .collect();
+      generate_from_impl_multiple_fields(item_name, &field_types, &params)
+  }
   };
 
   Ok( result )
+}
+
+fn generate_from_impl(item_name: syn::Ident, field_type: &syn::Type, field_name: &syn::Ident) -> TokenStream {
+  qt! {
+      #[automatically_derived]
+      impl From<#item_name> for #field_type {
+          #[inline(always)]
+          fn from(src: #item_name) -> Self {
+              src.#field_name
+          }
+      }
+  }
+}
+
+fn generate_from_impl_no_field_names(item_name: syn::Ident, field_type: &syn::Type) -> TokenStream {
+  qt! {
+      #[automatically_derived]
+      impl From<#item_name> for #field_type {
+          #[inline(always)]
+          fn from(src: #item_name) -> Self {
+              src.0
+          }
+      }
+  }
+}
+
+fn generate_from_impl_multiple_fields(item_name: syn::Ident, field_types: &Vec<syn::Type>, params: &Vec<TokenStream>) -> TokenStream {
+  qt! {
+      #[automatically_derived]
+      impl From<#item_name> for (#(#field_types), *) {
+          #[inline(always)]
+          fn from(src: #item_name) -> Self {
+              (#(#params), *)
+          }
+      }
+  }
 }
