3e96f03a8268ede1dff73b04d7a04ed5d17836d0
wandalen <wandalen.me@gmail.com>
2023-10-28 07:15:40 +0300

    fixing modules

--- module/core/typing_tools/tests/inc/mod.rs
+++ module/core/typing_tools/tests/inc/mod.rs
@@ -4,13 +4,11 @@ use super::*;
 #[ allow( unused_imports ) ]
 use TheModule::typing as TheModule;
 
-// #[ path = "../../../../core/implements/tests/inc/mod.rs" ]
-// mod implements_test;
+#[ path = "../../../../core/implements/tests/inc/mod.rs" ]
+mod implements_test;
 
 #[ path = "../../../../core/inspect_type/tests/inc/mod.rs" ]
 mod inspect_type_test;
 
-// #[ path = "../../../../core/is_slice/tests/inc/mod.rs" ]
-// mod is_slice_test;
-
-// xxx2
+#[ path = "../../../../core/is_slice/tests/inc/mod.rs" ]
+mod is_slice_test;
