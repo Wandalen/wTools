f4ca5006de9fa701c2b41345b0f658083c42c7be
Barsik <bogdan.balushkin5@gmail.com>
2023-11-16 15:41:09 +0200

    refactor: split tests

--- module/move/willbe/tests/inc/endpoints/list.rs
+++ module/move/willbe/tests/inc/endpoints/list.rs
@@ -1,18 +1,17 @@
 const ASSETS_PATH : &str = "tests/assets";
 
-use cargo_metadata::MetadataCommand;
 use assert_fs::prelude::*;
-use test_tools::{ tests_impls, tests_index };
 use crate::TheModule::endpoint::{ self, list::* };
 
 //
 
-tests_impls!
+// a -> b -> c
+mod chain_of_three_packages
 {
-  // a -> b -> c
-  fn chain_of_three_packages_format()
+  use super::*;
+
+  fn arrange() -> assert_fs::TempDir
   {
-    // Arrange
     let root_path = std::path::Path::new( env!( "CARGO_MANIFEST_DIR" ) );
     let assets_relative_path = std::path::Path::new( ASSETS_PATH );
     let assets_path = root_path.join( assets_relative_path );
@@ -20,9 +19,14 @@ tests_impls!
     let temp = assert_fs::TempDir::new().unwrap();
     temp.copy_from( assets_path.join( "chain_of_packages" ), &[ "**" ] ).unwrap();
 
-    let metadata = MetadataCommand::new().no_deps().current_dir( temp.as_ref() ).exec().unwrap();
+    temp
+  }
 
-    // Tree format, without filters
+  #[ test ]
+  fn tree_format_for_single_package()
+  {
+    // Arrange
+    let temp = arrange();
 
     // Act
     let output = endpoint::list( temp.join( "a" ), ListFormat::Tree, ListFilter::Nothing ).unwrap();
@@ -34,18 +38,28 @@ tests_impls!
     assert_eq!( 2, graph.edge_count() );
     assert_eq!( 1, names.len() );
     assert_eq!( Some( "a" ), graph.node_weight( names[ 0 ] ).map( | x | x.as_str() ) );
+  }
 
-    // List format, without filters
+  #[ test ]
+  fn list_format_for_single_package()
+  {
+    // Arrange
+    let temp = arrange();
 
     // Act
     let output = endpoint::list( temp.join( "a" ), ListFormat::Topological, ListFilter::Nothing ).unwrap();
 
     // Assert
-    let ListReport::List( names ) = &output else { panic!( "Expected `Topological` format, but found another" ) };
+    let ListReport::List( names ) = &output else { panic!("Expected `Topological` format, but found another") };
 
     assert_eq!( &[ "c".to_string(), "b".to_string(), "a".to_string() ], names.as_slice() );
+  }
 
-    // List format, without filters, full workspace
+  #[ test ]
+  fn list_format_for_whole_workspace()
+  {
+    // Arrange
+    let temp = arrange();
 
     // Act
     let output = endpoint::list( temp.to_path_buf(), ListFormat::Topological, ListFilter::Nothing ).unwrap();
@@ -55,11 +69,15 @@ tests_impls!
 
     assert_eq!( &[ "c".to_string(), "b".to_string(), "a".to_string() ], names.as_slice() );
   }
+}
 
-  // a -> ( remote, b )
-  fn package_with_remote_dependency_format()
+// a -> ( remote, b )
+mod package_with_remote_dependency
+{
+  use super::*;
+
+  fn arrange() -> assert_fs::TempDir
   {
-    // Arrange
     let root_path = std::path::Path::new( env!( "CARGO_MANIFEST_DIR" ) );
     let assets_relative_path = std::path::Path::new( ASSETS_PATH );
     let assets_path = root_path.join( assets_relative_path );
@@ -67,9 +85,14 @@ tests_impls!
     let temp = assert_fs::TempDir::new().unwrap();
     temp.copy_from( assets_path.join( "package_with_remote_dependency" ), &[ "**" ] ).unwrap();
 
-    let metadata = MetadataCommand::new().no_deps().current_dir( temp.as_ref() ).exec().unwrap();
+    temp
+  }
 
-    // Tree format, without filters
+  #[ test ]
+  fn tree_format_for_single_package()
+  {
+    // Arrange
+    let temp = arrange();
 
     // Act
     let output = endpoint::list( temp.join( "a" ), ListFormat::Tree, ListFilter::Nothing ).unwrap();
@@ -81,8 +104,13 @@ tests_impls!
     assert_eq!( 2, graph.edge_count() );
     assert_eq!( 1, names.len() );
     assert_eq!( Some( "a" ), graph.node_weight( names[ 0 ] ).map( | x | x.as_str() ) );
+  }
 
-    // List format, without filters
+  #[ test ]
+  fn list_format_for_single_package()
+  {
+    // Arrange
+    let temp = arrange();
 
     // Act
     let output = endpoint::list( temp.join( "a" ), ListFormat::Topological, ListFilter::Nothing ).unwrap();
@@ -97,20 +125,11 @@ tests_impls!
     assert!( ( "b" == &names[ 0 ] && "foo" == &names[ 1 ] ) || ( "b" == &names[ 1 ] && "foo" == &names[ 0 ] ) );
   }
 
-  // a -> ( remote, b )
-  fn package_with_remote_dependency_filter()
+  #[ test ]
+  fn only_local_dependency_filter()
   {
     // Arrange
-    let root_path = std::path::Path::new( env!( "CARGO_MANIFEST_DIR" ) );
-    let assets_relative_path = std::path::Path::new( ASSETS_PATH );
-    let assets_path = root_path.join( assets_relative_path );
-
-    let temp = assert_fs::TempDir::new().unwrap();
-    temp.copy_from( assets_path.join( "package_with_remote_dependency" ), &[ "**" ] ).unwrap();
-
-    let metadata = MetadataCommand::new().no_deps().current_dir( temp.as_ref() ).exec().unwrap();
-
-    // List format, with local only filter
+    let temp = arrange();
 
     // Act
     let output = endpoint::list( temp.join( "a" ), ListFormat::Topological, ListFilter::Local ).unwrap();
@@ -120,9 +139,15 @@ tests_impls!
 
     assert_eq!( &[ "b".to_string(), "a".to_string() ], names.as_slice() );
   }
+}
 
-  // a -> b -> a
-  fn workspace_with_cyclic_dependency_format()
+// a -> b -> a
+mod workspace_with_cyclic_dependency
+{
+  use super::*;
+
+  #[ test ]
+  fn can_not_show_list_with_cyclic_dependencies()
   {
     // Arrange
     let root_path = std::path::Path::new( env!( "CARGO_MANIFEST_DIR" ) );
@@ -132,8 +157,6 @@ tests_impls!
     let temp = assert_fs::TempDir::new().unwrap();
     temp.copy_from( assets_path.join( "workspace_with_cyclic_dependency" ), &[ "**" ] ).unwrap();
 
-    let metadata = MetadataCommand::new().no_deps().current_dir( temp.as_ref() ).exec().unwrap();
-
     // Act
     let output = endpoint::list( temp.join( "a" ), ListFormat::Tree, ListFilter::Nothing );
 
@@ -142,14 +165,4 @@ tests_impls!
     // can not process topological sorting for cyclic dependencies
     assert!( output.is_err() );
   }
-}
-
-//
-
-tests_index!
-{
-  chain_of_three_packages_format,
-  package_with_remote_dependency_format,
-  package_with_remote_dependency_filter,
-  workspace_with_cyclic_dependency_format,
 }
