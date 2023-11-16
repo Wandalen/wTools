a637b8a7f3b2a7cd4f930b31f1d980c4146a8455
Barsik <bogdan.balushkin5@gmail.com>
2023-11-17 01:31:33 +0200

    feat: update `bump` function report to store more information

--- module/move/willbe/src/version.rs
+++ module/move/willbe/src/version.rs
@@ -1,11 +1,46 @@
 /// Internal namespace.
 mod private
 {
+  use std::fmt::Formatter;
   use std::path::Path;
   use toml_edit::value;
   use crate::manifest;
   use crate::process::CmdReport;
   use crate::wtools::error::Result;
+
+  /// Bump report.
+  #[ derive( Debug, Clone ) ]
+  pub struct BumpReport
+  {
+    /// Bumped package name.
+    pub package_name : Option< String >,
+    /// Old package version.
+    pub old_version : Option< String >,
+    /// New package version.
+    pub new_version : Option< String >,
+    /// Actual result.
+    pub report : CmdReport,
+  }
+
+  impl BumpReport
+  {
+    fn new( report : CmdReport ) -> Self
+    {
+      BumpReport { package_name : None, old_version : None, new_version : None, report }
+    }
+  }
+
+  impl std::fmt::Display for BumpReport
+  {
+    fn fmt( &self, f : &mut Formatter< '_ >) -> std::fmt::Result
+    {
+      f
+      .debug_struct( "BumpReport" )
+      .field( "stdout", &self.report.out )
+      .field( "stderr", &self.report.err )
+      .finish()
+    }
+  }
 
   /// Bump version as a string.
   pub fn bump_from_str( version : &str ) -> Result< String >
@@ -19,38 +54,39 @@ mod private
   }
 
   /// Bump package version by manifest path.
-  pub fn bump< P >( path : P, dry : bool ) -> Result< CmdReport >
+  pub fn bump< P >( path : P, dry : bool ) -> Result< BumpReport >
   where
     P : AsRef< Path >,
   {
     let mut manifest = manifest::get( path.as_ref() )?;
-    let mut report = CmdReport
+    let internal_report = CmdReport
     {
       command : "bump".to_string(),
       path : path.as_ref().to_path_buf(),
       out : String::new(),
       err : String::new(),
     };
+    let mut report = BumpReport::new( internal_report );
     let ( name, version ) =
     {
       let data = manifest.manifest_data.as_ref().unwrap();
-      if manifest.package_is()
+      if !manifest.package_is()
       {
-        report.err = format!( "`{}` - not a package", manifest.manifest_path.display() );
+        report.report.err = format!( "`{}` - not a package", manifest.manifest_path.display() );
 
         return Ok( report );
       }
       let package = data.get( "package" ).unwrap();
 
-      let name = package.get( "name" ).unwrap().to_string();
+      let name = package.get( "name" ).unwrap().as_str().unwrap().to_string();
       let version = package.get( "version" );
       if version.is_none()
       {
-        report.err = format!( "`{}` - can not read the version", manifest.manifest_path.display() );
+        report.report.err = format!( "`{}` - can not read the version", manifest.manifest_path.display() );
 
         return Ok( report );
       }
-      let version = version.unwrap().to_string();
+      let version = version.unwrap().as_str().unwrap().to_string();
 
       ( name, version )
     };
@@ -60,11 +96,14 @@ mod private
     if !dry
     {
       let manifest = manifest.manifest_data.as_mut().unwrap();
-      manifest[ "package" ][ "version" ] = value( new_version );
+      manifest[ "package" ][ "version" ] = value( &new_version );
     }
 
-    report.out = format!( "`{name}` bumped from `{version}` to `{new_version}`" );
+    report.report.out = format!( "`{name}` bumped from `{version}` to `{new_version}`" );
 
+    report.package_name = Some( name );
+    report.old_version = Some( version );
+    report.new_version = Some( new_version );
 
     Ok( report )
   }
@@ -74,6 +113,8 @@ mod private
 
 crate::mod_interface!
 {
+  /// Bump report.
+  protected use BumpReport;
   /// Bump version.
   protected use bump;
 }
