9f57b79a3b85aa9db22f089566ecd7bcf9610b4b
SRetip <shvayko.p.y@gmail.com>
2023-11-17 18:09:55 +0200

    ready

--- module/move/willbe/src/endpoint/table.rs
+++ module/move/willbe/src/endpoint/table.rs
@@ -12,23 +12,19 @@ mod private
     SeekFrom 
   };
   use std::io::Write;
-  use cargo_metadata::
-  {
-    MetadataCommand,
-    Package
-  };
+  use cargo_metadata::{Dependency, DependencyKind, MetadataCommand, Package};
   use wca::wtools::Itertools;
   use convert_case::Case;
   use convert_case::Casing;
-  use std::fs::
-  { 
-    OpenOptions
-  };
+  use std::fs::OpenOptions;
+  use std::path::Path;
+  use std::str::FromStr;
 
   use error_tools::for_app::
   {
     Result,
     anyhow,
+    bail,
   };
   use crate::package::functions;
   use crate::package::functions::FilterMapOptions;
@@ -52,8 +48,9 @@ mod private
   /// Anything between the opening and closing tag will be destroyed.
   pub fn table_create() -> Result< () >
   {
-    let workspace_root = workspace_root()?;
-    let read_me_path = workspace_root.join("Readme.md");
+    let cargo_metadata = MetadataCommand::new().no_deps().exec()?;
+    let workspace_root = workspace_root( &cargo_metadata )?;
+    let read_me_path = readme_path(&workspace_root).ok_or_else( || anyhow!( "Fail to find README.md" ) )?;
     let mut file = OpenOptions::new()
     .read( true )
     .write( true )
@@ -63,36 +60,39 @@ mod private
 
     file.read_to_end( &mut contents )?;
     let mut buffer = vec![];
-    let open_caps = TAG_TEMPLATE.captures_iter( &*contents );
-    let close_caps = CLOUSE_TAG.captures_iter( &*contents );
     let mut tags_closures = vec![];
     let mut tables = vec![];
-    // iterate by regex matches and generate table content for each dir py taken
-    for (open_captures, close_captures) in open_caps.zip( close_caps )
+    let open_caps = TAG_TEMPLATE.captures_iter( &*contents );
+    let close_caps = CLOUSE_TAG.captures_iter( &*contents );
+    // iterate by regex matches and generate table content for each dir which taken from open-tag
+    for ( open_captures, close_captures ) in open_caps.zip( close_caps )
     {
       for captures in open_captures.iter().zip( close_captures.iter() )
       {
         if let ( Some( open ), Some( close ) ) = captures
         {
-          let module_path =
-            PathBuf::from_str(
-            std::str::from_utf8(
+          let module_path = PathBuf::from_str
+          (
+            std::str::from_utf8
+            (
               TAG_TEMPLATE.captures( open.as_bytes() )
-                .ok_or( anyhow!("Fail to parse tag") )?
-                .get( 1 )
-                .ok_or( anyhow!("Fail to parse group") )?
-                .as_bytes() )? )?;
-          tables.push( table_prepare( directory_names( &module_path )?, module_path.to_string_lossy().into() ) );
-          tags_closures.push( (open.end(), close.start() ) );
+              .ok_or( anyhow!( "Fail to parse tag" ) )?
+              .get( 1 )
+              .ok_or( anyhow!( "Fail to parse group" ) )?
+              .as_bytes()
+            )?
+          )?;
+          tables.push( table_prepare( directory_names( workspace_root.join(module_path.clone() ), &cargo_metadata.packages ), &module_path ) );
+          tags_closures.push( ( open.end(), close.start() ) );
         }
       }
     }
     let mut start: usize = 0;
-    for ( tag_closure, con ) in tags_closures.iter().zip( tables.iter() )
+    for ( ( end_of_start_tag, start_of_end_tag ), con ) in tags_closures.iter().zip( tables.iter() )
     {
-      copy_range_to_target( &*contents, &mut buffer, start, tag_closure.0 )?;
+      copy_range_to_target( &*contents, &mut buffer, start, *end_of_start_tag )?;
       copy_range_to_target( con.as_bytes(), &mut buffer, 0,con.len() - 1 )?;
-      start = tag_closure.1;
+      start = *start_of_end_tag;
     }
     copy_range_to_target( &*contents,&mut buffer,start,contents.len() - 1 )?;
 
@@ -104,27 +104,37 @@ mod private
     Ok( () )
   }
 
-
   fn directory_names( path: PathBuf, packages: &[Package] ) -> Vec< String >
   {
-    let core_filter: Option< Box< dyn Fn( &Package) -> bool > > = Some
+    let path_clone = path.clone();
+    let module_package_filter: Option< Box< dyn Fn( &Package) -> bool > > = Some
     (
       Box::new
       (
-        move | p |
-        p.publish.is_none() && p.manifest_path.starts_with( &path )
+        move | p | {
+          p.publish.is_none() && p.manifest_path.starts_with(&path)
+        }
       )
     );
-    let core_packages_map = functions::packages_filter_map
+    let module_dependency_filter: Option< Box< dyn Fn( &Package, &Dependency) -> bool > > = Some
+      (
+        Box::new
+          (
+            move | _, d | {
+              d.path.is_some() && d.kind != DependencyKind::Development && d.path.as_ref().unwrap().starts_with( &path_clone )
+            }
+          )
+      );
+    let module_packages_map = functions::packages_filter_map
     (
       packages,
-     FilterMapOptions{ package_filter: core_filter, ..Default::default() },
+      FilterMapOptions{ package_filter: module_package_filter, dependency_filter: module_dependency_filter },
     );
-    let core_graph = functions::graph_build( &core_packages_map );
-    functions::toposort( core_graph )
+    let module_graph = functions::graph_build( &module_packages_map);
+    functions::toposort(module_graph)
   }
 
-  fn table_prepare( modules: Vec< String >, dir: String ) -> String
+  fn table_prepare( modules: Vec< String >, dir: &Path ) -> String
   {
     let table_header = "| Module | Stability | Master | Alpha | Docs | Online |\n|--------|-----------|--------|-------|:----:|:------:|";
     let table_content = modules
@@ -133,7 +143,7 @@ mod private
     (
       | ref module_name | 
       {
-        let column_module = format!( "[{}](./{}/{})", &module_name, &dir, &module_name );
+        let column_module = format!( "[{}](./{}/{})", &module_name, &dir.display(), &module_name );
         let column_stability = format!( "[![experimental](https://raster.shields.io/static/v1?label=&message=experimental&color=orange)](https://github.com/emersion/stability-badges#experimental)" );
         let column_master = format!( "[![rust-status](https://img.shields.io/github/actions/workflow/status/Wandalen/wTools/Module{}Push.yml?label=&branch=master)](https://github.com/Wandalen/wTools/actions/workflows/Module{}Push.yml)", &module_name.to_case( Case::Pascal ), &module_name.to_case( Case::Pascal ) );
         let column_alpha = format!( "[![rust-status](https://img.shields.io/github/actions/workflow/status/Wandalen/wTools/Module{}Push.yml?label=&branch=alpha)](https://github.com/Wandalen/wTools/actions/workflows/Module{}Push.yml)", &module_name.to_case( Case::Pascal ), &module_name.to_case( Case::Pascal ) );
@@ -151,7 +161,8 @@ mod private
     Ok( metadata.workspace_root.clone().into_std_path_buf() )
   }
 
-  fn copy_range_to_target<T: Clone>( source: &[T], target: &mut Vec<T>, from: usize, to: usize ) -> Result<()> {
+  fn copy_range_to_target< T: Clone >( source: &[T], target: &mut Vec< T >, from: usize, to: usize ) -> Result< () >
+  {
     if from < source.len() && to < source.len() && from <= to
     {
       target.extend_from_slice( &source[ from..= to ] );
@@ -161,6 +172,55 @@ mod private
     {
       bail!( "Incorrect indexes" )
     }
+  }
+
+  /// Searches for a README file in specific subdirectories of the given directory path.
+  ///
+  /// This function attempts to find a README file in the following subdirectories: ".github",
+  /// the root directory, and "./docs". It returns the path to the first found README file, or
+  /// `None` if no README file is found in any of these locations.
+  fn readme_path( dir_path : &Path ) -> Option< PathBuf >
+  {
+    if let Some( path )  = readme_in_dir_find(&dir_path.join( ".github" ) )
+    {
+      Some( path )
+    }
+    else if let Some( path )  = readme_in_dir_find( dir_path )
+    {
+      Some( path )
+    }
+    else if let Some( path )  = readme_in_dir_find( &dir_path.join( "docs" ) )
+    {
+      Some( path )
+    }
+    else
+    {
+      None
+    }
+  }
+
+
+  /// Searches for a file named "readme.md" in the specified directory path.
+  ///
+  /// Given a directory path, this function searches for a file named "readme.md" in the specified
+  /// directory.
+  fn readme_in_dir_find( path: &Path ) -> Option< PathBuf >
+  {
+    fs::read_dir( path )
+    .ok()?
+    .filter_map( Result::ok )
+    .filter( | p | p.path().is_file() )
+    .filter_map( | f |
+    {
+      let l_f = f.file_name().to_ascii_lowercase();
+      if l_f == "readme.md"
+      {
+        return Some( f.file_name() )
+      }
+        None
+    })
+    .max()
+    .map( PathBuf::from )
   }
 }
 
