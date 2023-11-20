55a752c2478d01bedfbdfed7b74c5b420ce790b6
Barsik <bogdan.balushkin5@gmail.com>
2023-11-20 12:22:34 +0200

    fix: improve description for `git` commands

--- module/move/willbe/src/git.rs
+++ module/move/willbe/src/git.rs
@@ -6,7 +6,17 @@ mod private
   use crate::process::CmdReport;
   use crate::wtools::error::Result;
 
-  /// Git add.
+  /// Adds changes to the Git staging area.
+  ///
+  /// Args:
+  /// - path - the root path
+  /// - objects - a list of objects from the root that will be added
+  /// - dry - a flag that indicates whether to apply the changes or not
+  ///         - true - does not modify git state
+  ///         - false - adds a change in the working directory to the staging area
+  ///
+  /// Returns:
+  /// Returns a result containing a report indicating the result of the operation.
   pub fn add< P, Os, O >( path : P, objects : Os, dry : bool ) -> Result< CmdReport >
   where
     P : AsRef< Path >,
@@ -35,7 +45,18 @@ mod private
     }
   }
 
-  /// Git commit.
+  /// Commits changes to the Git repository.
+  ///
+  /// Args:
+  ///
+  /// - path - the root path
+  /// - message - a commit message describing the changes
+  /// - dry - a flag that indicates whether to apply the changes or not
+  ///   - true - does not modify the Git state
+  ///   - false - commits changes to the repository
+  ///
+  /// Returns:
+  /// Returns a result containing a report indicating the result of the operation.
   pub fn commit< P, M >( path : P, message : M, dry : bool ) -> Result< CmdReport >
   where
     P : AsRef< Path >,
@@ -62,7 +83,17 @@ mod private
     }
   }
 
-  /// Git push.
+  /// Pushes changes to the remote Git repository.
+  ///
+  /// Args:
+  ///
+  /// - path - the root path
+  /// - dry - a flag that indicates whether to apply the changes or not
+  ///   - true - does not modify the Git state
+  ///   - false - pushes changes to the remote repository
+  ///
+  /// Returns:
+  /// Returns a result containing a report indicating the result of the operation.
   pub fn push< P >( path : P, dry : bool ) -> Result< CmdReport >
   where
     P : AsRef< Path >,
