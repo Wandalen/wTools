adbe5571be8e29f54a3ca3dca476b692bccc4b8d
Sakapoi <yuhnon@mail.ru>
2023-11-22 04:11:29 +0200

    enables tests

--- module/core/wtest/tests/inc/mod.rs
+++ module/core/wtest/tests/inc/mod.rs
@@ -1,5 +1,5 @@
 // use super::*;
 
-// #[ path = "../../../../core/test_tools/tests/inc" ]
-// mod test_tools;
+#[ path = "../../../../core/test_tools/tests/inc/mod.rs" ]
+mod test_tools;
 pub mod wtest;
