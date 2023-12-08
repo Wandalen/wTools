108dec3ba078fa81cae58d49189e366430669244
Sakapoi <yuhnon@mail.ru>
2023-12-08 10:54:14 +0200

    fund_data_type: fixed features

--- module/alias/fundamental_data_type/tests/fundamental_data_type_tests.rs
+++ module/alias/fundamental_data_type/tests/fundamental_data_type_tests.rs
@@ -1,2 +1,2 @@
-#[ path="../../../../module/core/type_constructor/tests/data_type_tests.rs" ]
-mod type_constructor;
+// #[ path="../../../../module/core/type_constructor/tests/data_type_tests.rs" ]
+// mod type_constructor;
