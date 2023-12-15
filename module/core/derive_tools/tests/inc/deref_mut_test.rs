a32fc6a82f2250b3c73bb1099cb68f7c6176bf83
Sakapoi <yuhnon@mail.ru>
2023-12-15 06:18:51 +0200

    derive_tools: fixed features

--- module/core/derive_tools/tests/inc/deref_mut_test.rs
+++ module/core/derive_tools/tests/inc/deref_mut_test.rs
@@ -3,7 +3,7 @@ use super::*;
 // use diagnostics_tools::prelude::*;
 // use derives::*;
 
-#[ derive( Debug, Clone, Copy, PartialEq, TheModule::Deref, TheModule::DerefMut ) ]
-pub struct IsTransparent( bool );
+// #[ derive( Debug, Clone, Copy, PartialEq, TheModule::Deref, TheModule::DerefMut ) ]
+// pub struct IsTransparent( bool );
 
-include!( "./only_test/deref_mut.rs" );
+// include!( "./only_test/deref_mut.rs" );
