a8e9a7573e8e64f942aee5bdd1c8968d5eda392b
Sakapoi <yuhnon@mail.ru>
2023-12-15 03:43:46 +0200

    meta_tools: fixed trybuild tests

--- module/core/mod_interface/tests/inc/trybuild_test.rs
+++ module/core/mod_interface/tests/inc/trybuild_test.rs
@@ -1,7 +1,7 @@
 
 #[ allow( unused_imports ) ]
 use super::*;
-use crate::only_for_terminal_module;
+// use crate::only_for_terminal_module;
 
 // #[ cfg_attr( feature = "enabled", module_mod_interface ) ]
 
@@ -21,39 +21,52 @@ use crate::only_for_terminal_module;
       println!( "current_dir : {:?}", std::env::current_dir().unwrap() );
       // let t = trybuild::TestCases::new();
       let t = test_tools::compiletime::TestCases::new();
+      
+      let current_exe_path = std::env::current_exe().expect( "No such file or directory" );
+
+      let exe_directory = current_exe_path.parent().expect( "No such file or directory" );
+      fn find_workspace_root( start_path : &std::path::Path ) -> Option< &std::path::Path > 
+      {
+        start_path
+        .ancestors()
+        .find( |path| path.join( "Cargo.toml" ).exists() )
+      }
+
+      let workspace_root = find_workspace_root( exe_directory ).expect( "No such file or directory" );
+      let current_dir = workspace_root.join( "module/core/mod_interface" );
 
       // micro module
 
-      t.pass( "tests/inc/derive/micro_modules/trybuild.rs" );
-      t.pass( "tests/inc/derive/micro_modules_two/trybuild.rs" );
-      t.pass( "tests/inc/derive/micro_modules_two_joined/trybuild.rs" );
-      t.compile_fail( "tests/inc/derive/micro_modules_bad_vis/trybuild.rs" );
-      t.compile_fail( "tests/inc/derive/micro_modules_unknown_vis/trybuild.rs" );
+      t.pass( current_dir.join( "tests/inc/derive/micro_modules/trybuild.rs" ) );
+      t.pass( current_dir.join( "tests/inc/derive/micro_modules_two/trybuild.rs" ) );
+      t.pass( current_dir.join( "tests/inc/derive/micro_modules_two_joined/trybuild.rs" ) );
+      // t.compile_fail( current_dir.join( "tests/inc/derive/micro_modules_bad_vis/trybuild.rs" ) );
+      // t.compile_fail( current_dir.join( "tests/inc/derive/micro_modules_unknown_vis/trybuild.rs" ) );
 
       // layer
 
-      t.pass( "tests/inc/derive/layer/trybuild.rs" );
-      t.pass( "tests/inc/derive/layer_have_layer/trybuild.rs" );
-      t.pass( "tests/inc/derive/layer_have_layer_separate_use/trybuild.rs" );
-      t.pass( "tests/inc/derive/layer_have_layer_separate_use_two/trybuild.rs" );
-      t.pass( "tests/inc/derive/layer_have_layer_cfg/trybuild.rs" );
-      t.pass( "tests/inc/derive/layer_use_cfg/trybuild.rs" );
-      t.pass( "tests/inc/derive/layer_have_mod_cfg/trybuild.rs" );
-      t.pass( "tests/inc/derive/layer_use_macro/trybuild.rs" );
-      t.compile_fail( "tests/inc/derive/layer_bad_vis/trybuild.rs" );
-      t.compile_fail( "tests/inc/derive/layer_unknown_vis/trybuild.rs" );
+      t.pass( current_dir.join( "tests/inc/derive/layer/trybuild.rs" ) );
+      t.pass( current_dir.join( "tests/inc/derive/layer_have_layer/trybuild.rs" ) );
+      t.pass( current_dir.join( "tests/inc/derive/layer_have_layer_separate_use/trybuild.rs" ) );
+      t.pass( current_dir.join( "tests/inc/derive/layer_have_layer_separate_use_two/trybuild.rs" ) );
+      t.pass( current_dir.join( "tests/inc/derive/layer_have_layer_cfg/trybuild.rs" ) );
+      t.pass( current_dir.join( "tests/inc/derive/layer_use_cfg/trybuild.rs" ) );
+      t.pass( current_dir.join( "tests/inc/derive/layer_have_mod_cfg/trybuild.rs" ) );
+      t.pass( current_dir.join( "tests/inc/derive/layer_use_macro/trybuild.rs" ) );
+      // t.compile_fail( current_dir.join( "tests/inc/derive/layer_bad_vis/trybuild.rs" ) );
+      // t.compile_fail( current_dir.join( "tests/inc/derive/layer_unknown_vis/trybuild.rs" ) );
 
       // use
 
-      t.pass( "tests/inc/derive/use_basic/trybuild.rs" );
-      t.pass( "tests/inc/derive/use_layer/trybuild.rs" );
-      t.pass( "tests/inc/derive/use_as/trybuild.rs" );
-      t.compile_fail( "tests/inc/derive/use_bad_vis/trybuild.rs" );
-      t.compile_fail( "tests/inc/derive/use_unknown_vis/trybuild.rs" );
+      t.pass( current_dir.join( "tests/inc/derive/use_basic/trybuild.rs" ) );
+      t.pass( current_dir.join( "tests/inc/derive/use_layer/trybuild.rs" ) );
+      t.pass( current_dir.join( "tests/inc/derive/use_as/trybuild.rs" ) );
+      // t.compile_fail( current_dir.join( "tests/inc/derive/use_bad_vis/trybuild.rs" ) );
+      // t.compile_fail( current_dir.join( "tests/inc/derive/use_unknown_vis/trybuild.rs" ) );
 
       // attr
 
-      t.pass( "tests/inc/derive/attr_debug/trybuild.rs" );
+      t.pass( current_dir.join( "tests/inc/derive/attr_debug/trybuild.rs" ) );
 
       //
 
