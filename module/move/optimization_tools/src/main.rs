2ac13dbc9ae8b13c87b83f4486e0c6712d976cb2
wandalen <wandalen.me@gmail.com>
2023-12-16 17:25:16 +0200

    optimization_tools : evolve it

--- module/move/optimization_tools/src/main.rs
+++ module/move/optimization_tools/src/main.rs
@@ -1,8 +1,32 @@
 use optimization_tools::*;
 use sudoku::*;
 
+const INPUT : &str = r#"
+024007000
+600000000
+003680415
+431005000
+500000032
+790000060
+209710800
+040093000
+310004750
+"#;
+
 fn main()
 {
-  let board = Board::default();
-  println!( "{board}" );
+  // let board = Board::default();
+  let seed : Seed = "seed1".into();
+  let board = Board::from( INPUT );
+  let initial = optimization::SudokuInitial::new( board, seed );
+
+  let ( reason, generation ) = initial.solve_with_sa();
+
+  log::trace!( "reason : {reason}" );
+  assert!( generation.is_some() );
+  let generation = generation.unwrap();
+  log::trace!( "{generation:#?}" );
+  log::trace!( "{:#?}", generation.person.board );
+
+  // println!( "{board}" );
 }
