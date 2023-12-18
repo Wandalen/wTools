e57b2780fc21f1caec310cbd61eb61728f393a4b
Sakapoi <yuhnon@mail.ru>
2023-12-18 03:22:44 +0200

    fixed deref tests

--- module/core/derive_tools/tests/inc/deref_mut_test.rs
+++ module/core/derive_tools/tests/inc/deref_mut_test.rs
@@ -3,7 +3,21 @@ use super::*;
 // use diagnostics_tools::prelude::*;
 // use derives::*;
 
-// #[ derive( Debug, Clone, Copy, PartialEq, TheModule::Deref, TheModule::DerefMut ) ]
-// pub struct IsTransparent( bool );
+#[ derive( Debug, Clone, Copy, PartialEq ) ]
+pub struct IsTransparent( bool );
 
-// include!( "./only_test/deref_mut.rs" );
+impl std::ops::Deref for IsTransparent {
+    type Target = bool;
+    
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl std::ops::DerefMut for IsTransparent {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.0
+    }
+}
+
+include!( "./only_test/deref_mut.rs" );
