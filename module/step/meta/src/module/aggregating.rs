d8244dacfc9e9b94212395a418d62bc4230cd7f2
Sakapoi <yuhnon@mail.ru>
2023-12-22 06:19:25 +0200

    added macro doc

--- module/step/meta/src/module/aggregating.rs
+++ module/step/meta/src/module/aggregating.rs
@@ -2,6 +2,11 @@
 /// This macro allows including the passed tokens in an aggregating module. 
 /// It does not restrict itself to any specific context and simply inserts 
 /// the provided tokens, making it useful for any code location.
+/// 
+/// This macro is designed for scenarios where certain code should only be available 
+/// within the context of the terminal module. It serves as a marker for code that is 
+/// exclusive to the terminal module and allows for better organization and control of 
+/// functionality specific to this module.
 #[ macro_export ]
 macro_rules! only_for_terminal_module
 {
@@ -13,6 +18,10 @@ macro_rules! only_for_terminal_module
 /// This macro allows including the passed tokens in an aggregating module. 
 /// It does not restrict itself to any specific context and simply inserts 
 /// the provided tokens, making it useful for any code location.
+/// 
+/// This macro is versatile and can be used to include code in any module, 
+/// providing a mechanism for aggregating functionality across different parts of the codebase. 
+/// It allows for the inclusion of code without context-specific restrictions.
 #[ macro_export ]
 macro_rules! only_for_aggregating_module
 {
