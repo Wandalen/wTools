caf05599d31f7e89b18db63746fd39af5e0a72e9
wandalen <wandalen.me@gmail.com>
2024-01-02 08:26:49 +0200

    only_for: sane description

--- module/step/meta/src/module/aggregating.rs
+++ module/step/meta/src/module/aggregating.rs
@@ -1,12 +1,6 @@
 
-/// This macro allows including the passed tokens in an aggregating module. 
-/// It does not restrict itself to any specific context and simply inserts 
-/// the provided tokens, making it useful for any code location.
-/// 
-/// This macro is designed for scenarios where certain code should only be available 
-/// within the context of the terminal module. It serves as a marker for code that is 
-/// exclusive to the terminal module and allows for better organization and control of 
-/// functionality specific to this module.
+/// Mechanism to include tests only to terminal crate.
+/// It exclude code in terminal module ( crate ), but include for aggregating module ( crate ).
 #[ macro_export ]
 macro_rules! only_for_terminal_module
 {
@@ -15,13 +9,8 @@ macro_rules! only_for_terminal_module
   }
 }
 
-/// This macro allows including the passed tokens in an aggregating module. 
-/// It does not restrict itself to any specific context and simply inserts 
-/// the provided tokens, making it useful for any code location.
-/// 
-/// This macro is versatile and can be used to include code in any module, 
-/// providing a mechanism for aggregating functionality across different parts of the codebase. 
-/// It allows for the inclusion of code without context-specific restrictions.
+/// Mechanism to include tests only to aggregating crate.
+/// It exclude code in terminal module ( crate ), but include for aggregating module ( crate ).
 #[ macro_export ]
 macro_rules! only_for_aggregating_module
 {
