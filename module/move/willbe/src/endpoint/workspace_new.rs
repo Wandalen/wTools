bab9d8e04f12681d33ffc221dd8bdf606a9a93f6
SRetip <shvayko.p.y@gmail.com>
2024-02-22 12:20:44 +0200

    refactor

--- module/move/willbe/src/endpoint/workspace_new.rs
+++ module/move/willbe/src/endpoint/workspace_new.rs
@@ -1,84 +1,104 @@
 mod private
 {
-	use std::fs;
-	use std::io::Write;
-	use std::path::Path;
-	use error_tools::for_app::bail;
-	use error_tools::Result;
+  use std::fs;
+  use std::io::Write;
+  use std::path::Path;
+  use error_tools::for_app::bail;
+  use error_tools::Result;
+  
+  /// Creates workspace template
+  pub fn workspace_new( path: &Path ) -> Result< () >
+	{ 
+    if fs::read_dir( path )?.count() != 0 
+    { 
+      bail!( "Directory should be empty" ) 
+    }
+    dot_cargo( &path )?;
+    dot_circleci( &path )?;
+    dot_github( &path )?;
+    static_dirs( &path )?;
+    static_files( &path )?;
+    example_module( &path )?;
+    Ok( () ) 
+  }
 
-	/// Creates workspace template
-	pub fn workspace_new( path: &Path ) -> Result< () >
-	{
-		if fs::read_dir( path )?.count() != 0
-		{
-			bail!("Directory should be empty")
-		}
+  fn example_module( path: &Path ) -> Result< () > 
+  {
+    create_dir( path, "module" )?;
+    create_dir( &path.join( "module" ), "example_module" )?;
+    create_file( &path.join( "module" ).join( "example_module" ), "Cargo.toml", include_str!( "../../files/template/module/example_module/Cargo.toml" ) )?;
+    create_file( &path.join( "module" ).join( "example_module" ), "Readme.md", include_str!( "../../files/template/module/example_module/Readme.md" ) )?;
+    create_dir( &path.join( "module" ).join( "example_module" ), "examples" )?;
+    create_dir( &path.join( "module" ).join( "example_module" ), "src" )?;
+    create_dir( &path.join( "module" ).join( "example_module" ), "tests" )?;
+    create_file( &path.join( "module" ).join( "example_module" ).join( "examples" ), "example_module_trivial_sample.rs", include_str!( "../../files/template/module/example_module/examples/example_module_trivial_sample.rs" ) )?;
+    create_file( &path.join( "module" ).join( "example_module" ).join( "src" ), "lib.rs", include_str!( "../../files/template/module/example_module/src/lib.rs" ) )?;
+    create_file( &path.join( "module" ).join( "example_module" ).join( "tests" ), "hello_test.rs", include_str!( "../../files/template/module/example_module/tests/hello_test.rs" ) )?;
+    
+    Ok( () )
+  }
 
-		create_dir( path, ".cargo" )?;
-		create_file( &path.join( ".cargo" ), "config.toml", include_str!("../../files/template/.cargo/config.toml") )?;
+  fn static_files(path: &Path) -> Result< () > 
+  { 
+    create_file( path, "Readme.md", include_str!( "../../files/template/Readme.md" ) )?;
+    create_file( path, ".gitattributes", include_str!( "../../files/template/.gitattributes" ) )?;
+    create_file( path, ".gitignore", include_str!( "../../files/template/.gitignore" ) )?;
+    create_file( path, ".gitpod.yml", include_str!( "../../files/template/.gitpod.yml" ) )?;
+    create_file( path, "Cargo.toml", include_str!("../../files/template/Cargo.toml" ) )?;
+    create_file( path, "Makefile", include_str!( "../../files/template/Makefile" ) )?;
+    
+    Ok( () )
+  }
 
-		create_dir( path, ".circleci" )?;
-		create_file( &path.join( ".circleci" ), "config.yml", include_str!("../../files/template/.circleci/config.yml") )?;
+  fn static_dirs( path: &Path ) -> Result< () > 
+  {
+    create_dir( path, "assets" )?;
+    create_dir( path, "docs" )?;
+    
+    Ok( () )
+  }
 
-		create_dir( path, ".github" )?;
-		create_dir( &path.join( ".github" ), "workflow" )?;
+  fn dot_github( path: &Path ) -> Result< () > 
+  {
+    create_dir( path, ".github" )?;
+    create_dir( &path.join( ".github" ),"workflow" )?;
+    
+    Ok( () )
+  }
 
-		create_dir( path, "assets" )?;
+  fn dot_circleci( path: &Path ) -> Result< () > 
+  {
+    create_dir( path, ".circleci" )?;
+    create_file( &path.join( ".circleci" ), "config.yml", include_str!( "../../files/template/.circleci/config.yml" ) )?;
+    
+    Ok( () )
+  }
 
-		create_dir( path, "docs" )?;
+  fn dot_cargo( path: &Path ) -> Result< () > 
+  {
+    create_dir( path, ".cargo" )?;
+    create_file( &path.join( ".cargo" ), "config.toml", include_str!( "../../files/template/.cargo/config.toml" ) )?;
+    
+    Ok( () )
+  }
 
-		create_file( path, "Readme.md", include_str!("../../files/template/Readme.md" ) )?;
-
-		create_file( path, ".gitattributes", include_str!("../../files/template/.gitattributes" ) )?;
-
-		create_file( path, ".gitignore", include_str!("../../files/template/.gitignore" ) )?;
-
-		create_file( path, ".gitpod.yml", include_str!("../../files/template/.gitpod.yml" ) )?;
-
-		create_file( path, "Cargo.toml", include_str!("../../files/template/Cargo.toml" ) )?;
-
-		create_file( path, "Makefile", include_str!("../../files/template/Makefile" ) )?;
-
-		create_dir( path, "module" )?;
-
-		create_dir( &path.join( "module" ), "example_module" )?;
-
-		create_file( &path.join( "module" ).join( "example_module" ), "Cargo.toml", include_str!("../../files/template/module/example_module/Cargo.toml" ) )?;
-
-		create_file( &path.join( "module" ).join( "example_module" ), "Readme.md", include_str!("../../files/template/module/example_module/Readme.md" ) )?;
-
-		create_dir( &path.join( "module" ).join( "example_module" ), "examples" )?;
-
-		create_dir( &path.join( "module" ).join( "example_module" ), "src" )?;
-
-		create_dir( &path.join( "module" ).join( "example_module" ), "tests" )?;
-
-		create_file( &path.join( "module" ).join( "example_module" ).join( "examples" ), "example_module_trivial_sample.rs", include_str!("../../files/template/module/example_module/examples/example_module_trivial_sample.rs" ) )?;
-
-		create_file( &path.join( "module" ).join( "example_module" ).join( "src" ), "lib.rs", include_str!("../../files/template/module/example_module/src/lib.rs" ) )?;
-
-		create_file( &path.join( "module" ).join( "example_module" ).join( "tests" ), "hello_test.rs", include_str!("../../files/template/module/example_module/tests/hello_test.rs" ) )?;
-
-		Ok(())
-	}
-
-	fn create_dir( path: &Path, name: &str ) -> Result< () >
-	{
-		fs::create_dir( path.join( name ) )?;
-		Ok( () )
-	}
-
-	fn create_file( path: &Path, name: &str, content: &str ) -> Result< () >
-	{
-		let mut file = fs::File::create( path.join( name ) )?;
-		file.write_all( content.as_bytes() )?;
-		Ok( () )
-	}
-
-
+  fn create_dir( path: &Path, name: &str ) -> Result< () > 
+  { 
+    fs::create_dir( path.join( name ) )?;
+    
+    Ok( () ) 
+  }
+  
+  fn create_file( path: &Path, name: &str, content: &str ) -> Result< () > 
+  { 
+    let mut file = fs::File::create( path.join( name ) )?;
+    file.write_all( content.as_bytes() )?;
+    
+    Ok( () ) 
+  }
 }
 
-crate::mod_interface!
+crate::mod_interface! 
 {
   prelude use workspace_new;
 }
