80fd3e99dc53d6c5f163d52a69a882c55ab6b6a0
wandalen <wandalen.me@gmail.com>
2024-02-28 23:54:01 +0200

    former : subformal experiment

--- module/core/former/tests/inc/only_test/subformer_basic.rs
+++ module/core/former/tests/inc/only_test/subformer_basic.rs
@@ -1,3 +1,18 @@
+// let ca = wca::CommandsAggregator::former()
+// .command( "echo" )
+//   .hint( "prints all subjects and properties" )
+//   .subject( "Subject", wca::Type::String, true )
+//   .property( "property", "simple property", wca::Type::String, true )
+//   .routine( f1 )
+//   .perform()
+// .command( "exit" )
+//   .hint( "just exit" )
+//   .routine( || exit() )
+//   .perform()
+// .perform()
+// ;
+// ca.execute( input ).unwrap();
+
 #[ test ]
 fn basic()
 {
@@ -10,7 +25,59 @@ fn basic()
   {
     hint : "a".to_string(),
     subject : "b".to_string(),
-    properties : std::collections::HashMap::< &str, String >::new(),
+    properties : std::collections::HashMap::< &str, Property< &str > >::new(),
+  };
+  a_id!( got, exp );
+
+}
+
+//
+
+#[ test ]
+fn properties()
+{
+
+  // with helper
+  let got = Command::< &str >::former()
+  .hint( "a" )
+  .subject( "b" )
+  .property( "property1", "simple property", 13isize )
+  .property( "property2", "simple property 2", 13isize )
+  .property( "property2", "simple property 3", 113isize )
+  .form();
+  let exp = Command::< &str >
+  {
+    hint : "a".to_string(),
+    subject : "b".to_string(),
+    properties : hmap!
+    {
+      "property1" => Property::new( "property1", "simple property", 13isize ),
+      "property2" => Property::new( "property2", "simple property 3", 113isize ),
+    },
+    // properties : std::collections::HashMap::< &str, Property< &str > >::new(),
+  };
+  a_id!( got, exp );
+
+  // with HashMapFormer
+  let got = Command::< &str >::former()
+  .hint( "a" )
+  .subject( "b" )
+  .properties()
+    .insert( "property1", Property::new( "property1", "simple property", 13isize ) )
+    .insert( "property2", Property::new( "property2", "simple property 2", 13isize ) )
+    .insert( "property2", Property::new( "property2", "simple property 3", 113isize ) )
+    .end()
+  .form();
+  let exp = Command::< &str >
+  {
+    hint : "a".to_string(),
+    subject : "b".to_string(),
+    properties : hmap!
+    {
+      "property1" => Property::new( "property1", "simple property", 13isize ),
+      "property2" => Property::new( "property2", "simple property 3", 113isize ),
+    },
+    // properties : std::collections::HashMap::< &str, Property< &str > >::new(),
   };
   a_id!( got, exp );
 
