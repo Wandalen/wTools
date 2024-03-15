e4945db9aa2e6d283dc3f34723d1642ea1004835
SRetip <shvayko.p.y@gmail.com>
2024-03-15 12:46:33 +0200

    add readme

--- module/alias/cargo_will/src/bin/willbe.rs
+++ module/alias/cargo_will/src/bin/willbe.rs
@@ -2,6 +2,7 @@
 #![ doc( html_favicon_url = "https://raw.githubusercontent.com/Wandalen/wTools/alpha/asset/img/logo_v3_trans_square_icon_small_v2.ico" ) ]
 #![ doc( html_root_url = "https://docs.rs/{{template_blank}}/latest/{{template_blank}}/" ) ]
 #![ doc = include_str!( concat!( env!( "CARGO_MANIFEST_DIR" ), "/", "Readme.md" ) ) ]
+
 #[ allow( unused_imports ) ]
 use::willbe::*;
 
