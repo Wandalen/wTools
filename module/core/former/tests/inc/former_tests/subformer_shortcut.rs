7c9de94a2155120b0dac707b1fea0fd064dbbf7c
wandalen <wandalen.me@gmail.com>
2024-03-20 20:01:46 +0200

    former : making subforming more friendly

--- module/core/former/tests/inc/former_tests/subformer_shortcut.rs
+++ module/core/former/tests/inc/former_tests/subformer_shortcut.rs
@@ -6,7 +6,7 @@ use super::*;
 #[ derive( Debug, Default, PartialEq, the_module::Former ) ]
 pub struct TemplateParameterDescriptor
 {
-  parameter : String,
+  descriptor : String,
   is_mandatory : bool,
 }
 
@@ -14,14 +14,14 @@ pub struct TemplateParameterDescriptor
 #[ derive( Debug, Default, PartialEq, the_module::Former ) ]
 pub struct TemplateParameters
 {
-  // #[ debug = the_module::VectorSubformer, parameter, parameter( name ) ]
+  // #[ debug = the_module::VectorSubformer, descriptor, descriptor( name ) ]
   #[ subformer( the_module::VectorSubformer ) ]
   descriptors : Vec< TemplateParameterDescriptor >,
 
   // #[ subformer_setter = the_module::VectorSubformer ]
-  // pub fn parameter( self, name : &str )
+  // pub fn descriptor( self, name : &str )
   // {
-  //   parameter( name )
+  //   descriptor( name )
   // }
 
 }
@@ -59,6 +59,7 @@ pub struct TemplateParameters
 // }
 
 
+/// Interface to construct a subformer passing `itself` as `context` and `on_end` which should return `itself` back.
 pub trait FormerBegin< Struct, Context >
 {
   type End : the_module::ToSuperFormer< Struct, Context >;
@@ -68,6 +69,56 @@ pub trait FormerBegin< Struct, Context >
     context : core::option::Option< Context >,
     on_end : Self::End,
   ) -> Self;
+
+}
+
+/// Interface to add new elements into a container.
+pub trait ContainerAdd
+{
+  type Element;
+
+  fn add( &mut self, e : Self::Element ) -> bool;
+
+}
+
+impl< T > ContainerAdd for collection_tools::Vec< T >
+{
+  type Element = T;
+
+  #[ inline( always ) ]
+  fn add( &mut self, e : Self::Element ) -> bool
+  {
+    self.push( e );
+    true
+  }
+
+}
+
+impl< E > ContainerAdd for collection_tools::HashSet< E >
+where
+  E : core::cmp::Eq + core::hash::Hash,
+{
+  type Element = E;
+
+  #[ inline( always ) ]
+  fn add( &mut self, e : Self::Element ) -> bool
+  {
+    self.insert( e )
+  }
+
+}
+
+impl< K, V > ContainerAdd for collection_tools::HashMap< K, V >
+where
+  K : core::cmp::Eq + core::hash::Hash,
+{
+  type Element = ( K, V );
+
+  #[ inline( always ) ]
+  fn add( &mut self, ( k, v ) : Self::Element ) -> bool
+  {
+    self.insert( k, v ).map_or_else( || true, | _ | false )
+  }
 
 }
 
@@ -95,82 +146,35 @@ where
   End : former::ToSuperFormer< TemplateParameters, Context >,
 {
 
-  // xxx2 : move to a trait and make easier to use subformer, trait with generic interface of a container should help
   #[ inline( always ) ]
-  pub fn __parameter< Former2, Struct >( self ) ->
+  pub fn descriptor3< Former2 >( self ) ->
   Former2
   where
     Former2 : FormerBegin< TemplateParameterDescriptor, Self, End = former::ToSuperFormerWrapper< TemplateParameterDescriptor, Self > >,
+    // FieldContainer : ContainerAdd,
   {
     let on_end = | descriptor : TemplateParameterDescriptor, super_former : core::option::Option< Self > | -> Self
     {
       let mut super_former = super_former.unwrap();
+      if super_former.container.descriptors.is_none()
+      {
+        super_former.container.descriptors = Some( Default::default() );
+      }
       if let Some( ref mut descriptors ) = super_former.container.descriptors
       {
-        descriptors.push( descriptor );
-      }
-      else
-      {
-        super_former.container.descriptors = Some( vec![ descriptor ] );
+        ContainerAdd::add( descriptors, descriptor );
       }
       super_former
     };
     Former2::_begin( Some( self ), former::ToSuperFormerWrapper::new( on_end ) )
   }
+  // xxx2 : move to a trait and make easier to use subformer, trait with generic interface of a container should help
 
   #[ inline( always ) ]
-  pub fn _parameter( self ) ->
+  pub fn descriptor( self, name : &str ) ->
   TemplateParameterDescriptorFormer< Self, impl former::ToSuperFormer< TemplateParameterDescriptor, Self > >
   {
-    let on_end = | descriptor : TemplateParameterDescriptor, super_former : core::option::Option< Self > | -> Self
-    {
-      let mut super_former = super_former.unwrap();
-      if let Some( ref mut descriptors ) = super_former.container.descriptors
-      {
-        descriptors.push( descriptor );
-      }
-      else
-      {
-        super_former.container.descriptors = Some( vec![ descriptor ] );
-      }
-      super_former
-    };
-    TemplateParameterDescriptorFormer::begin( Some( self ), on_end )
-  }
-
-  // #[ inline( always ) ]
-  // pub fn __parameter< Former2, Struct >( self ) ->
-  // // pub fn __parameter( self ) ->
-  // // impl FormerBegin< TemplateParameterDescriptor, Self, Box< dyn former::ToSuperFormer< TemplateParameterDescriptor, Self > > >
-  // // pub fn __parameter( self ) ->
-  // // impl FormerBegin< TemplateParameterDescriptor, Self, impl former::ToSuperFormer< TemplateParameterDescriptor, Self > >
-  // // Former2
-  // // where
-  //   // Former2 : FormerBegin< TemplateParameterDescriptor, Self, impl former::ToSuperFormer< TemplateParameterDescriptor, Self > >,
-  //   // Former2 : FormerBegin< TemplateParameterDescriptor, Self >,
-  //   // End2 : former::ToSuperFormer< TemplateParameterDescriptor, Self >,
-  // {
-  //   let on_end = | descriptor : TemplateParameterDescriptor, super_former : core::option::Option< Self > | -> Self
-  //   {
-  //     let mut super_former = super_former.unwrap();
-  //     if let Some( ref mut descriptors ) = super_former.container.descriptors
-  //     {
-  //       descriptors.push( descriptor );
-  //     }
-  //     else
-  //     {
-  //       super_former.container.descriptors = Some( vec![ descriptor ] );
-  //     }
-  //     super_former
-  //   };
-  //   Former2::_begin( Some( self ), on_end.into() )
-  // }
-
-  #[ inline( always ) ]
-  pub fn parameter( self, name : &str ) ->
-  TemplateParameterDescriptorFormer< Self, impl former::ToSuperFormer< TemplateParameterDescriptor, Self > >
-  {
-    self._parameter().parameter( name )
+    self.descriptor3::< TemplateParameterDescriptorFormer< _, _ > >().descriptor( name )
   }
 
 }
@@ -181,15 +185,15 @@ fn basic()
 
   let got = TemplateParameters::former()
   .descriptors()
-    .push( TemplateParameterDescriptor::former().parameter( "a" ).form() )
-    .push( TemplateParameterDescriptor::former().parameter( "b" ).form() )
+    .push( TemplateParameterDescriptor::former().descriptor( "a" ).form() )
+    .push( TemplateParameterDescriptor::former().descriptor( "b" ).form() )
     .end()
   .form();
 
   let descriptors = vec!
   [
-    TemplateParameterDescriptor { parameter : "a".to_string(), is_mandatory : false },
-    TemplateParameterDescriptor { parameter : "b".to_string(), is_mandatory : false },
+    TemplateParameterDescriptor { descriptor : "a".to_string(), is_mandatory : false },
+    TemplateParameterDescriptor { descriptor : "b".to_string(), is_mandatory : false },
   ];
   let exp = TemplateParameters { descriptors };
   a_id!( got, exp );
