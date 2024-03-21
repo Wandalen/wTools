e5dcc414330645b7e76c4e6f19f0735575127c84
SRetip <shvayko.p.y@gmail.com>
2024-03-21 15:13:36 +0200

    add docx

--- module/move/willbe/src/entity/workspace.rs
+++ module/move/willbe/src/entity/workspace.rs
@@ -12,6 +12,7 @@ mod private
   use wtools::error::{ for_app::Context, for_lib::Error, Result };
   use _path::AbsolutePath;
 
+  /// Facade for cargo_metadata::Package
   #[ derive( Debug, Clone ) ]
   pub struct WorkspacePackage
   {
@@ -31,41 +32,73 @@ mod private
   
   impl WorkspacePackage
   {
+    /// The name field as given in the Cargo.toml
     pub fn name( &self ) -> &String
     {
       &self.inner.name
     }
     
+    /// List of dependencies of this particular package
     pub fn dependencies( &self ) -> &[ Dependency ]
     {
       self.inner.dependencies.as_slice()
     }
     
+    /// Path containing the Cargo.toml
     pub fn manifest_path( &self ) -> &Utf8Path
     {
       self.inner.manifest_path.as_path()
     }
     
+    /// The version field as specified in the Cargo.toml
     pub fn version( &self ) -> Version
     {
       self.inner.version.clone()
     }
     
+    /// List of registries to which this package may be published (derived from the publish field).
+    /// Publishing is unrestricted if None, and forbidden if the Vec is empty.
+    /// This is always None if running with a version of Cargo older than 1.39.
     pub fn publish( &self ) -> Option< &Vec< String > >
     {
       self.inner.publish.as_ref()
     }
     
+    ///Contents of the free form package.metadata section.
+    /// This contents can be serialized to a struct using serde:
+    /// ``` rust
+    /// use serde::Deserialize;
+    /// use serde_json::json;
+    /// 
+    /// #[ derive( Debug, Deserialize ) ]
+    /// struct SomePackageMetadata 
+    /// {
+    ///   some_value : i32,
+    /// }
+    /// 
+    /// fn main() 
+    /// {
+    ///   let value = json!
+    ///   ({
+    ///     "some_value" : 42,
+    ///   });
+    /// 
+    ///   let package_metadata : SomePackageMetadata = serde_json::from_value( value ).unwrap();
+    ///   assert_eq!( package_metadata.some_value, 42 );
+    /// }
+    /// ```
     pub fn metadata( &self ) -> &Value
     {
       &self.inner.metadata
     }
     
+    /// The repository URL as specified in the Cargo.toml
     pub fn repository( &self ) -> Option< &String >
     {
       self.inner.repository.as_ref()
     }
     
+    /// Features provided by the crate, mapped to the features required by that feature.
     pub fn features( &self ) -> &BTreeMap< String, Vec< String > >
     {
       &self.inner.features
@@ -107,13 +140,12 @@ mod private
     /// Load data from current directory
     pub fn with_crate_dir( crate_dir : CrateDir ) -> Result< Self >
     {
-      let metadata = MetadataCommand::new().no_deps().exec().context("fail to load CargoMetadata")?;
       Ok
       (
         Self
         {
-        metadata : Some( MetadataCommand::new().current_dir( crate_dir.as_ref() ).no_deps().exec().context( "fail to load CargoMetadata" )? ),
-        manifest_dir : crate_dir,
+          metadata : Some( MetadataCommand::new().current_dir( crate_dir.as_ref() ).no_deps().exec().context( "fail to load CargoMetadata" )? ),
+          manifest_dir : crate_dir,
         }
       )
     }
