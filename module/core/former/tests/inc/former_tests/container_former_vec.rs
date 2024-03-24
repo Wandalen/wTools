2d9841efdca11530028e3beeca4c837fe58818d6
wandalen <wandalen.me@gmail.com>
2024-03-25 00:13:47 +0200

    former : experimenting

--- module/core/former/tests/inc/former_tests/container_former_vec.rs
+++ module/core/former/tests/inc/former_tests/container_former_vec.rs
@@ -140,33 +140,61 @@ fn replace()
 //
 
 #[ test ]
-fn custom_end()
+fn begin_and_custom_end()
 {
 
   // xxx2 : continue
-  struct Return13;
-  impl former::FormerDefinition for Return13
+  // struct Return13;
+  // impl former::FormerDefinition for Return13
+  // {
+  //   type Storage = Vec< String >;
+  //   type Formed = i32;
+  //   type Context = ();
+  // }
+//
+//   fn return_13( _storage : Vec< String >, _context : Option< () > ) -> i32
+//   {
+//     13
+//   }
+//
+//   let end_wrapper : the_module::FormingEndWrapper< Return13 > = the_module::FormingEndWrapper::new( return_13 );
+
+  // xxx : make example with that
+
+  // basic case
+
+  fn return_13( _storage : Vec< String >, context : Option< () > ) -> f32
   {
-    type Storage = Vec< String >;
-    type Formed = i32;
-    type Context = ();
+    13.1
   }
-
-  fn return_13( _storage : Vec< String >, _context : Option< () > ) -> i32
-  {
-    13
-  }
-
-  let end_wrapper : the_module::FormingEndWrapper< Return13 > = the_module::FormingEndWrapper::new( return_13 );
-
-  let got : i32 = the_module::VectorSubformer::< String, (), i32, _ >::begin( None, None, return_13 )
+  let got = the_module::VectorSubformer::begin( None, None, return_13 )
   .push( "a" )
   .push( "b" )
   .form();
-  // let exp = 13;
-  // a_id!( got, exp );
+  let exp = 13.1;
+  a_id!( got, exp );
 
-  // pub type VectorSubformer< E, Context, End > = ContainerSubformer::< E, VectorDefinition< E, Context, End > >;
+  // with a context
+
+  fn context_plus_13( _storage : Vec< String >, context : Option< f32 > ) -> f32
+  {
+    if let Some( context ) = context
+    {
+      13.1 + context
+    }
+    else
+    {
+      13.1
+    }
+  }
+  let got = the_module::VectorSubformer::begin( None, Some( 10.0 ), context_plus_13 )
+  .push( "a" )
+  .push( "b" )
+  .form();
+  let exp = 23.1;
+  a_id!( got, exp );
+
+  //
 
 }
 
