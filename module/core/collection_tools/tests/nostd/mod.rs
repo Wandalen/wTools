cabc9d38172bd3aa0b1d19f3bf6e1ba36e3106d6
Anton Parfonov <antonparfonov@gmail.com>
2024-03-26 13:25:49 +0200

    Add use_alloc macros, using alloc / hashbrown collection alternatives

--- module/core/collection_tools/tests/nostd/mod.rs
+++ module/core/collection_tools/tests/nostd/mod.rs
@@ -1,8 +1,6 @@
 #[ allow( unused_imports ) ]
 use super::*;
 
-// qqq : xxx : does not work for `use_alloc`, make it working
-#[ cfg( not( feature = "use_alloc" ) ) ]
 #[ cfg( any( feature = "collection_constructors" ) ) ]
 mod constructor;
 
