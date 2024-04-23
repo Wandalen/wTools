5a5a615f220cae60015210ad55c5efb082f19e83
wandalen <wandalen.me@gmail.com>
2024-04-23 09:54:24 +0300

    former : experimenting

--- module/core/former/tests/inc/former_tests/subformer_shortcut.rs
+++ module/core/former/tests/inc/former_tests/subformer_shortcut.rs
@@ -45,41 +45,6 @@ where
   Definition::Types : former::FormerDefinitionTypes< Storage = TemplateParametersFormerStorage >,
 {
 
-  #[ inline( always ) ]
-  pub fn descriptor3< Former2, Definition2, Types2 >( self ) ->
-  Former2
-  where
-    Types2 : former::FormerDefinitionTypes
-    <
-      Storage = TemplateParameterDescriptorFormerStorage,
-      Formed = Self,
-      Context = Self,
-    >,
-    Definition2 : former::FormerDefinition< Types = Types2, End = former::FormingEndClosure< Types2 > >,
-    // Definition2 : former::FormerDefinition< Types = Types2 >,
-    Definition2::End : former::FormingEnd< Definition2::Types >,
-    Former2 : former::FormerBegin
-    <
-      Definition2,
-    >,
-  {
-    let on_end = | descriptor : TemplateParameterDescriptorFormerStorage, super_former : core::option::Option< Self > | -> Self
-    {
-      let mut super_former = super_former.unwrap();
-      if super_former.storage.descriptors.is_none()
-      {
-        super_former.storage.descriptors = Some( Default::default() );
-      }
-      if let Some( ref mut descriptors ) = super_former.storage.descriptors
-      {
-        former::ContainerAdd::add( descriptors, former::StoragePreform::preform( descriptor ) );
-      }
-      super_former
-    };
-    Former2::_begin( None, Some( self ), former::FormingEndClosure::new( on_end ) )
-    // Former2::_begin( None, Some( self ), on_end )
-  }
-
   // xxx2 : move to a trait and make easier to use subformer, trait with generic interface of a container should help
 
   #[ inline( always ) ]
@@ -104,7 +69,123 @@ where
     .name( name )
   }
 
+  #[ inline( always ) ]
+  pub fn descriptor3< Former2, Definition2, Types2 >( self ) ->
+  Former2
+  where
+    Types2 : former::FormerDefinitionTypes
+    <
+      Storage = TemplateParameterDescriptorFormerStorage,
+      Formed = Self,
+      Context = Self,
+    >,
+    Definition2 : former::FormerDefinition< Types = Types2, End = former::FormingEndClosure< Types2 > >,
+    // Definition2 : former::FormerDefinition< Types = Types2 >,
+    Definition2::End : former::FormingEnd< Definition2::Types >,
+    Former2 : former::FormerBegin
+    <
+      Definition2,
+    >,
+  {
+    let on_end = | substorage : TemplateParameterDescriptorFormerStorage, super_former : core::option::Option< Self > | -> Self
+    {
+      let mut super_former = super_former.unwrap();
+      if super_former.storage.descriptors.is_none()
+      {
+        super_former.storage.descriptors = Some( Default::default() );
+      }
+      if let Some( ref mut descriptors ) = super_former.storage.descriptors
+      {
+        former::ContainerAdd::add( descriptors, former::StoragePreform::preform( substorage ) );
+      }
+      super_former
+    };
+    Former2::_begin( None, Some( self ), former::FormingEndClosure::new( on_end ) )
+    // Former2::_begin( None, Some( self ), on_end )
+  }
+
 }
+
+/// Handles the completion of the subformer for `TemplateParameterDescriptor`.
+pub struct TemplateParameterDescriptorEnd< Definition, Former2, Definition2, Types2 >
+{
+  _phantom : core::marker::PhantomData< fn( Definition, Former2, Definition2, Types2 ) >,
+}
+
+impl< Definition, Former2, Definition2, Types2 > Default
+for TemplateParameterDescriptorEnd< Definition, Former2, Definition2, Types2 >
+{
+  #[ inline( always ) ]
+  fn default() -> Self
+  {
+    Self
+    {
+      _phantom : core::marker::PhantomData,
+    }
+  }
+}
+
+// impl< Definition, Former2, Definition2, Types2 > former::FormingEnd
+// <
+//   Types2,
+// >
+// for TemplateParameterDescriptorEnd< Definition, Former2, Definition2, Types2 >
+// where
+//
+//   Definition : former::FormerDefinition,
+//   Definition::Types : former::FormerDefinitionTypes
+//   <
+//     Storage = TemplateParametersFormerStorage
+//   >,
+//
+//   // Self : former::FormerDefinition,
+//   // Self::Types : former::FormerDefinitionTypes< Storage = TemplateParametersFormerStorage, >
+//   Types2 : former::FormerDefinitionTypes
+//   <
+//     Storage = TemplateParameterDescriptorFormerStorage,
+//     Formed = Self,
+//     Context = Self,
+//   >,
+//   Definition2 : former::FormerDefinition< Types = Types2, End = former::FormingEndClosure< Types2 > >,
+//   // Definition2 : former::FormerDefinition< Types = Types2 >,
+//   Definition2::End : former::FormingEnd< Definition2::Types >,
+//   Former2 : former::FormerBegin
+//   <
+//     Definition2,
+//   >,
+//
+// {
+//   #[ inline( always ) ]
+//   fn call
+//   (
+//     &self,
+//     substorage : TemplateParameterDescriptorFormerStorage,
+//     super_former : core::option::Option< Self >,
+//     // descriptor : TemplateParameterDescriptorFormerStorage,
+//     // super_former : Option< TemplateParametersFormer< Self > >,
+//   )
+//   -> TemplateParametersFormer< Definition >
+//   {
+//       let mut super_former = super_former.unwrap();
+//       if super_former.storage.descriptors.is_none()
+//       {
+//         super_former.storage.descriptors = Some( Default::default() );
+//       }
+//       if let Some( ref mut descriptors ) = super_former.storage.descriptors
+//       {
+//         former::ContainerAdd::add( descriptors, former::StoragePreform::preform( substorage ) );
+//       }
+//       super_former
+//     // let mut super_former = super_former.unwrap();
+//     // if super_former.storage.descriptors.is_none()
+//     // {
+//     //   super_former.storage.descriptors = Some( Vec::new() );
+//     // }
+//     // let preformed_descriptor = former::StoragePreform::preform( substorage );
+//     // super_former.storage.descriptors.as_mut().unwrap().push( preformed_descriptor );
+//     // super_former
+//   }
+// }
 
 #[ test ]
 fn basic()
