97cb1fb0a74de665b2850b3b42d210fbd7236780
Anton Parfonov <antonparfonov@gmail.com>
2024-04-26 09:45:53 +0300

    DerefMut tests now use the macro

--- module/core/derive_tools/tests/inc/deref_mut_test.rs
+++ module/core/derive_tools/tests/inc/deref_mut_test.rs
@@ -3,21 +3,7 @@ use super::*;
 // use diagnostics_tools::prelude::*;
 // use derives::*;
 
-#[ derive( Debug, Clone, Copy, PartialEq ) ]
+#[ derive( Debug, Clone, Copy, PartialEq, the_module::Deref, the_module::DerefMut ) ]
 pub struct IsTransparent( bool );
-
-impl std::ops::Deref for IsTransparent {
-    type Target = bool;
-    
-    fn deref(&self) -> &Self::Target {
-        &self.0
-    }
-}
-
-impl std::ops::DerefMut for IsTransparent {
-    fn deref_mut(&mut self) -> &mut Self::Target {
-        &mut self.0
-    }
-}
 
 include!( "./only_test/deref_mut.rs" );
