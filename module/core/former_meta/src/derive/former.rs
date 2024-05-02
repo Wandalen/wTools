1dd73e2f1d1526efe8ab87493cd1665a85a4d6a2
wandalen <wandalen.me@gmail.com>
2024-05-02 19:23:24 +0300

    former : experimenting

--- module/core/former_meta/src/derive/former.rs
+++ module/core/former_meta/src/derive/former.rs
@@ -4,6 +4,8 @@ use iter_tools::{ Itertools, process_results };
 use macro_tools::{ attr, diag, generic_params, generic_args, container_kind, typ, Result };
 use proc_macro2::TokenStream;
 
+// zzz : feature to have storage fields
+
 ///
 /// Definition of a field.
 ///
