d649b487e75c125fbf1dabf47512da398b39c7d0
wandalen <wandalen.me@gmail.com>
2024-05-12 17:13:26 +0300

    program_tools : draft

--- module/core/program_tools/src/program.rs
+++ module/core/program_tools/src/program.rs
@@ -2,96 +2,96 @@
 pub( crate ) mod private
 {
 
-  use former::Former;
-  use std::
-  {
-    path::{ Path, PathBuf },
-    // process::Command,
-  };
-
-  #[ derive( Debug, Default, Former ) ]
-  pub struct SourceFile
-  {
-    file_path : PathBuf,
-    data : GetData,
-  }
-
-  #[ derive( Debug, Default, Former ) ]
-  pub struct Entry
-  {
-    source_file : SourceFile,
-    typ : EntryType,
-  }
-
-  #[ derive( Debug, Default, Former ) ]
-  pub struct CargoFile
-  {
-    file_path : PathBuf,
-    data : GetData,
-  }
-
-  #[ derive( Debug, Default, Former ) ]
-  // #[ debug ]
-  pub struct Program
-  {
-    write_path : Option< PathBuf >,
-    read_path : Option< PathBuf >,
-    entries : Vec< Entry >,
-    sources : Vec< SourceFile >,
-    cargo_file : Option< CargoFile >,
-  }
-
-  #[ derive( Debug, Default, Former ) ]
-  pub struct ProgramRun
-  {
-    // #[ embed ]
-    program : Program,
-    calls : Vec< ProgramCall >,
-  }
-
-  #[ derive( Debug ) ]
-  pub enum GetData
-  {
-    FromStr( &'static str ),
-    FromBin( &'static [ u8 ] ),
-    FromFile( PathBuf ),
-    FromString( String ),
-  }
-
-  impl Default for GetData
-  {
-    fn default() -> Self
-    {
-      GetData::FromStr( "" )
-    }
-  }
-
-  #[ derive( Debug, Default ) ]
-  pub struct ProgramCall
-  {
-    action : ProgramAction,
-    current_path : Option< PathBuf >,
-    args : Vec< String >,
-    index_of_entry : i32,
-  }
-
-  #[ derive( Debug, Default ) ]
-  pub enum ProgramAction
-  {
-    #[ default ]
-    Run,
-    Build,
-    Test,
-  }
-
-  #[ derive( Debug, Default ) ]
-  pub enum EntryType
-  {
-    #[ default ]
-    Bin,
-    Lib,
-    Test,
-  }
+//   use former::Former;
+//   use std::
+//   {
+//     path::{ Path, PathBuf },
+//     // process::Command,
+//   };
+//
+//   #[ derive( Debug, Default, Former ) ]
+//   pub struct SourceFile
+//   {
+//     file_path : PathBuf,
+//     data : GetData,
+//   }
+//
+//   #[ derive( Debug, Default, Former ) ]
+//   pub struct Entry
+//   {
+//     source_file : SourceFile,
+//     typ : EntryType,
+//   }
+//
+//   #[ derive( Debug, Default, Former ) ]
+//   pub struct CargoFile
+//   {
+//     file_path : PathBuf,
+//     data : GetData,
+//   }
+//
+//   #[ derive( Debug, Default, Former ) ]
+//   // #[ debug ]
+//   pub struct Program
+//   {
+//     write_path : Option< PathBuf >,
+//     read_path : Option< PathBuf >,
+//     entries : Vec< Entry >,
+//     sources : Vec< SourceFile >,
+//     cargo_file : Option< CargoFile >,
+//   }
+//
+//   #[ derive( Debug, Default, Former ) ]
+//   pub struct ProgramRun
+//   {
+//     // #[ embed ]
+//     program : Program,
+//     calls : Vec< ProgramCall >,
+//   }
+//
+//   #[ derive( Debug ) ]
+//   pub enum GetData
+//   {
+//     FromStr( &'static str ),
+//     FromBin( &'static [ u8 ] ),
+//     FromFile( PathBuf ),
+//     FromString( String ),
+//   }
+//
+//   impl Default for GetData
+//   {
+//     fn default() -> Self
+//     {
+//       GetData::FromStr( "" )
+//     }
+//   }
+//
+//   #[ derive( Debug, Default ) ]
+//   pub struct ProgramCall
+//   {
+//     action : ProgramAction,
+//     current_path : Option< PathBuf >,
+//     args : Vec< String >,
+//     index_of_entry : i32,
+//   }
+//
+//   #[ derive( Debug, Default ) ]
+//   pub enum ProgramAction
+//   {
+//     #[ default ]
+//     Run,
+//     Build,
+//     Test,
+//   }
+//
+//   #[ derive( Debug, Default ) ]
+//   pub enum EntryType
+//   {
+//     #[ default ]
+//     Bin,
+//     Lib,
+//     Test,
+//   }
 
 }
 
