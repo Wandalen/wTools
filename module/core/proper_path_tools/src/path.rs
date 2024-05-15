42e975a299439eea75781a88a3ba0db08fefdc04
SupperZum <levchenko.d25@gmail.com>
2024-05-15 09:32:35 +0300

    fix workflow

--- module/core/proper_path_tools/src/path.rs
+++ module/core/proper_path_tools/src/path.rs
@@ -213,6 +213,10 @@ pub( crate ) mod private
   {
     #[ cfg( target_os = "windows" ) ]
     use std::path::PathBuf;
+    #[ cfg( feature = "no_std" ) ]
+    extern crate alloc;
+     #[ cfg( feature = "no_std" ) ]
+    use alloc::string::ToString;
 
     // println!( "a" );
     // let path = path.as_ref().canonicalize()?;
