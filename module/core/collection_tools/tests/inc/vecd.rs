ad811979267a0f8e21619ec066a9723b35191c40
Anton Parfonov <antonparfonov@gmail.com>
2024-05-15 17:50:11 +0300

    Add restrictions to collections module

--- module/core/collection_tools/tests/inc/vecd.rs
+++ module/core/collection_tools/tests/inc/vecd.rs
@@ -1,6 +1,5 @@
 use super::*;
 
-#[ cfg( any( feature = "use_alloc", not( feature = "no_std" ) ) ) ]
 #[ test ]
 fn reexport()
 {
@@ -50,7 +49,6 @@ fn into_constructor()
 
 }
 
-#[ cfg( any( not( feature = "no_std" ), feature = "use_alloc" ) ) ]
 #[ test ]
 fn iters()
 {
