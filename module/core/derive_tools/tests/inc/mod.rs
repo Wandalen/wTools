0f972c46abf3cc6a72e1ac322fe4cb5943416e9f
wandalen <wandalen.me@gmail.com>
2024-05-18 07:57:14 +0300

    macro_tools: refactoring

--- module/core/derive_tools/tests/inc/mod.rs
+++ module/core/derive_tools/tests/inc/mod.rs
@@ -5,38 +5,38 @@ use super::*;
 // mod clone_dyn_test;
 
 // xxx
-// mod all_manual_test;
-// #[ cfg
-// (
-// 	all
-// 	(
-// 		feature = "derive_as_mut",
-// 		feature = "derive_as_ref",
-// 		feature = "derive_deref",
-// 		feature = "derive_deref_mut",
-// 		feature = "derive_from",
-// 		feature = "derive_inner_from",
-// 	)
-// )]
-// mod all_test;
-//
-// mod basic_test;
-//
-// mod as_mut_manual_test;
-// #[ cfg( feature = "derive_as_mut" ) ]
-// mod as_mut_test;
-//
-// mod as_ref_manual_test;
-// #[ cfg( feature = "derive_as_ref" ) ]
-// mod as_ref_test;
-//
-// mod deref_manual_test;
-// #[ cfg( feature = "derive_deref" ) ]
-// mod deref_test;
-//
-// mod deref_mut_manual_test;
-// #[ cfg( feature = "derive_deref_mut" ) ]
-// mod deref_mut_test;
+mod all_manual_test;
+#[ cfg
+(
+	all
+	(
+		feature = "derive_as_mut",
+		feature = "derive_as_ref",
+		feature = "derive_deref",
+		feature = "derive_deref_mut",
+		feature = "derive_from",
+		feature = "derive_inner_from",
+	)
+)]
+mod all_test;
+
+mod basic_test;
+
+mod as_mut_manual_test;
+#[ cfg( feature = "derive_as_mut" ) ]
+mod as_mut_test;
+
+mod as_ref_manual_test;
+#[ cfg( feature = "derive_as_ref" ) ]
+mod as_ref_test;
+
+mod deref_manual_test;
+#[ cfg( feature = "derive_deref" ) ]
+mod deref_test;
+
+mod deref_mut_manual_test;
+#[ cfg( feature = "derive_deref_mut" ) ]
+mod deref_mut_test;
 
 #[ cfg( feature = "derive_from" ) ]
 mod from_inner_named_test;
@@ -44,36 +44,36 @@ mod from_inner_named_manual_test;
 
 // xxx
 
-// mod from_inner_manual_test;
-// mod from_inner_multiple_named_manual_test;
-// mod from_inner_multiple_manual_test;
-// mod from_inner_unit_manual_test;
-// #[ cfg( feature = "derive_from" ) ]
-// mod from_inner_test;
-// #[ cfg( feature = "derive_from" ) ]
-// mod from_inner_multiple_named_test;
-// #[ cfg( feature = "derive_from" ) ]
-// mod from_inner_unit_test;
-// #[ cfg( feature = "derive_from" ) ]
-// mod from_inner_multiple_test;
-// #[ cfg( feature = "derive_from" ) ]
-// mod from_inner_variants;
-//
-// mod inner_from_manual_test;
-// mod inner_from_named_manual_test;
-// mod inner_from_multiple_named_manual_test;
-// mod inner_from_multiple_manual_test;
-// mod inner_from_unit_manual_test;
-// #[ cfg( feature = "derive_inner_from" ) ]
-// mod inner_from_test;
-// #[ cfg( feature = "derive_inner_from" ) ]
-// mod inner_from_named_test;
-// #[ cfg( feature = "derive_inner_from" ) ]
-// mod inner_from_multiple_named_test;
-// #[ cfg( feature = "derive_inner_from" ) ]
-// mod inner_from_unit_test;
-// #[ cfg( feature = "derive_inner_from" ) ]
-// mod inner_from_multiple_test;
+mod from_inner_manual_test;
+mod from_inner_multiple_named_manual_test;
+mod from_inner_multiple_manual_test;
+mod from_inner_unit_manual_test;
+#[ cfg( feature = "derive_from" ) ]
+mod from_inner_test;
+#[ cfg( feature = "derive_from" ) ]
+mod from_inner_multiple_named_test;
+#[ cfg( feature = "derive_from" ) ]
+mod from_inner_unit_test;
+#[ cfg( feature = "derive_from" ) ]
+mod from_inner_multiple_test;
+#[ cfg( feature = "derive_from" ) ]
+mod from_inner_variants;
+
+mod inner_from_manual_test;
+mod inner_from_named_manual_test;
+mod inner_from_multiple_named_manual_test;
+mod inner_from_multiple_manual_test;
+mod inner_from_unit_manual_test;
+#[ cfg( feature = "derive_inner_from" ) ]
+mod inner_from_test;
+#[ cfg( feature = "derive_inner_from" ) ]
+mod inner_from_named_test;
+#[ cfg( feature = "derive_inner_from" ) ]
+mod inner_from_multiple_named_test;
+#[ cfg( feature = "derive_inner_from" ) ]
+mod inner_from_unit_test;
+#[ cfg( feature = "derive_inner_from" ) ]
+mod inner_from_multiple_test;
 
 // qqq : for Petro : xxx : fix
 // #[ cfg( all( feature = "type_variadic_from" ) ) ]
