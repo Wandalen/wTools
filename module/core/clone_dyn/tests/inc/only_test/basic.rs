a0d18cbd728f32fd77602e1dcc70b849f3a8ff31
wandalen <wandalen.me@gmail.com>
2024-06-03 14:48:26 +0300

    clone_dyn : fixing it

--- module/core/clone_dyn/tests/inc/only_test/basic.rs
+++ module/core/clone_dyn/tests/inc/only_test/basic.rs
@@ -70,12 +70,21 @@ fn vector()
   //
 
   let e_i32 : Box< dyn Trait1 > = Box::new( 13 );
-  let e_i64 : Box< dyn Trait1 > = Box::new( 13 );
+  let e_i64 : Box< dyn Trait1 > = Box::new( 14 );
   let e_string : Box< dyn Trait1 > = Box::new( "abc".to_string() );
-  let e_str_slice : Box< dyn Trait1 > = Box::new( "abc" );
-  let e_slice : Box< dyn Trait1 > = Box::new( &[ 1i32, 2i32, 3i32 ] as &[ i32 ] );
+  let e_str_slice : Box< dyn Trait1 > = Box::new( "abcd" );
+  let e_slice : Box< dyn Trait1 > = Box::new( &[ 1i32, 2i32 ] as &[ i32 ] );
 
-  let vec : Vec< Box< dyn Trait1 > > = vec![ e_i32, e_i64, e_string, e_str_slice ];
+  //
+
+  let vec : Vec< Box< dyn Trait1 > > = vec![ e_i32.clone(), e_i64.clone(), e_string.clone(), e_str_slice.clone(), e_slice.clone() ];
+  let vec = vec.iter().map( | e | e.val() ).collect::< Vec< _ > >();
+  let vec2 = vec![ 13, 14, 3, 4, 2 ];
+  a_id!( vec, vec2 );
+
+  //
+
+  let vec : Vec< Box< dyn Trait1 > > = vec![ e_i32.clone(), e_i64.clone(), e_string.clone(), e_str_slice.clone(), e_slice.clone() ];
   let vec2 = clone_dyn::clone( &vec );
   let vec = vec.iter().map( | e | e.val() ).collect::< Vec< _ > >();
   let vec2 = vec2.iter().map( | e | e.val() ).collect::< Vec< _ > >();
@@ -83,10 +92,12 @@ fn vector()
 
   //
 
-  let vec : Vec< Box< dyn Trait1 > > = vec![ Box::new( 13 ), Box::new( 14i64 ) ];
+  let vec : Vec< Box< dyn Trait1 > > = vec![ e_i32.clone(), e_i64.clone(), e_string.clone(), e_str_slice.clone(), e_slice.clone() ];
   let vec2 = vec.clone();
   let vec = vec.iter().map( | e | e.val() ).collect::< Vec< _ > >();
   let vec2 = vec2.iter().map( | e | e.val() ).collect::< Vec< _ > >();
   a_id!( vec, vec2 );
+
+  //
 
 }
