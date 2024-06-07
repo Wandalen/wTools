02635d944f94864bccee5991716a7f1c450be563
wandalen <wandalen.me@gmail.com>
2024-06-07 20:06:31 +0300

    experimenting

--- module/move/assistant/src/wrapper_option_cow.rs
+++ module/move/assistant/src/wrapper_option_cow.rs
@@ -4,9 +4,9 @@ use core::ops::{ Deref };
 /// Transparent reference wrapper to emphasizing a specific aspect of identity of its internal type.
 #[ repr( transparent ) ]
 #[ derive( Clone, Copy ) ]
-pub struct AsCow< 'a, T >( &'a T );
+pub struct AsOptionCow< 'a, T >( &'a T );
 
-impl< 'a, T > AsCow< 'a, T >
+impl< 'a, T > AsOptionCow< 'a, T >
 {
 
   /// Just a constructor.
@@ -25,7 +25,7 @@ impl< 'a, T > AsCow< 'a, T >
 
 }
 
-impl< 'a, T > AsRef< T > for AsCow< 'a, T >
+impl< 'a, T > AsRef< T > for AsOptionCow< 'a, T >
 {
   fn as_ref( &self ) -> &T
   {
@@ -33,7 +33,7 @@ impl< 'a, T > AsRef< T > for AsCow< 'a, T >
   }
 }
 
-impl< 'a, T > Deref for AsCow< 'a, T >
+impl< 'a, T > Deref for AsOptionCow< 'a, T >
 {
   type Target = T;
   fn deref( &self ) -> &Self::Target
@@ -42,39 +42,39 @@ impl< 'a, T > Deref for AsCow< 'a, T >
   }
 }
 
-impl< 'a, T > From< &'a T > for AsCow< 'a, T >
+impl< 'a, T > From< &'a T > for AsOptionCow< 'a, T >
 {
   fn from( table : &'a T ) -> Self
   {
-    AsCow( table )
+    AsOptionCow( table )
   }
 }
 
-// impl< 'a, T > From< AsCow< 'a, T > > for &'a T
+// impl< 'a, T > From< AsOptionCow< 'a, T > > for &'a T
 // {
-//   fn from( wrapper : AsCow< 'a, T > ) -> &'a T
+//   fn from( wrapper : AsOptionCow< 'a, T > ) -> &'a T
 //   {
 //     wrapper.0
 //   }
 // }
 
-// impl< 'a, T > Default for AsCow< 'a, T >
+// impl< 'a, T > Default for AsOptionCow< 'a, T >
 // where
 //   T : Default,
 // {
 //   fn default() -> Self
 //   {
-//     AsCow( &T::default() )
+//     AsOptionCow( &T::default() )
 //   }
 // }
 
-impl< 'a, T > fmt::Debug for AsCow< 'a, T >
+impl< 'a, T > fmt::Debug for AsOptionCow< 'a, T >
 where
   T : fmt::Debug,
 {
   fn fmt( &self, f : &mut fmt::Formatter< '_ > ) -> fmt::Result
   {
-    f.debug_struct( "AsCow" )
+    f.debug_struct( "AsOptionCow" )
     .field( "0", &self.0 )
     .finish()
   }
