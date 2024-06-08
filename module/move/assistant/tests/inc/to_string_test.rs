3dbf392bf179afd360cfc500b60ed9de6ea200aa
wandalen <wandalen.me@gmail.com>
2024-06-08 16:05:04 +0300

    reflect: ToString

--- module/move/assistant/tests/inc/to_string_test.rs
+++ module/move/assistant/tests/inc/to_string_test.rs
@@ -8,6 +8,7 @@ use assistant::
   MaybeAs,
   ToStringWith,
   WithDebug,
+  WithDisplay,
 };
 
 use std::
@@ -20,12 +21,33 @@ use std::
 //
 
 #[ test ]
-fn test_vec_fields()
+fn to_string_with_test()
 {
+
+  // -
 
   let src = 13i32;
   let got = ToStringWith::< WithDebug >::to_string_with( &src );
   let exp = "13".to_string();
   a_id!( got, exp );
+
+  let src = "abc".to_string();
+  let got = ToStringWith::< WithDebug >::to_string_with( &src );
+  let exp = "\"abc\"".to_string();
+  a_id!( got, exp );
+
+  // -
+
+  let src = 13i32;
+  let got = ToStringWith::< WithDisplay >::to_string_with( &src );
+  let exp = "13".to_string();
+  a_id!( got, exp );
+
+  let src = "abc".to_string();
+  let got = ToStringWith::< WithDisplay >::to_string_with( &src );
+  let exp = "abc".to_string();
+  a_id!( got, exp );
+
+  // -
 
 }
