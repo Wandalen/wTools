2fe809637a29d02775536d406fe0ddcac26a8617
wandalen <wandalen.me@gmail.com>
2024-06-09 23:50:32 +0300

    clone_dyn : cleaning

--- module/core/clone_dyn/src/lib.rs
+++ module/core/clone_dyn/src/lib.rs
@@ -5,17 +5,20 @@
 // #![ doc = include_str!( concat!( env!( "CARGO_MANIFEST_DIR" ), "/", "Readme.md" ) ) ]
 // xxx
 
-#[ allow( unused_extern_crates ) ]
-#[ cfg( feature = "enabled" ) ]
-#[ cfg( all( feature = "no_std", feature = "use_alloc" ) ) ]
-extern crate alloc;
+// #[ allow( unused_extern_crates ) ]
+// #[ cfg( feature = "enabled" ) ]
+// #[ cfg( all( feature = "no_std", feature = "use_alloc" ) ) ]
+// extern crate alloc;
 
 /// Namespace with dependencies.
 
 #[ cfg( feature = "enabled" ) ]
 pub mod dependency
 {
+  #[ cfg( feature = "clone_dyn_meta" ) ]
   pub use ::clone_dyn_meta;
+  #[ cfg( feature = "clone_dyn_types" ) ]
+  pub use ::clone_dyn_types;
 }
 
 /// Internal namespace.
@@ -23,163 +26,6 @@ pub mod dependency
 #[ cfg( feature = "enabled" ) ]
 pub( crate ) mod private
 {
-
-  #[ cfg( any( not( feature = "no_std" ), feature = "use_alloc" ) ) ]
-  extern crate alloc;
-  #[ cfg( any( not( feature = "no_std" ), feature = "use_alloc" ) ) ]
-  #[ allow( unused_imports ) ]
-  use alloc::boxed::Box;
-  #[ cfg( all( feature = "use_std", not( feature = "use_alloc" ) ) ) ]
-  use std::boxed::Box;
-
-  /// A trait to upcast a clonable entity and clone it.
-  /// It's implemented for all entities which can be cloned.
-  pub trait CloneDyn : Sealed
-  {
-    fn __clone_dyn( &self, _ : DontCallMe ) -> *mut ();
-  }
-
-  // clonable
-  impl< T > CloneDyn for T
-  where
-    T : Clone,
-  {
-    #[ inline ]
-    fn __clone_dyn( &self, _ : DontCallMe ) -> *mut ()
-    {
-      Box::< T >::into_raw( Box::new( self.clone() ) ) as *mut ()
-    }
-  }
-
-  // slice
-  impl< T > CloneDyn for [ T ]
-  where
-    T : Clone,
-  {
-    #[ inline ]
-    fn __clone_dyn( &self, _ : DontCallMe ) -> *mut ()
-    {
-      Box::< [ T ] >::into_raw( self.iter().cloned().collect() ) as *mut ()
-    }
-  }
-
-  // str slice
-  impl CloneDyn for str
-  {
-    #[ inline ]
-    fn __clone_dyn( &self, _ : DontCallMe ) -> *mut ()
-    {
-      Box::< str >::into_raw( Box::from( self ) ) as *mut ()
-    }
-  }
-
-  ///
-  /// True clone which is applicable not only to clonable entities, but to trait objects implementing CloneDyn.
-  ///
-  /// # Example
-  ///
-  /// ```
-  /// use clone_dyn::clone;
-  ///
-  /// #[ derive( Clone ) ]
-  /// struct MyStruct
-  /// {
-  ///   value : i32,
-  /// }
-  ///
-  /// let original = MyStruct { value : 42 };
-  /// let cloned = clone( &original );
-  ///
-  /// assert_eq!( original.value, cloned.value );
-  /// ```
-
-  #[ inline ]
-  pub fn clone< T >( src : &T ) -> T
-  where
-    T : CloneDyn,
-  {
-    /// # Safety
-    ///
-    /// This function uses an `unsafe` block because it performs low-level memory manipulations. Specifically, it handles
-    /// raw pointers and converts them to and from `Box< T >`. This is necessary to dynamically clone a trait object, which
-    /// does not support cloning through the standard `Clone` trait. The safety of this function depends on the guarantee
-    /// that the `CloneDyn` trait is correctly implemented for the given type `T`, ensuring that `__clone_dyn` returns a
-    /// valid pointer to a cloned instance of `T`.
-    ///
-    #[ allow( unsafe_code ) ]
-    unsafe
-    {
-      *Box::from_raw( < T as CloneDyn >::__clone_dyn( src, DontCallMe ) as *mut T )
-    }
-  }
-
-  ///
-  /// Clone boxed dyn.
-  ///
-  /// Clones a dynamically sized trait object into a `Box< T >`.
-  ///
-  /// # Example
-  ///
-  /// ```
-  /// use clone_dyn::{ clone_into_box, clone_dyn };
-  ///
-  /// #[ derive( Clone ) ]
-  /// struct MyStruct
-  /// {
-  ///   value : i32,
-  /// }
-  ///
-  /// #[ clone_dyn ]
-  /// trait MyTrait
-  /// {
-  ///   fn val( &self ) -> i32;
-  /// }
-  ///
-  /// impl MyTrait for MyStruct
-  /// {
-  ///   fn val( &self ) -> i32
-  ///   {
-  ///     self.value
-  ///   }
-  /// }
-  ///
-  /// let cloned : Box< dyn MyTrait > = clone_into_box( &MyStruct { value : 42 } );
-  ///
-  /// ```
-
-  #[ inline ]
-  pub fn clone_into_box< T >( ref_dyn : &T ) -> Box< T >
-  where
-    T : ?Sized + CloneDyn,
-  {
-    /// # Safety
-    ///
-    /// This function uses an `unsafe` block because it performs low-level memory manipulations involving raw pointers.
-    /// The `unsafe` block is necessary here because we're manually handling raw pointers and converting them to and from
-    /// `Box<T>`. This bypasses Rust's ownership and borrowing rules to achieve dynamic cloning of a boxed trait object.
-    /// The safety of this function relies on the correct implementation of the `CloneDyn` trait for the given type `T`.
-    /// Specifically, `__clone_dyn` must return a valid pointer to a cloned instance of `T`.
-    ///
-    #[ allow( unsafe_code ) ]
-    unsafe
-    {
-      let mut ptr = ref_dyn as *const T;
-      let data_ptr = &mut ptr as *mut *const T as *mut *mut ();
-      *data_ptr = < T as CloneDyn >::__clone_dyn( ref_dyn, DontCallMe );
-      Box::from_raw( ptr as *mut T )
-    }
-  }
-
-  mod sealed
-  {
-    pub struct DontCallMe;
-    pub trait Sealed {}
-    impl< T : Clone > Sealed for T {}
-    impl< T : Clone > Sealed for [ T ] {}
-    impl Sealed for str {}
-  }
-  use sealed::*;
-
 }
 
 #[ cfg( feature = "enabled" ) ]
@@ -200,9 +46,16 @@ pub mod protected
 #[ cfg( feature = "enabled" ) ]
 pub mod orphan
 {
+
   #[ doc( inline ) ]
   #[ allow( unused_imports ) ]
   pub use super::exposed::*;
+
+  #[ doc( inline ) ]
+  #[ allow( unused_imports ) ]
+  #[ cfg( feature = "clone_dyn_types" ) ]
+  pub use super::dependency::clone_dyn_types::exposed::*;
+
 }
 
 /// Exposed namespace of the module.
@@ -218,17 +71,15 @@ pub mod exposed
 #[ cfg( feature = "enabled" ) ]
 pub mod prelude
 {
+
   #[ doc( inline ) ]
   #[ allow( unused_imports ) ]
-  #[ cfg( any( not( feature = "no_std" ), feature = "use_alloc" ) ) ]
+  #[ cfg( feature = "clone_dyn_meta" ) ]
   pub use ::clone_dyn_meta::clone_dyn;
+
   #[ doc( inline ) ]
   #[ allow( unused_imports ) ]
-  #[ cfg( any( not( feature = "no_std" ), feature = "use_alloc" ) ) ]
-  pub use super::private::
-  {
-    CloneDyn,
-    clone_into_box,
-    clone,
-  };
+  #[ cfg( feature = "clone_dyn_types" ) ]
+  pub use super::dependency::clone_dyn_types::prelude::*;
+
 }
