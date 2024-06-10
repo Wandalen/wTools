67759f6ea48723c42b59dabc9bc7533a5e187d34
wandalen <wandalen.me@gmail.com>
2024-06-10 17:57:09 +0300

    macro_tools: specific types instead of return impl

--- module/core/macro_tools/src/item_struct.rs
+++ module/core/macro_tools/src/item_struct.rs
@@ -8,22 +8,25 @@ pub( crate ) mod private
   use crate::*;
 
   /// Extracts the types of each field into a vector.
-  pub fn field_types< 'a >( t : &'a syn::ItemStruct ) -> impl IterTrait< 'a, &'a syn::Type > + Clone
+  pub fn field_types< 'a >( t : &'a syn::ItemStruct )
+  // -> impl IterTrait< 'a, &'a syn::Type > + Clone
+  -> std::iter::Map
+  <
+    // std::boxed::Box< dyn _IterTrait< '_, &syn::Field > + 'a >,
+    syn::punctuated::Iter< 'a, syn::Field >,
+    impl FnMut( &'a syn::Field ) -> &'a syn::Type + 'a,
+  >
   {
     t.fields.iter().map( | field | &field.ty )
   }
 
   /// Retrieves the names of each field, if they exist.
-  // pub fn field_names< 'a >( t : &'a syn::ItemStruct ) -> Option< impl IterTrait< 'a, &'a syn::Ident > >
   pub fn field_names< 'a >( t : &'a syn::ItemStruct ) -> Option< BoxedIter< 'a, &'a syn::Ident > >
-  // xxx
   {
     let result : Option< BoxedIter< 'a, &'a syn::Ident > > = match &t.fields
     {
       syn::Fields::Named( fields ) => Some( Box::new( fields.named.iter().map( | field | field.ident.as_ref().unwrap() ) ) ),
       syn::Fields::Unit => Some( Box::new( core::iter::empty() ) ),
-      // syn::Fields::Named( fields ) => Some( DynIter::new( fields.named.iter().map( | field | field.ident.as_ref().unwrap() ) ) ),
-      // syn::Fields::Unit => Some( DynIter::new( core::iter::empty() ) ),
       _ => None,
     };
     return result;
