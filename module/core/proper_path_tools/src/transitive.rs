0510ec49a2ab5a803cfbd53abc6fa12c143c40a7
wandalen <wandalen.me@gmail.com>
2024-06-22 08:23:34 +0300

    willbe: refactoring

--- module/core/proper_path_tools/src/transitive.rs
+++ module/core/proper_path_tools/src/transitive.rs
@@ -10,31 +10,52 @@ pub( crate ) mod private
     fn transitive_try_from( value : T ) -> Result< Self, Self::Error >;
   }
 
-  impl< T, U > TransitiveTryInto< U > for T
-  where
-    U : TransitiveTryFrom< T >,
-  {
-    type Error = U::Error;
+//   impl< IntoAbsolutePathType > TransitiveTryFrom< IntoAbsolutePathType > for CrateDir
+//   where
+//     IntoAbsolutePathType : TryInto< AbsolutePath >,
+//     PathError : From< < IntoAbsolutePathType as TryInto< AbsolutePath > >::Error >,
+//   {
+//     type Error = PathError;
+//
+//     #[ inline( always ) ]
+//     fn transitive_try_from( crate_dir_path : IntoAbsolutePathType ) -> Result< Self, Self::Error >
+//     {
+//       // let crate_dir_path = IntoAbsolutePathType::into_absolute_path( crate_dir_path )?;
+//       let crate_dir_path = TryInto::< AbsolutePath >::try_into( crate_dir_path )?;
+//       if !crate_dir_path.as_ref().join( "Cargo.toml" ).is_file()
+//       {
+//         let err =  io::Error::new( io::ErrorKind::InvalidData, format!( "Cannot find crate dir at {crate_dir_path:?}" ) );
+//         return Err( PathError::Io( err ) );
+//       }
+//       Ok( Self( crate_dir_path ) )
+//     }
+//   }
 
-    #[ inline ]
-    fn transitive_try_into( self ) -> Result< U, U::Error >
-    {
-      U::transitive_try_from( self )
-    }
-  }
-
-  pub trait TransitiveTryInto< T > : Sized
-  {
-    type Error;
-
-    // Required method
-    fn transitive_try_into( self ) -> Result< T, Self::Error >;
-  }
+//   impl< T, U > TransitiveTryInto< U > for T
+//   where
+//     U : TransitiveTryFrom< T >,
+//   {
+//     type Error = U::Error;
+//
+//     #[ inline ]
+//     fn transitive_try_into( self ) -> Result< U, U::Error >
+//     {
+//       U::transitive_try_from( self )
+//     }
+//   }
+//
+//   pub trait TransitiveTryInto< T > : Sized
+//   {
+//     type Error;
+//
+//     // Required method
+//     fn transitive_try_into( self ) -> Result< T, Self::Error >;
+//   }
 
 }
 
 crate::mod_interface!
 {
   exposed use TransitiveTryFrom;
-  exposed use TransitiveTryInto;
+  // exposed use TransitiveTryInto;
 }
