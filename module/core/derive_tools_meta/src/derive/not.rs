f043e7c1e9a4efdaa5510a2c24a84e52a55187c2
Dmytro Kriuchkov <kriuchkovdima@gmail.com>
2024-06-25 12:25:33 +0300

    Basic macros implementation

--- module/core/derive_tools_meta/src/derive/not.rs
+++ module/core/derive_tools_meta/src/derive/not.rs
@@ -1,11 +1,5 @@
 use super::*;
-use macro_tools::
-{
-  attr,
-  diag,
-  Result,
-  syn::ItemStruct,
-};
+use macro_tools::{attr, diag, generic_params, item_struct, Result, syn::ItemStruct};
 
 pub fn not( input : proc_macro::TokenStream  ) -> Result< proc_macro2::TokenStream >
 {
@@ -14,15 +8,22 @@ pub fn not( input : proc_macro::TokenStream  ) -> Result< proc_macro2::TokenStre
   let has_debug = attr::has_debug( parsed.attrs.iter() )?;
   let item_name = &parsed.ident;
 
+  let ( _generics_with_defaults, generics_impl, generics_ty, generics_where )
+    = generic_params::decompose( &parsed.generics );
+
+  let constructor = generate_struct_constructor( &parsed );
+
   let result = qt!
   {
-    impl ::core::ops::Not for #item_name
+    impl< #generics_impl > ::core::ops::Not for #item_name< #generics_ty >
+    where
+      #generics_where
     {
-      type Output = ();
+      type Output = Self;
 
       fn not( self ) -> Self::Output
       {
-        ()
+        #constructor
       }
     }
 };
@@ -34,4 +35,50 @@ pub fn not( input : proc_macro::TokenStream  ) -> Result< proc_macro2::TokenStre
   }
 
   Ok( result )
+}
+
+fn generate_struct_constructor( item_struct: &ItemStruct ) -> proc_macro2::TokenStream
+{
+  let field_types = item_struct::field_types( &item_struct );
+  let field_names = item_struct::field_names( &item_struct );
+
+  match ( field_types.len(), field_names )
+  {
+    ( 0, _ ) => generate_unit_constructor(),
+    ( _, Some( field_names )) => generate_named_constructor( field_names ),
+    ( _, None ) => generate_tuple_constructor( field_types.len() ),
+  }
+}
+
+fn generate_unit_constructor() -> proc_macro2::TokenStream
+{
+  qt! { Self {} }
+}
+
+fn generate_named_constructor<'a>
+(
+  field_names : impl macro_tools::IterTrait< 'a, &'a syn::Ident >
+)
+-> proc_macro2::TokenStream
+{
+  let values = field_names
+    .clone()
+    .map( | field_name  |
+    {
+      qt! { #field_name: !self.#field_name }
+    });
+
+  qt! { Self { #(#values),* } }
+}
+
+fn generate_tuple_constructor(fields_len : usize) -> proc_macro2::TokenStream
+{
+  let values = (0..fields_len)
+    .map(|i|
+    {
+      let index = syn::Index::from(i);
+      qt! { !self.#index }
+    });
+
+  qt! { Self ( #(#values),* ) }
 }
