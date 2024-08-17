8a68608cf1ed9c0c6c052249209ba33826a174ef
wandalen <wandalen.me@gmail.com>
2024-08-17 16:19:01 +0300

    experimenting

--- module/core/format_tools/src/format/print.rs
+++ module/core/format_tools/src/format/print.rs
@@ -185,9 +185,10 @@ pub( crate ) mod private
                   write!( f.buf, "{}", cell_separator )?;
                 }
 
-                println!( "icol : {icol} | irow : {irow} | width : {width} | cell_width : {cell_width}" );
                 let lspaces = ( width - cell_width ) / 2;
-                let rspaces = ( width - cell_width ) / 2 + cell_width - slice.len();
+                let rspaces = ( width - cell_width + 1 ) / 2 + cell_width - slice.len();
+                // println!( "icol : {icol} | irow : {irow} | width : {width} | cell_width : {cell_width} | lspaces : {lspaces} | rspaces : {rspaces}" );
+
                 if lspaces > 0
                 {
                   write!( f.buf, "{:<width$}", " ", width = lspaces )?;
