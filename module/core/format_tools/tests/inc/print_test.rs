fdb240642fe4a2c3f798fe6c9217595eda5886f4
wandalen <wandalen.me@gmail.com>
2024-08-18 21:57:13 +0300

    experimenting

--- module/core/format_tools/tests/inc/print_test.rs
+++ module/core/format_tools/tests/inc/print_test.rs
@@ -169,89 +169,89 @@ fn table_to_string()
 
 //
 
-// #[ test ]
-// fn custom_formatter()
-// {
-//   // use the_module::TableToString;
-//   let test_objects = test_objects_gen();
+#[ test ]
+fn custom_formatter()
+{
+  // use the_module::TableToString;
+  let test_objects = test_objects_gen();
+
+  let mut output = String::new();
+  let mut formatter = the_module::Styles::default();
+
+  formatter.cell_prefix = "( ".into();
+  formatter.cell_postfix = " )".into();
+  formatter.cell_separator = "|".into();
+
+  formatter.row_prefix = ">".into();
+  formatter.row_postfix = "<".into();
+  formatter.row_separator = "\n".into();
+
+  let as_table = AsTable::new( &test_objects );
+  // let mut context : Context< '_, print::All > = Context::new( &mut output, formatter );
+  let mut context = Context::new( &mut output, formatter );
+  let result = the_module::TableFormatter::fmt( &as_table, &mut context );
+  assert!( result.is_ok() );
+
+  println!( "\noutput\n{output}" );
+  assert!( output.contains( "id" ) );
+  assert!( output.contains( "created_at" ) );
+  assert!( output.contains( "file_ids" ) );
+  assert!( output.contains( "tools" ) );
+
+  let exp = r#">( id )|( created_at )|(          file_ids          )|(           tools            )<
+>( 1  )|( 1627845583 )|(        [                   )|(                            )<
+>(    )|(            )|(            "file1",        )|(                            )<
+>(    )|(            )|(            "file2",        )|(                            )<
+>(    )|(            )|(        ]                   )|(                            )<
+>( 2  )|(     13     )|( [                          )|( [                          )<
+>(    )|(            )|(     "file3",               )|(     {                      )<
+>(    )|(            )|(     "file4\nmore details", )|(         "tool1": "value1", )<
+>(    )|(            )|( ]                          )|(     },                     )<
+>(    )|(            )|(                            )|(     {                      )<
+>(    )|(            )|(                            )|(         "tool2": "value2", )<
+>(    )|(            )|(                            )|(     },                     )<
+>(    )|(            )|(                            )|( ]                          )<"#;
+
+  a_id!( output.as_str(), exp );
+
+
+}
+
 //
-//   let mut output = String::new();
-//   let mut formatter = the_module::Styles::default();
+
+#[ test ]
+fn filter_col()
+{
+  let test_objects = test_objects_gen();
+
+  let mut output = String::new();
+  let mut formatter = the_module::Styles::default();
+
+  // formatter.filter_col = print::No;
+
+  let as_table = AsTable::new( &test_objects );
+  // let mut context : Context< '_, _ > = Context::new( &mut output, formatter );
+  let mut context = Context::new( &mut output, formatter );
+  let result = the_module::TableFormatter::fmt( &as_table, &mut context );
+  assert!( result.is_ok() );
+
+  let exp = r#"│ id │ created_at │          file_ids          │           tools            │
+│ 1  │ 1627845583 │        [                   │                            │
+│    │            │            "file1",        │                            │
+│    │            │            "file2",        │                            │
+│    │            │        ]                   │                            │
+│ 2  │     13     │ [                          │ [                          │
+│    │            │     "file3",               │     {                      │
+│    │            │     "file4\nmore details", │         "tool1": "value1", │
+│    │            │ ]                          │     },                     │
+│    │            │                            │     {                      │
+│    │            │                            │         "tool2": "value2", │
+│    │            │                            │     },                     │
+│    │            │                            │ ]                          │"#;
+
+  a_id!( output.as_str(), exp );
+
+
+}
+
 //
-//   formatter.cell_prefix = "( ".into();
-//   formatter.cell_postfix = " )".into();
-//   formatter.cell_separator = "|".into();
-//
-//   formatter.row_prefix = ">".into();
-//   formatter.row_postfix = "<".into();
-//   formatter.row_separator = "\n".into();
-//
-//   let as_table = AsTable::new( &test_objects );
-//   // let mut context : Context< '_, print::All > = Context::new( &mut output, formatter );
-//   let mut context = Context::new( &mut output, formatter );
-//   let result = the_module::TableFormatter::fmt( &as_table, &mut context );
-//   assert!( result.is_ok() );
-//
-//   println!( "\noutput\n{output}" );
-//   assert!( output.contains( "id" ) );
-//   assert!( output.contains( "created_at" ) );
-//   assert!( output.contains( "file_ids" ) );
-//   assert!( output.contains( "tools" ) );
-//
-//   let exp = r#">( id )|( created_at )|(          file_ids          )|(           tools            )<
-// >( 1  )|( 1627845583 )|(        [                   )|(                            )<
-// >(    )|(            )|(            "file1",        )|(                            )<
-// >(    )|(            )|(            "file2",        )|(                            )<
-// >(    )|(            )|(        ]                   )|(                            )<
-// >( 2  )|(     13     )|( [                          )|( [                          )<
-// >(    )|(            )|(     "file3",               )|(     {                      )<
-// >(    )|(            )|(     "file4\nmore details", )|(         "tool1": "value1", )<
-// >(    )|(            )|( ]                          )|(     },                     )<
-// >(    )|(            )|(                            )|(     {                      )<
-// >(    )|(            )|(                            )|(         "tool2": "value2", )<
-// >(    )|(            )|(                            )|(     },                     )<
-// >(    )|(            )|(                            )|( ]                          )<"#;
-//
-//   a_id!( output.as_str(), exp );
-//
-//
-// }
-//
-// //
-//
-// #[ test ]
-// fn filter_col()
-// {
-//   let test_objects = test_objects_gen();
-//
-//   let mut output = String::new();
-//   let mut formatter = the_module::Styles::default();
-//
-//   // formatter.filter_col = print::No;
-//
-//   let as_table = AsTable::new( &test_objects );
-//   // let mut context : Context< '_, _ > = Context::new( &mut output, formatter );
-//   let mut context = Context::new( &mut output, formatter );
-//   let result = the_module::TableFormatter::fmt( &as_table, &mut context );
-//   assert!( result.is_ok() );
-//
-//   let exp = r#"│ id │ created_at │          file_ids          │           tools            │
-// │ 1  │ 1627845583 │        [                   │                            │
-// │    │            │            "file1",        │                            │
-// │    │            │            "file2",        │                            │
-// │    │            │        ]                   │                            │
-// │ 2  │     13     │ [                          │ [                          │
-// │    │            │     "file3",               │     {                      │
-// │    │            │     "file4\nmore details", │         "tool1": "value1", │
-// │    │            │ ]                          │     },                     │
-// │    │            │                            │     {                      │
-// │    │            │                            │         "tool2": "value2", │
-// │    │            │                            │     },                     │
-// │    │            │                            │ ]                          │"#;
-//
-//   a_id!( output.as_str(), exp );
-//
-//
-// }
-//
-// // xxx
