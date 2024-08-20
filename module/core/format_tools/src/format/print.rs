f74056cb5580f685ef20911f3bf7e7f1d2c6bd80
wandalen <wandalen.me@gmail.com>
2024-08-20 12:43:03 +0300

    experimenting

--- module/core/format_tools/src/format/print.rs
+++ module/core/format_tools/src/format/print.rs
@@ -157,80 +157,80 @@ pub( crate ) mod private
   #[ derive( Debug, Default ) ]
   pub struct TableStringerDefault;
 
-  impl< CellKey > TableWriter< CellKey > for TableStringerDefault
-  where
-    CellKey : table::CellKey + ?Sized,
-  {
-    fn extract_write< 'buf, 'data >( x : &FormatExtract< 'data >, c : &mut Context< 'buf > ) -> fmt::Result
-    {
-      use md_math::MdOffset;
-
-      let cell_prefix = &c.styles.cell_prefix;
-      let cell_postfix = &c.styles.cell_postfix;
-      let cell_separator = &c.styles.cell_separator;
-      let row_prefix = &c.styles.row_prefix;
-      let row_postfix = &c.styles.row_postfix;
-      let row_separator = &c.styles.row_separator;
-
-      for ( irow, row ) in x.row_descriptors.iter().enumerate()
-      {
-        let height = row.0;
-
-        for islice in 0..height
-        {
-
-          if irow > 0
-          {
-            write!( c.buf, "{}", row_separator )?;
-          }
-
-          write!( c.buf, "{}", row_prefix )?;
-
-          // for k in &x.col_order
-          // for k in &x.col_order
-          for icol in 0 .. x.col_descriptors.len()
-          {
-            let col = &x.col_descriptors[ &icol ];
-            let cell_width = x.data[ irow ][ &icol ].1[0];
-            let width = col.0;
-            // let icol = col.1;
-            let md_index = [ islice, icol, irow as usize ];
-            let slice = x.slices[ x.slices_dim.md_offset( md_index ) ];
-
-            // println!( "md_index : {md_index:?} | md_offset : {} | slice : {slice}", x.slices_dim.md_offset( md_index ) );
-
-            if icol > 0
-            {
-              write!( c.buf, "{}", cell_separator )?;
-            }
-
-            write!( c.buf, "{}", cell_prefix )?;
-
-            let lspaces = ( width - cell_width ) / 2;
-            let rspaces = ( width - cell_width + 1 ) / 2 + cell_width - slice.len();
-            // println!( "icol : {icol} | irow : {irow} | width : {width} | cell_width : {cell_width} | lspaces : {lspaces} | rspaces : {rspaces}" );
-
-            if lspaces > 0
-            {
-              write!( c.buf, "{:<width$}", " ", width = lspaces )?;
-            }
-            write!( c.buf, "{}", slice )?;
-            if rspaces > 0
-            {
-              write!( c.buf, "{:>width$}", " ", width = rspaces )?;
-            }
-
-            write!( c.buf, "{}", cell_postfix )?;
-          }
-
-          write!( c.buf, "{}", row_postfix )?;
-        }
-
-      }
-
-      Ok(())
-    }
-  }
+//   impl< CellKey > TableWriter< CellKey > for TableStringerDefault
+//   where
+//     CellKey : table::CellKey + ?Sized,
+//   {
+//     fn extract_write< 'buf, 'data >( x : &FormatExtract< 'data >, c : &mut Context< 'buf > ) -> fmt::Result
+//     {
+//       use md_math::MdOffset;
+//
+//       let cell_prefix = &c.styles.cell_prefix;
+//       let cell_postfix = &c.styles.cell_postfix;
+//       let cell_separator = &c.styles.cell_separator;
+//       let row_prefix = &c.styles.row_prefix;
+//       let row_postfix = &c.styles.row_postfix;
+//       let row_separator = &c.styles.row_separator;
+//
+//       for ( irow, row ) in x.row_descriptors.iter().enumerate()
+//       {
+//         let height = row.0;
+//
+//         for islice in 0..height
+//         {
+//
+//           if irow > 0
+//           {
+//             write!( c.buf, "{}", row_separator )?;
+//           }
+//
+//           write!( c.buf, "{}", row_prefix )?;
+//
+//           // for k in &x.col_order
+//           // for k in &x.col_order
+//           for icol in 0 .. x.col_descriptors.len()
+//           {
+//             let col = &x.col_descriptors[ icol ];
+//             let cell_width = x.data[ irow ][ icol ].1[0];
+//             let width = col.0;
+//             // let icol = col.1;
+//             let md_index = [ islice, icol, irow as usize ];
+//             let slice = x.slices[ x.slices_dim.md_offset( md_index ) ];
+//
+//             // println!( "md_index : {md_index:?} | md_offset : {} | slice : {slice}", x.slices_dim.md_offset( md_index ) );
+//
+//             if icol > 0
+//             {
+//               write!( c.buf, "{}", cell_separator )?;
+//             }
+//
+//             write!( c.buf, "{}", cell_prefix )?;
+//
+//             let lspaces = ( width - cell_width ) / 2;
+//             let rspaces = ( width - cell_width + 1 ) / 2 + cell_width - slice.len();
+//             // println!( "icol : {icol} | irow : {irow} | width : {width} | cell_width : {cell_width} | lspaces : {lspaces} | rspaces : {rspaces}" );
+//
+//             if lspaces > 0
+//             {
+//               write!( c.buf, "{:<width$}", " ", width = lspaces )?;
+//             }
+//             write!( c.buf, "{}", slice )?;
+//             if rspaces > 0
+//             {
+//               write!( c.buf, "{:>width$}", " ", width = rspaces )?;
+//             }
+//
+//             write!( c.buf, "{}", cell_postfix )?;
+//           }
+//
+//           write!( c.buf, "{}", row_postfix )?;
+//         }
+//
+//       }
+//
+//       Ok(())
+//     }
+//   }
 
   /// Trait for defining table formatting logic.
   ///
@@ -294,8 +294,8 @@ pub( crate ) mod private
               // for k in &x.col_order
               for icol in 0 .. x.col_descriptors.len()
               {
-                let col = &x.col_descriptors[ &icol ];
-                let cell_width = x.data[ irow ][ &icol ].1[0];
+                let col = &x.col_descriptors[ icol ];
+                let cell_width = x.data[ irow ][ icol ].1[0];
                 let width = col.0;
                 // let icol = col.1;
                 let md_index = [ islice, icol, irow as usize ];
@@ -310,6 +310,7 @@ pub( crate ) mod private
 
                 write!( c.buf, "{}", cell_prefix )?;
 
+                // println!( "icol : {icol} | irow : {irow} | width : {width} | cell_width : {cell_width}" );
                 let lspaces = ( width - cell_width ) / 2;
                 let rspaces = ( width - cell_width + 1 ) / 2 + cell_width - slice.len();
                 // println!( "icol : {icol} | irow : {irow} | width : {width} | cell_width : {cell_width} | lspaces : {lspaces} | rspaces : {rspaces}" );
@@ -361,7 +362,8 @@ pub( crate ) mod private
 
     /// Descriptors for each column, including optional title, width, and index.
     //                             key        width, index
-    pub col_descriptors : HashMap< usize, ( usize, usize ) >,
+    pub col_descriptors : Vec< ( usize, usize ) >,
+    // pub col_descriptors : HashMap< usize, ( usize, usize ) >,
 
     /// Descriptors for each row, including height.
     //                           height
@@ -369,7 +371,7 @@ pub( crate ) mod private
 
     /// Extracted data for each cell, including string content and size.
     //                        key,      string,              size,
-    pub data : Vec< HashMap< usize, ( Cow< 'data, str >, [ usize ; 2 ] ) > >,
+    pub data : Vec< Vec< ( Cow< 'data, str >, [ usize ; 2 ] ) > >,
 
     /// Dimensions of slices for retrieving data from multi-matrix.
     pub slices_dim : [ usize ; 3 ],
@@ -435,38 +437,42 @@ pub( crate ) mod private
     )
     -> fmt::Result
     where
-      't : 'data,
       'data : 't,
       Table : TableRows< RowKey = RowKey, Row = Row, CellKey = CellKey, CellRepr = CellRepr >,
       Table : TableHeader< CellKey = CellKey >,
       Table : TableSize,
       RowKey : table::RowKey,
       Row : Cells< CellKey, CellRepr > + 'data,
-      CellKey : table::CellKey + ?Sized + 't,
+      CellKey : table::CellKey + ?Sized + 'data,
       CellRepr : table::CellRepr,
     {
       use md_math::MdOffset;
 
       let mcells = table.mcells();
+
       //                                 key        width, index
-      let mut col_descriptors : HashMap< usize, ( usize, usize ) > = HashMap::new();
+      let mut key_to_ikey : HashMap< &'t CellKey, usize > = HashMap::new();
+      // let mut col_order : Vec< &'t CellKey > = Vec::new();
+
+      //                               width, index
+      // let mut col_descriptors : Vec< ( usize, usize ) > = HashMap::new();
+      let mut col_descriptors : Vec< ( usize, usize ) > = Vec::new();
       //                               height
       let mut row_descriptors : Vec< ( usize, ) > = Vec::with_capacity( mcells[ 1 ] );
 
-      let mut col_order : Vec< &'t CellKey > = Vec::new();
       let mut has_header = false;
 
-      let mut data : Vec< HashMap< usize, ( Cow< 'data, str >, [ usize ; 2 ] ) > > = Vec::new();
+      let mut data : Vec< Vec< ( Cow< 't, str >, [ usize ; 2 ] ) > > = Vec::new();
       let rows = table.rows();
       let mut irow : isize = -1;
 
-      let mut row_add = | row : &'_ mut dyn _IteratorTrait< Item = ( &'t CellKey, Cow< 'data, str > ) > |
+      let mut row_add = | row : &'_ mut dyn _IteratorTrait< Item = ( &'t CellKey, Cow< 't, str > ) > |
       {
 
         irow += 1;
         row_descriptors.push( ( 1, ) );
 
-        let fields : HashMap< usize, ( Cow< 'data, str >, [ usize ; 2 ] ) > = row
+        let fields : Vec< ( Cow< 't, str >, [ usize ; 2 ] ) > = row
         .filter_map
         (
           | ( key, val ) |
@@ -479,28 +485,65 @@ pub( crate ) mod private
 
             let sz = string::size( &val );
             let l = col_descriptors.len();
-            row_descriptors[ irow as usize ] = ( row_descriptors[ irow as usize ].0.max( sz[ 1 ] ), );
+            let mut ikey = 0;
 
-            col_descriptors
-            .entry( l )
-            .and_modify( | col |
+            key_to_ikey
+            .entry( key )
+            .and_modify( | _ikey |
             {
+              let col = &mut col_descriptors[ *_ikey ];
               col.0 = col.0.max( sz[ 0 ] );
+              // println!( "and_modify {col:?}" );
+              ikey = *_ikey;
             })
             .or_insert_with( ||
             {
-              col_order.push( key );
-              ( sz[ 0 ], l )
-              // let title = if is_title { Some( val.as_ref() ) } else { None };
-              // ( title, sz[ 0 ], l )
+              let _ikey = l;
+              // col_order.push( key );
+              ikey = _ikey;
+              col_descriptors.push( ( sz[ 0 ], _ikey ) );
+              let mut col = col_descriptors[ _ikey ];
+              // println!( "or_insert_with {col:?}" );
+              _ikey
             });
 
-            return Some( ( l, ( val, sz ) ) );
+            //   {
+            //     let ikey = l;
+            //     col_order.push( key );
+            //     key_to_ikey.push( ( sz[ 0 ], ikey ) );
+            //     ikey
+            //   },
+            //   Some( &ikey ) =>
+            //   {
+            //     let col = col_descriptors[ ikey ];
+            //     col.0 = col.0.max( sz[ 0 ] );
+            //     ikey
+            //   },
+//
+//             };
+
+            row_descriptors[ irow as usize ] = ( row_descriptors[ irow as usize ].0.max( sz[ 1 ] ), );
+
+            // col_descriptors
+            // .entry( l )
+            // .and_modify( | col |
+            // {
+            //   col.0 = col.0.max( sz[ 0 ] );
+            // })
+            // .or_insert_with( ||
+            // {
+            //   col_order.push( key );
+            //   ( sz[ 0 ], l )
+            //   // let title = if is_title { Some( val.as_ref() ) } else { None };
+            //   // ( title, sz[ 0 ], l )
+            // });
+
+            return Some( ( val, sz ) );
+            // return Some( ( ikey, ( val, sz ) ) );
           }
         )
         .collect();
         data.push( fields );
-
 
       };
 
@@ -591,10 +634,12 @@ pub( crate ) mod private
 
         irow += 1;
 
+        // dbg!( x.col_descriptors.len() );
         // for ( icol, k ) in x.col_order.iter().enumerate()
         for icol in 0 .. x.col_descriptors.len()
         {
-          let cell = &row_data[ &icol ];
+          // dbg!( &row_data, icol );
+          let cell = &row_data[ icol ];
           string::lines( cell.0.as_ref() )
           .enumerate()
           .for_each( | ( layer, s ) |
