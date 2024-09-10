cd27dce716b7ec427e84ce0fcf013bf6407f07db
wandalen <wandalen.me@gmail.com>
2024-09-10 17:25:13 +0300

    mod_interface : fixing

--- module/core/mod_interface/examples/mod_interface_debug/src/main.rs
+++ module/core/mod_interface/examples/mod_interface_debug/src/main.rs
@@ -3,21 +3,18 @@ use mod_interface::mod_interface;
 
 //
 
-fn main()
-{
-  assert_eq!( prelude::inner_is(), child::prelude::inner_is() );
-}
-
-//
-
 mod private {}
-
-//
-
 mod_interface!
 {
   // Uncomment to see expanded code.
   // #![ debug ]
   /// Child.
   layer child;
+}
+
+//
+
+fn main()
+{
+  assert_eq!( prelude::inner_is(), child::prelude::inner_is() );
 }
--- module/core/mod_interface/examples/mod_interface_trivial/src/child.rs
+++ module/core/mod_interface/examples/mod_interface_trivial/src/child.rs
@@ -1,15 +1,23 @@
+
+// Define a private namespace for all its items.
 mod private
 {
-  /// Routine of inner module.
-  pub fn inner_is() -> bool
-  {
-    true
-  }
+  /// Only my thing.
+  pub fn my_thing() -> bool { true }
+  /// Parent module should also has this thing.
+  pub fn orphan_thing() -> bool { true }
+  /// This thing should be exposed.
+  pub fn exposed_thing() -> bool { true }
+  /// This thing should be in prelude.
+  pub fn prelude_thing() -> bool { true }
 }
 
 //
 
-mod_interface::mod_interface!
+crate::mod_interface!
 {
-  prelude use inner_is;
+  own use my_thing;
+  orphan use orphan_thing;
+  exposed use exposed_thing;
+  prelude use prelude_thing;
 }
