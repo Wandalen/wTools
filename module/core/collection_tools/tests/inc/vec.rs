eaa0cd1afb260c037088534e983c85aea8e069af
wandalen <wandalen.me@gmail.com>
2024-11-05 18:31:53 +0200

    test_tools : rid off cyclic dependcy wip

--- module/core/collection_tools/tests/inc/vec.rs
+++ module/core/collection_tools/tests/inc/vec.rs
@@ -86,7 +86,7 @@ fn iters()
   impl IntoIterator for MyContainer
   {
     type Item = i32;
-    type IntoIter = the_module::vec::IntoIter< i32 >;
+    type IntoIter = the_module::vector::IntoIter< i32 >;
     // qqq : should work -- works
 
     fn into_iter( self ) -> Self::IntoIter
@@ -98,7 +98,7 @@ fn iters()
   impl< 'a > IntoIterator for &'a MyContainer
   {
     type Item = &'a i32;
-    type IntoIter = the_module::vec::Iter< 'a, i32 >;
+    type IntoIter = the_module::vector::Iter< 'a, i32 >;
 
     fn into_iter( self ) -> Self::IntoIter
     {
@@ -109,7 +109,7 @@ fn iters()
   impl< 'a > IntoIterator for &'a mut MyContainer
   {
     type Item = &'a mut i32;
-    type IntoIter = the_module::vec::IterMut< 'a, i32 >;
+    type IntoIter = the_module::vector::IterMut< 'a, i32 >;
 
     fn into_iter( self ) -> Self::IntoIter
     {
