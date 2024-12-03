209dab45bc653bf168331565b47e23fee2d740ca
InAnYan <ruslanpopov1512@gmail.com>
2024-12-03 14:24:54 +0200

    Implement `ScenarioProcessed`

--- module/move/assistant/src/agents/scenario_processed.rs
+++ module/move/assistant/src/agents/scenario_processed.rs
@@ -7,28 +7,28 @@
 
 mod private
 {
-  use serde::
-  {
-    Serialize,
-    Deserialize,
-  };
+  use std::collections::HashMap;
 
   use crate::*;
   use agents::
   {
     path::Path,
-    scenario_raw::ScenarioRaw,
+    scenario_raw::
+    {
+      ScenarioRaw,
+      NodeRaw,
+    },
   };
 
   /// New type for templates in scenarios.
-  #[ derive( Debug, Serialize, Deserialize, PartialEq ) ]
+  #[ derive( Debug, PartialEq ) ]
   pub struct TemplateBody( pub String );
 
   /// Struct that represents user written scenarios.
   ///
-  /// This is a processed form of a scenario, templates and paths are distinguished here with types.
+  /// This is a processed form of a scenario, paths are distinguished here with types.
   /// For more simplistic representation of scenarios, use `ScenarioRaw` type.
-  #[ derive( Debug, Serialize, Deserialize, PartialEq ) ]
+  #[ derive( Debug, PartialEq ) ]
   pub struct ScenarioProcessed
   {
     /// Nodes in the scenario.
@@ -41,17 +41,18 @@ mod private
 
     fn try_from( scenario_raw : ScenarioRaw ) -> Result< Self, Self::Error >
     {
-      let nodes : Result< Vec< Node >, Self::Error > = scenario_raw.nodes.into_iter().map( | n | n.try_from() ).collect();
+      let nodes : Result< Vec< Node >, Self::Error > = 
+      scenario_raw.nodes.into_iter().map( | rn | Node::try_from( rn ) ).collect();
       
-      Ok( Self { nodes } )
+      Ok( Self { nodes : nodes? } )
     }
   }
 
   /// Node representation in a scenario file.
   ///
-  /// This is a processed form of a node, templates and paths are distinguished here with types.
+  /// This is a processed form of a node, paths are distinguished here with types.
   /// For more simplistic representation of scenarios, use `NodeRaw` type.
-  #[ derive( Debug, Serialize, Deserialize, PartialEq ) ]
+  #[ derive( Debug, PartialEq ) ]
   pub struct Node
   {
     /// ID of the node. Must be unique, will also identify node output. 
@@ -60,8 +61,8 @@ mod private
     /// Type of the node.
     pub r#type : Path,
 
-    /// Specific type of `Node`.
-    pub kind : NodeKind,
+    /// Parameters of the node.
+    pub params : HashMap< String, String >,
 
     /// ID of the next node to execute.
     pub next : Path,
@@ -71,7 +72,7 @@ mod private
   {
     type Error = std::io::Error;
 
-    fn try_form( node_raw : NodeRaw ) -> Result< Self, Self::Error >
+    fn try_from( node_raw : NodeRaw ) -> Result< Self, Self::Error >
     {
       Ok
       (
@@ -79,70 +80,10 @@ mod private
         {
           id : node_raw.id,
           r#type : Path::try_from( node_raw.r#type )?,
-          kind : NodeKind::try_from( node_raw.params )?,
-          next : Path::try_from( node_raw.path )?,
+          params : node_raw.params,
+          next : Path::try_from( node_raw.next )?,
         }
       )
-    }
-  }
-
-  /// Representation of different types of nodes. Contains parameters unique to types.
-  /// Used in `Node` struct, where you can find common parameters like `id`, `type`, and others.
-  #[ derive( Debug, Serialize, Deserialize, PartialEq ) ]
-  pub enum NodeKind
-  {
-    /// Read input from `stdin`.
-    TriggerStdin
-    {
-      /// Prompt to display to `stdout` before reading the `stdin`.
-      prompt : TemplateBody,
-    },
-
-    /// Get output from LLM.
-    AgentsCompletion
-    {
-      /// Agent's system message template.
-      system_message : TemplateBody,
-      /// Agent's user message template.
-      user_message : TemplateBody,
-      /// Reuse chat history of other agent.
-      agent_reuse : Path,
-    },
-
-    /// Print output to `stdout`.
-    EventStdout
-    {
-      /// Prompt to display to `stdout`.
-      output : TemplateBody,
-    },
-  }
-
-  impl NodeKind
-  {
-    /// Convert `NodeRaw` into `NodeKind`.
-    pub fn try_from_params
-    (
-      params : HashMap< String, String >,
-      r#type : &Path,
-    ) -> Result< Self, std::io::Error >
-    {
-      match path.inner().as_str()
-      {
-        "::trigger::stdin" =>
-        {
-          let Some()
-        }
-      }
-    }
-  }
-
-  impl TryFrom< HashMap< String, String > > for NodeKind
-  {
-    type Error = std::io::Error;
-
-    fn try_from( map : HashMap< String, String> ) -> Result< Self, Self::Error >
-    {
-      
     }
   }
 }
@@ -153,5 +94,6 @@ crate::mod_interface!
   {
     TemplateBody,
     ScenarioProcessed,
+    Node,
   };
 }
