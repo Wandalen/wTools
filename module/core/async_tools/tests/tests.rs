d3ca519ec94cd27e6f3c1e57f5d28b47e30a53d5
wandalen <wandalen.me@gmail.com>
2025-04-09 16:34:50 +0300

    cleaning async_tools

--- module/core/async_tools/tests/tests.rs
+++ module/core/async_tools/tests/tests.rs
@@ -1,9 +1,9 @@
+//! All tests
 #![ allow( unused_imports ) ]
 
 include!( "../../../../module/step/meta/src/module/terminal.rs" );
 
 use async_tools as the_module;
-// use test_tools::exposed::*;
 
 #[ cfg( feature = "enabled" ) ]
 #[ path = "../../../../module/core/async_from/tests/inc/mod.rs" ]
