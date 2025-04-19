a50546dc74183b74341cdfb0219995e64ad86a51
wandalen <wandalen.me@gmail.com>
2025-04-19 23:09:20 +0300

    tasks and move out llm tools

--- module/move/llm_tools/tests/inc/mod.rs
+++
@@ -1,6 +0,0 @@
-use super::*;
-use test_tools::exposed::*;
-
-mod basic_test;
-mod content_tests;
-mod experiment;
--- module/move/llm_tools/tests/tests.rs
+++
@@ -1,8 +0,0 @@
-//! All test.
-#![ allow( unused_imports ) ]
-
-include!( "../../../../module/step/meta/src/module/terminal.rs" );
-
-pub use llm_tools as the_module;
-#[ cfg( feature = "enabled" ) ]
-mod inc;
