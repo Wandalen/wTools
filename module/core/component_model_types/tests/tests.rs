f7d505b447d46b2a30a1fc779fc26b357d6ae79c
wandalen <wandalen.me@gmail.com>
2025-05-18 17:25:23 +0300

    fix(component_model_types): Add crate-level documentation to tests/tests.rs

--- module/core/component_model_types/tests/tests.rs
+++ module/core/component_model_types/tests/tests.rs
@@ -1,4 +1,4 @@
-#![allow(missing_docs)]
+//! Integration tests for the component_model_types crate.
 #![ allow( unused_imports ) ]
 
 include!( "../../../../module/step/meta/src/module/aggregating.rs" );
