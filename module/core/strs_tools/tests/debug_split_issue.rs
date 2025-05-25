03ca11763f11e2ee9df10b2e55875730dfdbcf06
wanguardd <wanguardd@gmail.com>
2025-05-25 10:10:08 +0000

    strs_tools : cover issue by test

---
+++ module/core/strs_tools/tests/debug_split_issue.rs
@@ -0,0 +1,21 @@
+// This file is for debugging purposes only and will be removed after the issue is resolved.
+
+#[ test ]
+fn debug_split_issue()
+{
+  use strs_tools::string::split::{ SplitOptionsFormer, SplitType };
+
+  let input = r#"cmd name::"a\\\\b\\\"c\\\'d\\ne\\tf""#;
+  let mut splitter = SplitOptionsFormer::new( vec![ "::", " " ] )
+  .src( input )
+  .quoting( true )
+  .quoting_prefixes( vec![ r#"""#, r#"'"# ] )
+  .quoting_postfixes( vec![ r#"""#, r#"'"# ] )
+  .perform();
+
+  println!( "Input: {:?}", input );
+  while let Some( item ) = splitter.next()
+  {
+    println!( "Split item: {:?}", item );
+  }
+}
