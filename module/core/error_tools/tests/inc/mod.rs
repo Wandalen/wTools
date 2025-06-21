1d36a2f28a44f56b4f6f07957fd49516cc36598b
wandalen <wandalen.me@gmail.com>
2025-06-21 11:01:57 +0300

    error_tools : fix

--- module/core/error_tools/tests/inc/mod.rs
+++ module/core/error_tools/tests/inc/mod.rs
@@ -1,7 +1,7 @@
 #[ allow( unused_imports ) ]
 use super::*;
-
-use test_tools::exposed::*;
+// use test_tools::exposed::*;
+use test_tools::{ tests_impls, tests_index, a_id };
 
 mod basic_test;
 mod namespace_test;
@@ -10,3 +10,4 @@ mod assert_test;
 #[ cfg( not( feature = "no_std" ) ) ]
 mod err_with_test;
 mod untyped_test;
+
