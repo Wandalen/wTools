1c0958ab1c8858dc89ad7df64ba290d2cb5846c6
wanguardd <wanguardd@gmail.com>
2025-07-05 18:30:01 +0000

    former : debug off

--- module/core/former/tests/inc/enum_unit_tests/keyword_variant_derive.rs
+++ module/core/former/tests/inc/enum_unit_tests/keyword_variant_derive.rs
@@ -2,7 +2,7 @@ use former::Former; // Ensure derive is in scope
 use super::*; // Needed for the include
 
 #[derive(Debug, PartialEq, Former)]
-#[former(standalone_constructors)]
+#[former(standalone_constructors, debug)]
 #[allow(non_camel_case_types)] // Explicitly allowing for testing keyword-like names
 pub enum KeywordTest {
   r#fn,
