f97d15e3a22542416bca1c1c68a1fe4a5e27fb31
wanguardd <wanguardd@gmail.com>
2025-07-23 21:05:25 +0000

    unilang_instruction_praser

---
+++ module/alias/unilang_instruction_parser/tests/smoke_test.rs
@@ -0,0 +1,13 @@
+//! Smoke testing of the package.
+
+#[ test ]
+fn local_smoke_test()
+{
+  ::test_tools::smoke_test_for_local_run();
+}
+
+#[ test ]
+fn published_smoke_test()
+{
+  ::test_tools::smoke_test_for_published_run();
+}
