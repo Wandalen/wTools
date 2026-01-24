c41e797c9cc03f1ac6a99923f2739381d4f81661
wandalen <wandalen.me@gmail.com>
2026-01-24 14:53:19 +0200

    chore: remove deprecated pth module and related workflows
    - Delete wtools/dev/module/core/pth crate entirely
    - Remove GitHub Actions workflows for unilang modules
    - Clean up 4 workflow files and complete pth module structure

--- module/core/pth/tests/inc/absolute_path_test.rs
+++
@@ -1,5 +0,0 @@
-use super :: *;
-
-mod basic_test;
-mod from_paths_test;
-mod try_from_test;
--- module/core/pth/tests/tests.rs
+++
@@ -1,9 +0,0 @@
-//! All tests.
-#![allow(unused_imports)]
-
-include!("../../../../module/step/meta/src/module/terminal.rs");
-
-use pth as the_module;
-
-#[ cfg( feature = "enabled" ) ]
-mod inc;
